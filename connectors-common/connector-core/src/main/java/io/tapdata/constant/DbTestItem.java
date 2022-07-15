f59afb4973cf86e4f7d0db986b2a86cb321c648e
jarad <coolgj@163.com>
2022-07-15 15:25:44 +0800

    fix:testitem

--- connectors-common/connector-core/src/main/java/io/tapdata/constant/DbTestItem.java
+++ connectors-common/connector-core/src/main/java/io/tapdata/constant/DbTestItem.java
@@ -3,6 +3,7 @@ package io.tapdata.constant;
 public enum DbTestItem {
 
     HOST_PORT("Check host port is invalid"),
+    CHECK_VERSION("Check supported database version"),
     CHECK_CDC_PRIVILEGES("Check replication privileges"),
     CHECK_TABLE_PRIVILEGE("Check all for table privilege"),
     CHECK_LOG_PLUGIN("Check log plugin for database"),
