85272ecbfc03fcf91e4b00b58e04df4a58a4bded
shuuufu <shu_fu1201@163.com>
2022-07-19 14:51:03 +0800

    merge schema remove job_analyze field

--- manager/tm-common/src/main/java/com/tapdata/tm/commons/schema/SchemaUtils.java
+++ manager/tm-common/src/main/java/com/tapdata/tm/commons/schema/SchemaUtils.java
@@ -39,6 +39,8 @@ public class SchemaUtils {
         List<Schema> _inputSchemas = inputSchemas.stream().filter(Objects::nonNull).collect(Collectors.toList());
         Schema targetSchema = cloneSchema(schema);
 
+
+
         if (targetSchema == null) {
             if (_inputSchemas.size() > 0) {
                 targetSchema = _inputSchemas.remove(0);
@@ -50,7 +52,19 @@ public class SchemaUtils {
         if (targetSchema == null) {
             log.warn("Can't merge non schema.");
             return null;
+        } else {
+            List<Field> fields = targetSchema.getFields();
+            if (CollectionUtils.isNotEmpty(fields)) {
+                List<Field> removeList = new ArrayList<>();
+                for (Field field : fields) {
+                    if ("job_analyze".equals(field.getSource())) {
+                        removeList.add(field);
+                    }
+                }
+                fields.removeAll(removeList);
+            }
         }
+
 
         Schema finalTargetSchema = targetSchema;
         List<String> inputSchemaFieldIds = _inputSchemas.stream().flatMap(s -> s.getFields().stream())
