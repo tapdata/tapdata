c52b8b8db9baf35d429e435b76a72455db2d0f91
Aplomb <aplomb.chen@gmail.com>
2022-07-25 22:13:18 +0800

    Fixed Time pattern issue; some issues.

--- iengine/api/src/main/java/io/tapdata/aspect/task/StopTaskException.java
+++ iengine/api/src/main/java/io/tapdata/aspect/task/StopTaskException.java
@@ -5,7 +5,14 @@ import io.tapdata.entity.error.TapAPIErrorCodes;
 import io.tapdata.error.EngineErrorCodes;
 
 public class StopTaskException extends CoreException {
-	public StopTaskException() {
-		super(EngineErrorCodes.STOP_TASK, "Stop task", LEVEL_ERROR);
+	public StopTaskException(String message, Throwable throwable) {
+		super(EngineErrorCodes.STOP_TASK, message, LEVEL_ERROR);
+		initCause(throwable);
+	}
+	public StopTaskException(String message) {
+		this(message, null);
+	}
+	public StopTaskException(Throwable throwable) {
+		this("Stop task", throwable);
 	}
 }
