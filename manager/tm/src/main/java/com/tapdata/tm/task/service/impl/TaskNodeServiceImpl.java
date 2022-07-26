62389a28b19c17a20367a937748ad9db9a76fac2
jiuyetx <meteor209@gmail.com>
2022-07-26 20:31:30 +0800

    fix bug

--- manager/tm/src/main/java/com/tapdata/tm/task/service/impl/TaskNodeServiceImpl.java
+++ manager/tm/src/main/java/com/tapdata/tm/task/service/impl/TaskNodeServiceImpl.java
@@ -252,6 +252,7 @@ public class TaskNodeServiceImpl implements TaskNodeService {
         DAG temp = DAG.build(build);
 
         taskDto.setDag(null);
+        taskDto.setSyncType(TaskDto.SYNC_TYPE_DEDUCE_SCHEMA);
         SubTaskDto subTaskDto = new SubTaskDto();
         subTaskDto.setStatus(SubTaskDto.STATUS_WAIT_RUN);
         subTaskDto.setParentTask(taskDto);
