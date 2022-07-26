043e7fb05a22fea9ce4eea51347360f8aa416f8a
jiuyetx <meteor209@gmail.com>
2022-07-26 22:53:13 +0800

    add getRun

--- manager/tm/src/main/java/com/tapdata/tm/task/controller/TaskController.java
+++ manager/tm/src/main/java/com/tapdata/tm/task/controller/TaskController.java
@@ -764,8 +764,7 @@ public class TaskController extends BaseController {
     @Operation(description = "js节点试运行结果获取")
     public ResponseMessage<JsResultVo> getRun(@RequestParam String taskId,
                                          @RequestParam String jsNodeId) {
-
-        return success(new JsResultVo());
+        return success(taskNodeService.getRun(taskId, jsNodeId));
     }
 
 }
