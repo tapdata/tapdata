1d03bdcdafcd3cab19b844250ed45afcbb971e35
Harvey <Zerohyuan@163.com>
2022-07-27 00:25:05 +0800

    fix: bug of js deduce schema

--- iengine/iengine-app/src/main/java/io/tapdata/websocket/handler/TestRunTaskHandler.java
+++ iengine/iengine-app/src/main/java/io/tapdata/websocket/handler/TestRunTaskHandler.java
@@ -15,6 +15,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @EventHandlerAnnotation(type = "testRun")
 public class TestRunTaskHandler implements WebSocketEventHandler<WebSocketEventResult> {
@@ -24,6 +25,9 @@ public class TestRunTaskHandler implements WebSocketEventHandler<WebSocketEventR
 	private ClientMongoOperator clientMongoOperator;
 
 	private TaskService<SubTaskDto> taskService;
+
+	private Map<String, TaskClient<SubTaskDto>> taskClientMap = new ConcurrentHashMap<>();
+
 
 	@Override
 	public void initialize(ClientMongoOperator clientMongoOperator) {
@@ -43,11 +47,17 @@ public class TestRunTaskHandler implements WebSocketEventHandler<WebSocketEventR
 		SubTaskDto subTaskDto = JSONUtil.map2POJO(event, SubTaskDto.class);
 
 		String taskId = subTaskDto.getId().toHexString();
+		if (taskClientMap.containsKey(taskId)) {
+			logger.warn("{} task is running, skip", taskId);
+			return WebSocketEventResult.handleFailed(WebSocketEventResult.Type.TEST_RUN, "task is running...");
+		}
 		TaskClient<SubTaskDto> taskClient = taskService.startTestTask(subTaskDto);
 		try {
+			taskClientMap.put(taskClient.getTask().getId().toHexString(), taskClient);
 			taskClient.join();
 		} finally {
 			AspectUtils.executeAspect(new TaskStopAspect().task(taskClient.getTask()));
+			taskClientMap.remove(taskClient.getTask().getId().toHexString());
 		}
 
 		logger.info("test run task {} {}, cost {}ms", taskId, taskClient.getStatus(), (System.currentTimeMillis() - startTs));
