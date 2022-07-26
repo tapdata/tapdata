884cd7f853a889fdd4067ec29c76b092faa1534b
harsen <harsen@tapdata.io>
2022-07-26 15:28:25 +0800

    Feat: connection add createType attribute

---
+++ manager/tm-common/src/main/java/com/tapdata/tm/commons/util/CreateTypeEnum.java
@@ -0,0 +1,27 @@
+package com.tapdata.tm.commons.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 创建类型
+ *
+ * @author <a href="mailto:harsen_lin@163.com">Harsen</a>
+ * @version v1.0 2022/7/26 15:10 Create
+ */
+@Getter
+@AllArgsConstructor
+public enum CreateTypeEnum {
+    User("用户"), System("系统"),
+    ;
+
+    private final String cn;
+
+    public static boolean isSystem(String str) {
+        return System.name().equals(str);
+    }
+
+    public static boolean isUser(String str) {
+        return !isSystem(str);
+    }
+}
