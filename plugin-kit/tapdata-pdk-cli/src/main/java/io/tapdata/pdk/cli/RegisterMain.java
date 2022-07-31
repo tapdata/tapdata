de5e3548349c74a959ba3e6dbd8e91c314793f19
dayun <dayun@tapdata.io>
2022-07-31 20:41:04 +0800

    update pom

--- plugin-kit/tapdata-pdk-cli/src/main/java/io/tapdata/pdk/cli/RegisterMain.java
+++ plugin-kit/tapdata-pdk-cli/src/main/java/io/tapdata/pdk/cli/RegisterMain.java
@@ -16,9 +16,9 @@ public class RegisterMain {
 		if (null != resource) basePath = resource.getPath() + "../../../../";
 		System.out.println("basePath:"+basePath);
 		args = new String[]{
-				"register", "-a", "3324cfdf-7d3e-4792-bd32-571638d4562f", "-t", "http://localhost:3000",
+//				"register", "-a", "3324cfdf-7d3e-4792-bd32-571638d4562f", "-t", "http://localhost:3000",
 //                "register", "-a", "3324cfdf-7d3e-4792-bd32-571638d4562f", "-t", "http://192.168.1.132:31787",
-//				"register", "-a", "3324cfdf-7d3e-4792-bd32-571638d4562f", "-t", "http://192.168.1.132:31966",
+				"register", "-a", "3324cfdf-7d3e-4792-bd32-571638d4562f", "-t", "http://192.168.1.132:31966",
 //				"register", "-a", "3324cfdf-7d3e-4792-bd32-571638d4562f", "-t", "http://192.168.1.181:31321",
 
 //                "/Users/aplomb/dev/tapdata/GithubProjects/idaas-pdk/dist/empty-connector-v1.1-SNAPSHOT.jar",
