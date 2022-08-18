6bcbe0ca6b1c417f0a373f4d338187c4252a1321
不掉发的程序员 <73671105+11000100111010101100111@users.noreply.github.com>
2022-08-18 14:39:59 +0800

    Multilingual support under connection test

--- connectors-common/mysql-core/src/main/java/io/tapdata/connector/mysql/constant/MysqlTestItem.java
+++ connectors-common/mysql-core/src/main/java/io/tapdata/connector/mysql/constant/MysqlTestItem.java
@@ -6,12 +6,12 @@ package io.tapdata.connector.mysql.constant;
  * @create 2022-04-26 14:17
  **/
 public enum MysqlTestItem {
-	HOST_PORT("Check host port is invalid"),
-	CHECK_VERSION("Check database version"),
-	CHECK_CDC_PRIVILEGES("Check database cdc privileges"),
-	CHECK_BINLOG_MODE("Check binlog mode"),
-	CHECK_BINLOG_ROW_IMAGE("Check binlog row image"),
-	CHECK_CREATE_TABLE_PRIVILEGE("Check create table privilege"),
+	HOST_PORT("${checkHostPortInvalid}"),
+	CHECK_VERSION("${checkDatabaseVersion}"),
+	CHECK_CDC_PRIVILEGES("${checkDatabaseCdcPrivileges}"),
+	CHECK_BINLOG_MODE("${checkBinlogMode}"),
+	CHECK_BINLOG_ROW_IMAGE("${checkBinlogRowImage}"),
+	CHECK_CREATE_TABLE_PRIVILEGE("${checkCreateTablePrivilege}"),
 	;
 
 	private String content;
