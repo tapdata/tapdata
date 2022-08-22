6e13a5c3b519c19fa75919777b5bb624a7825b3c
不掉发的程序员 <73671105+11000100111010101100111@users.noreply.github.com>
2022-08-22 11:49:40 +0800

    Decomposing the two branches of multilingualism and charset

--- connectors-common/mysql-core/src/main/java/io/tapdata/connector/mysql/constant/MysqlTestItem.java
+++ connectors-common/mysql-core/src/main/java/io/tapdata/connector/mysql/constant/MysqlTestItem.java
@@ -6,12 +6,12 @@ package io.tapdata.connector.mysql.constant;
  * @create 2022-04-26 14:17
  **/
 public enum MysqlTestItem {
-	HOST_PORT("${checkHostPortInvalid}"),
-	CHECK_VERSION("${checkDatabaseVersion}"),
-	CHECK_CDC_PRIVILEGES("${checkDatabaseCdcPrivileges}"),
-	CHECK_BINLOG_MODE("${checkBinlogMode}"),
-	CHECK_BINLOG_ROW_IMAGE("${checkBinlogRowImage}"),
-	CHECK_CREATE_TABLE_PRIVILEGE("${checkCreateTablePrivilege}"),
+	HOST_PORT("checkHostPortInvalid"),
+	CHECK_VERSION("checkDatabaseVersion"),
+	CHECK_CDC_PRIVILEGES("checkDatabaseCdcPrivileges"),
+	CHECK_BINLOG_MODE("checkBinlogMode"),
+	CHECK_BINLOG_ROW_IMAGE("checkBinlogRowImage"),
+	CHECK_CREATE_TABLE_PRIVILEGE("checkCreateTablePrivilege"),
 	;
 
 	private String content;
