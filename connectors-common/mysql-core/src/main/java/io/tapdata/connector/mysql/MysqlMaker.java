55bc6541590d68c08e798757e880ba3f0491cf5a
ningmeng777 <54097058+ningmeng777@users.noreply.github.com>
2022-08-26 18:30:17 +0800

    fix taptype defaultValue带有''没法建表成功

--- connectors-common/mysql-core/src/main/java/io/tapdata/connector/mysql/MysqlMaker.java
+++ connectors-common/mysql-core/src/main/java/io/tapdata/connector/mysql/MysqlMaker.java
@@ -8,6 +8,7 @@ import io.tapdata.entity.schema.TapField;
 import io.tapdata.entity.schema.TapIndex;
 import io.tapdata.entity.schema.TapIndexField;
 import io.tapdata.entity.schema.TapTable;
+import io.tapdata.entity.schema.type.TapNumber;
 import io.tapdata.entity.utils.DataMap;
 import io.tapdata.pdk.apis.context.TapConnectorContext;
 import io.tapdata.pdk.apis.entity.QueryOperator;
@@ -225,7 +226,14 @@ public class MysqlMaker implements SqlMaker {
 		// default value
 		String defaultValue = tapField.getDefaultValue() == null ? "" : tapField.getDefaultValue().toString();
 		if (StringUtils.isNotBlank(defaultValue)) {
-			fieldSql += " DEFAULT '" + defaultValue + "'";
+			if(tapField.getTapType() instanceof TapNumber){
+               if(defaultValue.contains("'")){
+				   defaultValue =defaultValue.replace("'","");
+			   }
+				fieldSql += " DEFAULT '" + defaultValue + "'";
+
+			}
+
 		}
 
 		// comment
