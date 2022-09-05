2ac91f2c68cd98117ca09a4a5f7c856568f6fd16
不掉发的程序员 <73671105+11000100111010101100111@users.noreply.github.com>
2022-09-05 19:07:24 +0800

    httpEntity

--- connectors/coding-connector/src/main/java/io/tapdata/coding/utils/http/HttpEntity.java
+++ connectors/coding-connector/src/main/java/io/tapdata/coding/utils/http/HttpEntity.java
@@ -4,30 +4,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HttpEntity<String,V> {
-    private Map<String,V> header;
+    private Map<String,V> entity;
     public static HttpEntity create(){
         return new HttpEntity<>();
     }
     public HttpEntity<String,V> builder(String key,V value){
-        if (null==this.header) this.header = new HashMap<>();
-        this.header.put(key,value);
+        if (null==this.entity) this.entity = new HashMap<>();
+        this.entity.put(key,value);
         return this;
     }
     public HttpEntity<String,V> clean(){
-        this.header = new HashMap<>();
+        this.entity = new HashMap<>();
         return this;
     }
     public HttpEntity<String,V> clean(String key){
-        this.header.remove(key);
+        this.entity.remove(key);
         return this;
     }
     public HttpEntity<String,V> clean(String ...keys){
         for (String key : keys) {
-            this.header.remove(key);
+            this.entity.remove(key);
         }
         return this;
     }
     public Map<String,V> getEntity(){
-        return this.header;
+        return this.entity;
     }
 }
