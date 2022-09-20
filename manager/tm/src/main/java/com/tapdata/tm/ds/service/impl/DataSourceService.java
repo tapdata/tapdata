a62c043a72bcdbdb804e550ea1dd627f5082c5c5
jiuyetx <meteor209@gmail.com>
2022-09-20 08:28:24 +0800

    copy connection set loadStatus=null

--- manager/tm/src/main/java/com/tapdata/tm/ds/service/impl/DataSourceService.java
+++ manager/tm/src/main/java/com/tapdata/tm/ds/service/impl/DataSourceService.java
@@ -657,6 +657,7 @@ public class DataSourceService extends BaseService<DataSourceConnectionDto, Data
 				//插入复制的数据源
 				entity.setName(connectionName);
 				entity.setLoadCount(0);
+				entity.setLoadFieldsStatus("");
 				entity = repository.save(entity, user);
 				break;
 			} catch (Exception e) {
