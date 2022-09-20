4cc60e820fdb2c95a98a6dcf6b1446ed1d308459
jiuyetx <meteor209@gmail.com>
2022-09-20 17:43:29 +0800

    add task tag search

--- manager/tm/src/main/java/com/tapdata/tm/task/entity/TaskEntity.java
+++ manager/tm/src/main/java/com/tapdata/tm/task/entity/TaskEntity.java
@@ -3,6 +3,7 @@ package com.tapdata.tm.task.entity;
 import com.tapdata.tm.base.entity.BaseEntity;
 import com.tapdata.tm.commons.dag.DAG;
 import com.tapdata.tm.commons.dag.SchemaTransformerResult;
+import com.tapdata.tm.commons.schema.Tag;
 import com.tapdata.tm.commons.task.dto.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -126,7 +127,7 @@ public class TaskEntity extends BaseEntity {
      */
     private List<SchemaTransformerResult> metadataMappings;
 
-    private List<Map<String,String>> listtags;
+    private List<Tag> listtags;
 
     /**
      * 访问节点
