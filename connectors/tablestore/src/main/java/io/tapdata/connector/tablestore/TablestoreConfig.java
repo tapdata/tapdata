e74da8eade52ab641e13760a3aac5a6d2e37bad6
jiuyetx <meteor209@gmail.com>
2022-10-04 00:11:46 +0800

    TableStore 1st commit

---
+++ connectors/tablestore/src/main/java/io/tapdata/connector/tablestore/TablestoreConfig.java
@@ -0,0 +1,70 @@
+package io.tapdata.connector.tablestore;
+
+import io.tapdata.entity.utils.BeanUtils;
+import io.tapdata.entity.utils.InstanceFactory;
+
+import java.util.Map;
+
+public class TablestoreConfig {
+    private static final BeanUtils BEAN_UTILS = InstanceFactory.instance(BeanUtils.class);
+
+    private String endpoint;
+    private String instance;
+    private String id;
+    private String key;
+    private String token;
+    private String clientType;
+
+    public TablestoreConfig load(Map<String, Object> map) {
+        assert BEAN_UTILS != null;
+        return BEAN_UTILS.mapToBean(map, this);
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    public String getInstance() {
+        return instance;
+    }
+
+    public void setInstance(String instance) {
+        this.instance = instance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getClientType() {
+        return clientType;
+    }
+
+    public void setClientType(String clientType) {
+        this.clientType = clientType;
+    }
+}
