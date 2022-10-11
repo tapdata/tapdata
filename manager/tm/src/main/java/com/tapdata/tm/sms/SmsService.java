09883a050215199f8c5dd8db6701d78268fa6aa6
Leon <lirufei0808@gmail.com>
2022-10-11 18:21:21 +0800

    remove access key in code

--- manager/tm/src/main/java/com/tapdata/tm/sms/SmsService.java
+++ manager/tm/src/main/java/com/tapdata/tm/sms/SmsService.java
@@ -75,9 +75,6 @@ public class SmsService {
             //初始化ascClient需要的几个参数
             final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
             final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
-            //替换成你的AK
-            final String accessKeyId = "LTAI5tRs7Bi4t4ngvH94uX17";//你的accessKeyId,参考本文档步骤2
-            final String accessKeySecret = "BD2jMva28sSjMoXiJNDMpetiTVkbQS";//你的accessKeySecret，参考本文档步骤2
             //初始化ascClient,暂时不支持多region（请勿修改）
             IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
                     accessKeySecret);
