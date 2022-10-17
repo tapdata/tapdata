e41f94796c774a14849ec4804744596df6c6bef7
shuuufu <shu_fu1201@163.com>
2022-10-17 18:44:08 +0800

    feat: task renew and delete timeout

--- manager/tm/src/main/java/com/tapdata/tm/task/controller/TaskResetLogController.java
+++ manager/tm/src/main/java/com/tapdata/tm/task/controller/TaskResetLogController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("api/TaskResetLogs")
 public class TaskResetLogController extends BaseController {
 
     @Autowired
