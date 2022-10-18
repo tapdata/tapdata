0376da0a9a33730acb5998224d7371b08d8fa9e2
Gavin <2749984520@qq.com>
2022-10-18 17:57:43 +0800

    fix: zoho file name of jar.

--- plugin-kit/tapdata-pdk-cli/src/main/java/io/tapdata/pdk/cli/RegisterMain.java
+++ plugin-kit/tapdata-pdk-cli/src/main/java/io/tapdata/pdk/cli/RegisterMain.java
@@ -34,7 +34,7 @@ public class RegisterMain {
         Hive1(BASE_PATH + "connectors/dist/hive1-connector-v1.0-SNAPSHOT.jar", "all", "hive1"),
         Mariadb(BASE_PATH + "connectors/dist/mariadb-connector-v1.0-SNAPSHOT.jar", "all", "mariadb"),
         Coding(BASE_PATH + "connectors/dist/coding-connector-v1.0-SNAPSHOT.jar", "all", "coding"),
-        ZoHo(BASE_PATH + "connectors/dist/ZoHo-connector-v1.0-SNAPSHOT.jar", "all", "zoho"),
+        ZoHo(BASE_PATH + "connectors/dist/zoho-connector-v1.0-SNAPSHOT.jar", "all", "zoho"),
         Tidb(BASE_PATH + "connectors/dist/tidb-connector-v1.0-SNAPSHOT.jar", "all", "tidb"),
 
 
