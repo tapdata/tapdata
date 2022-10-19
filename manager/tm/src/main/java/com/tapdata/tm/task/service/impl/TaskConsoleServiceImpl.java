6506e6bdd71b36d334e812e1145d72945a81daf2
jiuyetx <meteor209@gmail.com>
2022-10-19 16:59:05 +0800

    update task relation api code

--- manager/tm/src/main/java/com/tapdata/tm/task/service/impl/TaskConsoleServiceImpl.java
+++ manager/tm/src/main/java/com/tapdata/tm/task/service/impl/TaskConsoleServiceImpl.java
@@ -10,6 +10,7 @@ import com.tapdata.tm.task.vo.RelationTaskRequest;
 import com.tapdata.tm.utils.Lists;
 import com.tapdata.tm.utils.MongoUtils;
 import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -37,13 +38,13 @@ public class TaskConsoleServiceImpl implements TaskConsoleService {
 
         List<RelationTaskInfoVo> result = Lists.newArrayList();
         if (RelationTaskRequest.type_logCollector.equals(request.getType())) {
-            getLogCollector(connectionIds, result);
+            getLogCollector(connectionIds, result, request);
         } else if (RelationTaskRequest.type_shareCache.equals(request.getType())) {
-            getShareCache(connectionIds, result);
+            getShareCache(connectionIds, result, request);
         } else if (RelationTaskRequest.type_inspect.equals(request.getType())) {
         } else {
-            getLogCollector(connectionIds, result);
-            getShareCache(connectionIds, result);
+            getLogCollector(connectionIds, result, request);
+            getShareCache(connectionIds, result, request);
 
             result = result.stream().sorted(Comparator.nullsFirst(Comparator.comparing(RelationTaskInfoVo::getStartTime).reversed()))
                     .collect(Collectors.toList());
@@ -51,11 +52,12 @@ public class TaskConsoleServiceImpl implements TaskConsoleService {
         return result;
     }
 
-    private void getShareCache(List<String> connectionIds, List<RelationTaskInfoVo> result) {
+    private void getShareCache(List<String> connectionIds, List<RelationTaskInfoVo> result, RelationTaskRequest request) {
         Criteria cacheCriteria = Criteria.where("is_deleted").is(false).and("syncType").ne("logCollector")
                 .and("dag.nodes.type").is(NodeEnum.mem_cache.name())
                 .and("dag.nodes.connectionId").in(connectionIds);
-        List<TaskDto> cacheTasks = taskService.findAll(new Query(cacheCriteria).with(Sort.by(Sort.Order.desc("startTime"))));
+
+        List<TaskDto> cacheTasks = getFilterCriteria(request, cacheCriteria);
         cacheTasks.forEach(task -> {
             RelationTaskInfoVo logRelation = RelationTaskInfoVo.builder().id(task.getId().toHexString()).name(task.getName())
                     .status(task.getStatus())
@@ -67,11 +69,12 @@ public class TaskConsoleServiceImpl implements TaskConsoleService {
         });
     }
 
-    private void getLogCollector(List<String> connectionIds, List<RelationTaskInfoVo> result) {
-        Criteria logCriteria = Criteria.where("is_deleted").is(false).and("syncType").is("logCollector")
+    private void getLogCollector(List<String> connectionIds, List<RelationTaskInfoVo> result, RelationTaskRequest request) {
+        Criteria criteria = Criteria.where("is_deleted").is(false).and("syncType").is("logCollector")
                 .and("dag.nodes.type").is(NodeEnum.logCollector.name())
                 .and("dag.nodes.connectionIds").in(connectionIds);
-        List<TaskDto> logTasks = taskService.findAll(new Query(logCriteria).with(Sort.by(Sort.Order.desc("startTime"))));
+
+        List<TaskDto> logTasks = getFilterCriteria(request, criteria);
         logTasks.forEach(task -> {
             RelationTaskInfoVo logRelation = RelationTaskInfoVo.builder().id(task.getId().toHexString()).name(task.getName())
                     .status(task.getStatus())
@@ -81,5 +84,16 @@ public class TaskConsoleServiceImpl implements TaskConsoleService {
 
             result.add(logRelation);
         });
+    }
+
+    private List<TaskDto> getFilterCriteria(RelationTaskRequest request, Criteria criteria) {
+        if (StringUtils.isNotBlank(request.getStatus())) {
+            criteria.and("status").is(request.getStatus());
+        }
+        if (StringUtils.isNotBlank(request.getKeyword())) {
+            criteria.and("name").regex(request.getKeyword());
+        }
+
+        return taskService.findAll(new Query(criteria).with(Sort.by(Sort.Order.desc("startTime"))));
     }
 }
