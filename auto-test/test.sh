559c3050db5f724d5cf5db4b71342cbf0c32475b
Berry <xbsura@gmail.com>
2022-10-25 18:26:40 +0800

    ci: trigger a ci

--- auto-test/test.sh
+++ auto-test/test.sh
@@ -5,5 +5,5 @@ python3 init/create_datasource.py
 
 cd cases || exit
 for case_file in `ls test_*`; do
-    python3 runner.py "$case_file" --core --bench 12345 --clean
+    python3 runner.py "$case_file" --core --bench 12345
 done
