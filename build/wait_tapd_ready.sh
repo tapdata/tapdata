591d2a30580533592886c545b246d18d6d45aeec
Berry <xbsura@gmail.com>
2022-10-26 02:21:43 +0800

    chore: make a ci

---
+++ build/wait_tapd_ready.sh
@@ -0,0 +1,14 @@
+i="."
+while [[ 1 ]]; do
+  if [[ $i == "....." ]]; then
+    break
+  fi
+  sleep 10
+  i=$i"."
+  docker logs tapd|grep register_connector_complete
+  if [[ $? -eq 0 ]]; then
+    echo "tapdata container ready!"
+    break
+  fi
+  echo "still wait tapdata container ready..."
+done
