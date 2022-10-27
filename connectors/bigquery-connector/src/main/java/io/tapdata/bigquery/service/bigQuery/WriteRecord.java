09d9cacf8eded6deca48413af56ffbf029f6aaf7
Gavin <2749984520@qq.com>
2022-10-27 16:54:03 +0800

    fix: use context config for service account.

--- connectors/bigquery-connector/src/main/java/io/tapdata/bigquery/service/bigQuery/WriteRecord.java
+++ connectors/bigquery-connector/src/main/java/io/tapdata/bigquery/service/bigQuery/WriteRecord.java
@@ -93,7 +93,7 @@ public class WriteRecord extends BigQueryStart{
     private final AtomicBoolean running = new AtomicBoolean(true);
 
     public void write(List<TapRecordEvent> tapRecordEvents, TapTable tapTable, Consumer<WriteListResult<TapRecordEvent>> writeListResultConsumer){
-        SqlMarker sqlMarker = SqlMarker.create(this.str);
+        SqlMarker sqlMarker = SqlMarker.create(this.config.serviceAccount());
         WriteListResult<TapRecordEvent> writeListResult = new WriteListResult<>(0L, 0L, 0L, new HashMap<>());
         TapRecordEvent errorRecord = null;
         try {
