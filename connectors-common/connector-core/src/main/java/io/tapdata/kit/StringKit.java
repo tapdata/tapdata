ad7771714d64ba8c514d751fb7fbe16c2ecea0a3
jarad <coolgj@163.com>
2022-10-31 10:23:19 +0800

    fix: move DateUtil,CalendarUtil to public

--- connectors-common/connector-core/src/main/java/io/tapdata/kit/StringKit.java
+++ connectors-common/connector-core/src/main/java/io/tapdata/kit/StringKit.java
@@ -209,4 +209,59 @@ public class StringKit {
         res.sort(Comparator.naturalOrder());
         return res;
     }
+
+    public static String leftPad(String str, int size, String padStr) {
+        if (str == null) {
+            return null;
+        } else {
+            if (EmptyKit.isEmpty(padStr)) {
+                padStr = " ";
+            }
+
+            int padLen = padStr.length();
+            int strLen = str.length();
+            int pads = size - strLen;
+            if (pads <= 0) {
+                return str;
+            } else if (padLen == 1 && pads <= 8192) {
+                return leftPad(str, size, padStr.charAt(0));
+            } else if (pads == padLen) {
+                return padStr.concat(str);
+            } else if (pads < padLen) {
+                return padStr.substring(0, pads).concat(str);
+            } else {
+                char[] padding = new char[pads];
+                char[] padChars = padStr.toCharArray();
+
+                for (int i = 0; i < pads; ++i) {
+                    padding[i] = padChars[i % padLen];
+                }
+
+                return (new String(padding)).concat(str);
+            }
+        }
+    }
+
+    public static String leftPad(String str, int size, char padChar) {
+        if (str == null) {
+            return null;
+        } else {
+            int pads = size - str.length();
+            if (pads <= 0) {
+                return str;
+            } else {
+                return pads > 8192 ? leftPad(str, size, String.valueOf(padChar)) : repeat(padChar, pads).concat(str);
+            }
+        }
+    }
+
+    public static String repeat(char ch, int repeat) {
+        if (repeat <= 0) {
+            return "";
+        } else {
+            char[] buf = new char[repeat];
+            Arrays.fill(buf, ch);
+            return new String(buf);
+        }
+    }
 }
