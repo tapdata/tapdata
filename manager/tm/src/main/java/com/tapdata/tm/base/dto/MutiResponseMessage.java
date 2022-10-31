d1cb14fd160dc43b5d32afc288498478265be5a1
shuuufu <shu_fu1201@163.com>
2022-10-31 17:44:30 +0800

    fix: #125907 batch operation response

---
+++ manager/tm/src/main/java/com/tapdata/tm/base/dto/MutiResponseMessage.java
@@ -0,0 +1,35 @@
+package com.tapdata.tm.base.dto;
+
+import com.tapdata.tm.utils.ThreadLocalUtils;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * @author lg<lirufei0808 @ gmail.com>
+ * @date 2020/9/11 9:24 上午
+ * @description
+ */
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class MutiResponseMessage {
+
+	private String reqId;
+
+	/**
+	 * 请求处理的代码
+	 */
+	protected String code = ResponseMessage.OK;
+
+	/**
+	 * 请求处理失败时的错误消息
+	 */
+	protected String message;
+	private String id;
+}
