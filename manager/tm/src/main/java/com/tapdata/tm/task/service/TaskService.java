ee961a92dd495dfc432323c6883ffb6f67ce7a8e
shuuufu <shu_fu1201@163.com>
2022-11-08 16:01:15 +0800

    fix: # 126719  dashboard data input output

--- manager/tm/src/main/java/com/tapdata/tm/task/service/TaskService.java
+++ manager/tm/src/main/java/com/tapdata/tm/task/service/TaskService.java
@@ -48,6 +48,7 @@ import com.tapdata.tm.messagequeue.dto.MessageQueueDto;
 import com.tapdata.tm.messagequeue.service.MessageQueueService;
 import com.tapdata.tm.metadatainstance.service.MetaDataHistoryService;
 import com.tapdata.tm.metadatainstance.service.MetadataInstancesService;
+import com.tapdata.tm.monitor.entity.MeasurementEntity;
 import com.tapdata.tm.monitor.param.IdParam;
 import com.tapdata.tm.monitor.service.MeasurementServiceV2;
 import com.tapdata.tm.monitoringlogs.service.MonitoringLogsService;
@@ -73,6 +74,7 @@ import com.tapdata.tm.worker.service.WorkerService;
 import com.tapdata.tm.worker.vo.CalculationEngineVo;
 import com.tapdata.tm.ws.enums.MessageType;
 import com.tapdata.tm.ws.handler.EditFlushHandler;
+import io.tapdata.common.sample.request.Sample;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
@@ -92,7 +94,12 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -1665,7 +1672,7 @@ public class TaskService extends BaseService<TaskDto, TaskEntity, ObjectId, Task
         resultChart.put("chart3", getDataDevChart(syncTypeToTaskList));
 //        resultChart.put("chart4", dataDev);
         resultChart.put("chart5", inspectChart(user));
-//        resultChart.put("chart6", measurementService.getTransmitTotal(user));
+        resultChart.put("chart6", chart6(user));
         return resultChart;
     }
 
@@ -3348,5 +3355,76 @@ public class TaskService extends BaseService<TaskDto, TaskEntity, ObjectId, Task
         Update update = new Update();
         update.set("logSetting", logSetting);
         updateById(taskObjectId, update, userDetail);
+    }
+
+    public Map<String, Long> chart6(UserDetail user) {
+        Criteria criteria = Criteria.where("is_deleted").ne(true).and("syncType").in(TaskDto.SYNC_TYPE_SYNC, TaskDto.SYNC_TYPE_MIGRATE);
+        Query query = new Query(criteria);
+        query.fields().include("_id");
+        List<TaskDto> allDto = findAllDto(query, user);
+        List<String> ids = allDto.stream().map(a->a.getId().toHexString()).collect(Collectors.toList());
+
+        List<MeasurementEntity>  allMeasurements = new ArrayList<>();
+        ids.parallelStream().forEach(id -> {
+            MeasurementEntity measurement = measurementServiceV2.findLastMinuteByTaskId(id, user);
+            if (measurement != null) {
+                allMeasurements.add(measurement);
+            }
+        });
+
+        long output = 0;
+        long input = 0;
+        long insert = 0;
+        long update = 0;
+        long delete = 0;
+
+        for (MeasurementEntity allMeasurement : allMeasurements) {
+            if (allMeasurement == null) {
+                continue;
+            }
+            List<Sample> samples = allMeasurement.getSamples();
+            if (CollectionUtils.isNotEmpty(samples)) {
+                Optional<Sample> max = samples.stream().max(Comparator.comparing(Sample::getDate));
+                if (max.isPresent()) {
+                    Sample sample = max.get();
+                    Map<String, Number> vs = sample.getVs();
+                    int inputInsertTotal = (int) vs.get("inputInsertTotal");
+                    int inputOthersTotal = (int) vs.get("inputOthersTotal");
+                    int inputDdlTotal = (int) vs.get("inputDdlTotal");
+                    int inputUpdateTotal = (int) vs.get("inputUpdateTotal");
+                    int inputDeleteTotal = (int) vs.get("inputDeleteTotal");
+
+                    int outputInsertTotal = (int) vs.get("outputInsertTotal");
+                    int outputOthersTotal = (int) vs.get("outputOthersTotal");
+                    int outputDdlTotal = (int) vs.get("outputDdlTotal");
+                    int outputUpdateTotal = (int) vs.get("outputUpdateTotal");
+                    int outputDeleteTotal = (int) vs.get("outputDeleteTotal");
+                    output += outputInsertTotal;
+                    output += outputOthersTotal;
+                    output += outputDdlTotal;
+                    output += outputUpdateTotal;
+                    output += outputDeleteTotal;
+
+                    input += inputInsertTotal;
+                    input += inputOthersTotal;
+                    input += inputDdlTotal;
+                    input += inputUpdateTotal;
+                    input += inputDeleteTotal;
+
+                    insert += inputInsertTotal;
+                    update += inputUpdateTotal;
+                    delete += inputDeleteTotal;
+
+                }
+            }
+        }
+
+        Map<String, Long> chart6Map = new HashMap<>();
+        chart6Map.put("output", output);
+        chart6Map.put("input", input);
+        chart6Map.put("insert", insert);
+        chart6Map.put("update", update);
+        chart6Map.put("delete", delete);
+        return chart6Map;
     }
 }
