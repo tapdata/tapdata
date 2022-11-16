60817d1bd562a06c4e58a87f9a876c61cadfbccf
Aplomb <aplomb.chen@gmail.com>
2022-11-16 23:18:24 +0800

    Support cloud version.  (#164)
    * fix(Proxy): Use login url's port as ws port for cloud env.
    
    * fix(Proxy): Handle 80/443 default ports will return -1 case.
    
    * fix(Proxy): use correct wsPath for cloud env.
    
    * fix(Proxy): Use new way to identify cloud env.  more efficient than before.
    
    * fix(Proxy): TM can return correct ws path when detected cloud env, I fixed it base on wrong case(TM failed to detect cloud env).
    
    * fix(Proxy): Cloud v3's ws path is out of expectation, support more cases for ws path now.
    
    * fix(Proxy): Remove useless comment.
    
    * fix(Proxy): Generate webhook url base on the information from request url, which is more stable way than hard code.

--- manager/tm/src/main/java/com/tapdata/tm/ds/controller/DataSourceController.java
+++ manager/tm/src/main/java/com/tapdata/tm/ds/controller/DataSourceController.java
@@ -36,6 +36,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -409,8 +410,8 @@ public class DataSourceController extends BaseController {
      */
     @Operation(summary = "复制数据源")
     @PostMapping("{id}/copy")
-    public ResponseMessage<DataSourceConnectionDto> copy(@PathVariable("id") String id) {
-        return success(dataSourceService.copy(getLoginUser(), id));
+    public ResponseMessage<DataSourceConnectionDto> copy(@PathVariable("id") String id, HttpServletRequest request) {
+        return success(dataSourceService.copy(getLoginUser(), id, request.getRequestURI()));
     }
 
     /**
