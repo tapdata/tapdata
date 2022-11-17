56de8f5a0f898bf0485c6bdb399f3e79f1b475e6
不掉发的程序员 <73671105+11000100111010101100111@users.noreply.github.com>
2022-11-17 17:21:22 +0800

    Feat connectors bigquery (#180)
    * feat: test connection for bigquery.
    
    * fix(BigQuery): Passed write permission
    
    * feat: move package.
    
    * feat: add sql marker to big query.
    
    * feat: Table result to map;
    
    * fix: TableResult cannot cast to BigQueryResult.
    
    * fix:bigquery table result.
    
    * fix: TableResult Exceptions.
    
    * fix: TableFileds value.
    
    * feat: add test to WriteRecord.class
    
    * fix: Process SQL generation based on field type
    
    * feat: Test add delete/update/insert/select case for WriteRecord Test.
    
    * feat: TapField name.
    
    * fix:bigQuery Test case.
    
    * fix: update sql .
    
    * fix: complete the test case.
    
    * fix:write record bigQuery.
    
    * feat:add icon.
    
    * feat:json schema for big query
    
    * fix: use context config for service account.
    
    * feat: connection test.
    
    * fix: Splicing update sql.
    
    * feat: reegisterMain add BigQuery connector.
    
    * fix: tap event filter
    
    * fix:stop task when stop thread
    
    * fix: set sql value into sql.
    
    * fix:execute sql error with error taplog.
    
    * feat:doc for BigQuery.
    
    * fix: doc of bigquery zh_ch.
    
    * feat: Use one row of SQL insert record if exits to update .
    
    * fix: TapEvent's getFilter primaryKeys is true.
    
    * feat:Create a connection to obtain the dataset list through the command.
    
    * fix:do not verify table set when Create a connection to obtain the dataset list through the command
    
    * feat:create table for bigquery.
    
    * fix: json schema of dataTypes.
    
    * feat:get tap table list of table set.
    
    * feat:discover schema.
    
    * fix: throw no exception when query execute with no permission.
    
    * fix: back registerMain.
    
    * fix: table name join to string for sql in .
    
    * fix: modify upload url in register main
    
    * feat:register bigquery's capabilities
    
    * feat: drop table and clean table for bigquery.
    
    * fix: before drop table check it's exists.
    
    * fix: create table query sql error when judge table is or not exist.
    
    * fix: ignore null or not null.
    
    * fix: some sql error.
    
    * fix: insert sql.
    
    * fix:full tapdata by taptype into sql.
    
    * fix:TapUpdateRecordEvent cast to tapevent error.
    
    * fix: add google document link into doc.
    
    * fix: select table set sql.
    
    * fix: get table's description from ddl .
    
    * feat: check bigquery field .
    
    * feat: check field name.
    
    * fix:Compatible with JSON format.
    
    * fix: TapConnectorContext need to verify field.
    
    * fix:get table by table name.
    
    * fix: data type of INT64 and FLOAT64.
    
    * feat:schema count.
    
    * fix:value and type for tapdata.
    
    * fix: support batch write and etc.
    
    * fix:discoverSchema.
    
    * fix:clear some unnecessary codes.
    
    Co-authored-by: Aplomb <aplomb.chen@gmail.com>

---
+++ connectors/bigquery-connector/src/main/java/io/tapdata/bigquery/service/bigQuery/SqlMarker.java
@@ -0,0 +1,86 @@
+package io.tapdata.bigquery.service.bigQuery;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.bigquery.*;
+import io.tapdata.entity.error.CoreException;
+import io.tapdata.entity.logger.TapLogger;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.*;
+
+public class SqlMarker{
+    private static final String TAG = SqlMarker.class.getSimpleName();
+
+    GoogleCredentials credentials;
+    BigQuery bigQuery;
+    private SqlMarker(String credentialsJson){
+        try {
+            GoogleCredentials credentials = ServiceAccountCredentials.fromStream(new ByteArrayInputStream(credentialsJson.getBytes("utf8")));
+            this.credentials = credentials;
+            bigQuery();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private SqlMarker(GoogleCredentials credentials){
+        this.credentials = credentials;
+        bigQuery();
+    }
+
+    public static SqlMarker create(String credentialsJson){
+        return new SqlMarker(credentialsJson);
+    }
+
+    public static SqlMarker create(GoogleCredentials credentials){
+        return new SqlMarker(credentials);
+    }
+
+    public List<BigQueryResult> execute(String ...sql){
+        List<BigQueryResult> results = new ArrayList<>();
+        try {
+            for (String subSql : sql) {
+                if (null == subSql) continue;
+                BigQueryResult execute = execute(subSql);
+                if (null!=execute) results.add(execute);
+            }
+            //results.iterateAll().forEach(row -> row.forEach(val -> System.out.printf("%s,", val.toString())));
+            //System.out.println("Query performed successfully.");
+        } catch (BigQueryException | InterruptedException e) {
+            //System.out.println("Insert operation not performed \n" + e.toString());
+            TapLogger.error(TAG,"Big Query execute error,{} , {}",e.getMessage(),sql);
+        }
+        return results;
+    }
+
+    public BigQueryResult executeOnce(String sql){
+        try {
+            return execute(sql);
+        }catch (BigQueryException | InterruptedException e){
+            TapLogger.error(TAG,"Big Query execute error,{} , {}",e.getMessage(),sql);
+            throw new CoreException(String.format("%s Big Query execute error,%s , %s",TAG,e.getMessage(),sql));
+        }catch (Exception e){
+            throw e;
+        }
+    }
+
+    private BigQueryResult execute(String sql) throws InterruptedException {
+        QueryJobConfiguration queryConfig = QueryJobConfiguration.newBuilder(sql).build();
+        try {
+            return BigQueryResult.create(this.bigQuery.query(queryConfig));
+        }catch (Exception e){
+            throw new CoreException(e.getMessage());
+        }
+    }
+
+    private void bigQuery(){
+        this.bigQuery = BigQueryOptions.newBuilder().setCredentials(this.credentials).build().getService();
+    }
+
+    public BigQuery query(){
+        return this.bigQuery;
+    }
+
+}
