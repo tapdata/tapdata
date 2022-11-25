d5d8673bc9a421aebac8848e9545a1a1ac29e8c7
Gavin <2749984520@qq.com>
2022-11-25 18:10:58 +0800

    feat:Data source capability description output to .xlsx.

--- plugin-kit/tapdata-pdk-cli/src/main/java/io/tapdata/pdk/cli/TDDMssqlMain1.java
+++ plugin-kit/tapdata-pdk-cli/src/main/java/io/tapdata/pdk/cli/TDDMssqlMain1.java
@@ -6,10 +6,10 @@ public class TDDMssqlMain1 {
     public static void main(String[] args) {
         CommonUtils.setProperty("pdk_external_jar_path", "./connectors/dist");
         args = new String[]{
-                "test",
-                "-c",
-                "plugin-kit/tapdata-pdk-cli/src/main/resources/config/mssql.json",
-                "./connectors/dist/mssql-connector-v1.0-SNAPSHOT.jar"
+                "capabilities",
+                "-o",
+                "./output",
+                "./connectors/dist"
         };
         Main.registerCommands().execute(args);
     }
