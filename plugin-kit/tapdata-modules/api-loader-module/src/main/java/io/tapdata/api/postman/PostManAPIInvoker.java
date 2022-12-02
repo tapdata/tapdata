a8f2af85885643bcc3aa76d7048c853a7193c329
Aplomb <aplomb.chen@gmail.com>
2022-12-02 11:25:49 +0800

    feat(APIFactory): Added APIFactory for Gavin.

---
+++ plugin-kit/tapdata-modules/api-loader-module/src/main/java/io/tapdata/api/postman/PostManAPIInvoker.java
@@ -0,0 +1,15 @@
+package io.tapdata.api.postman;
+
+import io.tapdata.pdk.apis.api.APIInvoker;
+
+import java.util.Map;
+
+/**
+ * @author aplomb
+ */
+public class PostManAPIInvoker implements APIInvoker {
+	@Override
+	public Map<String, Object> invoke(String uri, String method, Map<String, Object> params) {
+		return null;
+	}
+}
