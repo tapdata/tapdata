77a99a103cc31abf629b358f45f666344877d38d
ningmeng777 <54097058+ningmeng777@users.noreply.github.com>
2022-12-02 19:16:56 +0800

    fix tidb checkConnection

--- connectors/tidb-connector/src/main/java/io/tapdata/connector/tidb/TidbConnector.java
+++ connectors/tidb-connector/src/main/java/io/tapdata/connector/tidb/TidbConnector.java
@@ -341,5 +341,6 @@ public class TidbConnector extends ConnectorBase {
         timeTemp = (hours < 10 ? ("0" + hours) : hours) + ":" + (minutes < 10 ? ("0" + minutes) : minutes) + ":" + (seconds < 10 ? ("0" + seconds) : seconds);
         return timeTemp;
     }
+
 }
 
