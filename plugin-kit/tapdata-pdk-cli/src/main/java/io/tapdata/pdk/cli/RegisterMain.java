32d5aff054c93128f8e562f0d036026e2b5ad95e
jiuyetx <meteor209@gmail.com>
2022-12-03 13:31:08 +0800

    add vika

--- plugin-kit/tapdata-pdk-cli/src/main/java/io/tapdata/pdk/cli/RegisterMain.java
+++ plugin-kit/tapdata-pdk-cli/src/main/java/io/tapdata/pdk/cli/RegisterMain.java
@@ -42,7 +42,8 @@ public class RegisterMain {
         Json(BASE_PATH + "connectors/dist/json-connector-v1.0-SNAPSHOT.jar", false, "all", "file"),
         Xml(BASE_PATH + "connectors/dist/xml-connector-v1.0-SNAPSHOT.jar", false, "all", "file"),
         Excel(BASE_PATH + "connectors/dist/excel-connector-v1.0-SNAPSHOT.jar", false, "all", "file"),
-        BigQuery(BASE_PATH + "connectors/dist/bigquery-connector-v1.0-SNAPSHOT.jar", false, "all", "bigquery")
+        BigQuery(BASE_PATH + "connectors/dist/bigquery-connector-v1.0-SNAPSHOT.jar", false, "all", "bigquery"),
+        Vika(BASE_PATH + "connectors/dist/vika-connector-v1.0-SNAPSHOT.jar", false, "all", "vika")
         ;
 
         private final String path;
