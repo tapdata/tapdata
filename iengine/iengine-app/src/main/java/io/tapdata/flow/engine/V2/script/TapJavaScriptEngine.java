46407a212b9de66ff0793ec47a19ce20309b1e7c
Aplomb <aplomb.chen@gmail.com>
2022-12-05 18:17:48 +0800

    fix(CustomConnector): Fixed load external jar caused error.
    Closes #129074

--- iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/script/TapJavaScriptEngine.java
+++ iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/script/TapJavaScriptEngine.java
@@ -16,8 +16,10 @@ import org.graalvm.polyglot.Value;
 
 import javax.script.*;
 import java.io.Reader;
+import java.net.URLClassLoader;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.function.Consumer;
 
 public class TapJavaScriptEngine implements ScriptEngine, Invocable {
 
@@ -25,10 +27,11 @@ public class TapJavaScriptEngine implements ScriptEngine, Invocable {
     private final Invocable invocable;
     private final String buildInScript;
 
+    private URLClassLoader externalJarClassLoader;
     public TapJavaScriptEngine(ScriptOptions scriptOptions) {
         ClientMongoOperator clientMongoOperator = BeanUtil.getBean(ClientMongoOperator.class);
         List<JavaScriptFunctions> javaScriptFunctions = JobUtil.getJavaScriptFunctions(clientMongoOperator);
-        this.buildInScript = ScriptUtil.initBuildInMethod(javaScriptFunctions, clientMongoOperator);
+        this.buildInScript = ScriptUtil.initBuildInMethod(javaScriptFunctions, clientMongoOperator, urlClassLoader -> externalJarClassLoader = urlClassLoader);
         this.scriptEngine = initScriptEngine(scriptOptions.getEngineName());
         invocable = (Invocable) scriptEngine;
     }
@@ -74,7 +77,10 @@ public class TapJavaScriptEngine implements ScriptEngine, Invocable {
 
     public Object applyClassLoaderContext(Callable<?> callable) {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        Thread.currentThread().setContextClassLoader(Application.class.getClassLoader());
+        ClassLoader theClassLoader = externalJarClassLoader;
+        if(theClassLoader == null)
+            theClassLoader = Application.class.getClassLoader();
+        Thread.currentThread().setContextClassLoader(theClassLoader);
         try {
             return callable.call();
         } catch (Exception e) {
