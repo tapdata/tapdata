1cdfdcf3be710f33453831d13913e45bef9fa453
IssaacWang <1960536585@qq.com>
2022-12-10 22:53:20 +0800

    tdengine testStreamRead fix

--- connectors/tdengine-connector/src/main/java/io/tapdata/connector/tdengine/subscribe/TDengineSubscribe.java
+++ connectors/tdengine-connector/src/main/java/io/tapdata/connector/tdengine/subscribe/TDengineSubscribe.java
@@ -38,8 +38,7 @@ public class TDengineSubscribe {
         try {
             // prepare
             CommonDbConfig config = tdengineJdbcContext.getConfig();
-            String jdbcUrl = String.format("jdbc:TAOS://%s:6030/%s?user=%s&password=%s", config.getHost(), config.getDatabase(), config.getUser(), config.getPassword());
-            Connection connection = DriverManager.getConnection(jdbcUrl);
+            Connection connection = tdengineJdbcContext.getConnection();
             // create topic
             List<String> topicList = new ArrayList<>();
             try (Statement statement = connection.createStatement()) {
@@ -75,20 +74,30 @@ public class TDengineSubscribe {
                 while (!shutdown.get()) {
                     ConsumerRecords<Map<String, Object>> records = taosConsumer.poll(Duration.ofMillis(100));
                     Optional<TopicPartition> topicPartition = getTopicPartition(records);
-                    if (topicPartition.isPresent() && EmptyKit.isNotBlank(topicPartition.get().getTableName())) {
+                    if (topicPartition.isPresent() && EmptyKit.isNotBlank(this.getTableName(topicPartition.get()))) {
                         for (Map<String, Object> record : records) {
-                            biConsumer.accept(record, topicPartition.get().getTableName());
+                            biConsumer.accept(record, this.getTableName(topicPartition.get()));
                         }
                     }
                     Thread.sleep(1000);
                 }
-                taosConsumer.unsubscribe();
             }
         } catch (SQLException | NoSuchFieldException | IllegalAccessException | InterruptedException e) {
             TapLogger.error(TAG, "Table data sync error: {}", e.getMessage(), e);
         } finally {
             shutdownCallBack.call();
         }
+    }
+
+    private String getTableName(TopicPartition topicPartition) {
+        String tableName = topicPartition.getTableName();
+        if (EmptyKit.isNotEmpty(tableName)) {
+            return tableName;
+        }
+        if (!topicPartition.getTopic().startsWith("topic_")) {
+            return null;
+        }
+        return topicPartition.getTopic().substring(6);
     }
 
     public Optional<TopicPartition> getTopicPartition(ConsumerRecords<Map<String, Object>> consumerRecords) throws NoSuchFieldException, IllegalAccessException {
