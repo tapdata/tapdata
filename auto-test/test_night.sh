3b1dee41b32b9c83bb661653e6d5a15bcee3de48
Berry <xbsura@gmail.com>
2022-12-14 23:44:07 +0800

    fix: add ut case

--- auto-test/test_night.sh
+++ auto-test/test_night.sh
@@ -18,9 +18,14 @@ rm -rf pass_jobs_number
 rm -rf /tmp/fail
 mkdir -p /tmp
 
+touch jobs_number
+touch pass_jobs_number
+
 if [[ "x"$CASE_CONFIG != "x" ]]; then
     echo $CASE_CONFIG|base64 -d > config.yaml
 fi
+
+cat config.yaml
 
 server=`head -3 config.yaml |grep server|awk -F '"' '{print $2}'`
 curl $server --max-time 3 2>&1 > /dev/null
@@ -49,12 +54,12 @@ data_set=$data_set"]"
 echo "std::out >> 环境准备: 用例执行编号为:"`cat init/.table_suffix_cache_file`", 导入数据集为: ${data_set}" >> cases/cases_result
 
 connectors="["
-for i in `cat config.yaml |grep connector|awk -F ":" '{print $2}'`; do
+for i in `cat config.yaml|grep connector|awk -F ":" '{print $2}'`; do
     connectors=$connectors","$i
 done
 connectors=$connectors"]"
 echo $connectors
-echo "std::out >> 用例: 创建数据源, 并加载模型, 类型包括: $connectos" >> cases/cases_result
+echo "std::out >> 用例: 创建数据源, 并加载模型, 数据源类型包括: $connectos" >> cases/cases_result
 
 cd cases
 
@@ -75,12 +80,12 @@ fi
 cd $p
 
 case_results=""
-for i in `cat cases/cases_result|grep "std::out >>"`; do
+for i in `cat cases/cases_result|grep "std::out"`; do
     case_results=$case_results'"'$i'",'
 done
 
-jobs_number=`wc -l cases/jobs_number`
-pass_jobs_number=`wc -l cases/pass_jobs_number`
+jobs_number=`wc -l cases/jobs_number|awk '{print $1}'`
+pass_jobs_number=`wc -l cases/pass_jobs_number|awk '{print $1}'`
 echo "jobs number is: $jobs_number"
 echo "pass jobs number is: $pass_jobs_number"
 
@@ -96,5 +101,9 @@ echo $case_results
 echo $manager_ut
 
 echo $iengine_ut
+
+pip3 install argparse, git
+
+env
 
 python3 ../build/feishu_notice.py --branch $CURRENT_BRANCH --runner "OP 版本每夜自动化测试" --detail_url "${server_url}/${repository}/actions/runs/${run_id}" --token ${GITHUB_TOKEN} --job_id ${run_id} --app_id ${FEISHU_APP_ID} --person_in_charge ${FEISHU_PERSON_IN_CHARGE} --app_secret ${FEISHU_APP_SECRET} --chat_id gf9b5g97 --message_type night_build_notice --message '{"pass":'$pass',"ut_sum":100,"ut_pass":10,"it_sum":'$jobs_number',"it_pass":'$pass_jobs_number',"build_result":"通过","start_result":"成功","its":['$case_results']}'
