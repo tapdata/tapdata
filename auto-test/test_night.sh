c1b2070b188257dc68760a73679479166e7aaf7e
Berry <xbsura@gmail.com>
2022-12-15 00:24:49 +0800

    fix: add ut case

--- auto-test/test_night.sh
+++ auto-test/test_night.sh
@@ -122,4 +122,7 @@ pip3 install argparse, git
 
 env
 
-python3 ../build/feishu_notice.py --branch $CURRENT_BRANCH --runner "OP 版本每夜自动化测试" --detail_url "${server_url}/${repository}/actions/runs/${run_id}" --token ${GITHUB_TOKEN} --job_id ${run_id} --app_id ${FEISHU_APP_ID} --person_in_charge ${FEISHU_PERSON_IN_CHARGE} --app_secret ${FEISHU_APP_SECRET} --chat_id gf9b5g97 --message_type night_build_notice --message '{"pass":'$pass',"ut_sum":'$ut_sum',"ut_pass":'$ut_pass',"it_sum":'$jobs_number',"it_pass":'$pass_jobs_number',"build_result":"通过","start_result":"成功","its":['$case_results']}'
+message='{"pass":'$pass',"ut_sum":'$ut_sum',"ut_pass":'$ut_pass',"it_sum":'$jobs_number',"it_pass":'$pass_jobs_number',"build_result":"通过","start_result":"成功","its":['$case_results']}'
+echo $message
+
+python3 ../build/feishu_notice.py --branch $CURRENT_BRANCH --runner "OP 版本每夜自动化测试" --detail_url "${server_url}/${repository}/actions/runs/${run_id}" --token ${GITHUB_TOKEN} --job_id ${run_id} --app_id ${FEISHU_APP_ID} --person_in_charge ${FEISHU_PERSON_IN_CHARGE} --app_secret ${FEISHU_APP_SECRET} --chat_id gf9b5g97 --message_type night_build_notice --message $message
