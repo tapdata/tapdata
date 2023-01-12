406550dbbcca2e3e64a7d00ef72cccd64b4b8494
Aplomb <aplomb.chen@gmail.com>
2023-01-12 11:46:58 +0800

    fix(Engine): Optimize performance (#403)
    1, cache jet job status for 2 seconds as getJobStatus has performance
    issue.

---
+++ iengine/iengine-app/src/main/test/cache/TapCacheTest.java
@@ -0,0 +1,42 @@
+package cache;
+
+import io.tapdata.flow.engine.V2.util.TapCache;
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.function.Supplier;
+
+/**
+ * @author aplomb
+ */
+public class TapCacheTest {
+	@Test
+	public void test() {
+		AtomicBoolean switcher = new AtomicBoolean(true);
+		TapCache<Boolean> cache = new TapCache<Boolean>().expireTime(1000L).disableCacheValue(false).supplier(switcher::get);
+
+		for(int i = 0; i < 100; i++) {
+			cache.get();
+		}
+		assert cache.getCacheHit() == 99;
+		assert cache.getCacheMiss() == 1;
+		assert cache.getCacheDisabled() == 0;
+
+		cache = new TapCache<Boolean>().expireTime(1000L).disableCacheValue(false).supplier(switcher::get);
+		for(int i = 0; i < 100; i++) {
+			cache.get();
+			if(i == 80) {
+				switcher.set(false);
+				try {
+					Thread.sleep(1010L);
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+		assert cache.getCacheHit() == 80;
+		assert cache.getCacheMiss() == 20;
+		assert cache.getCacheDisabled() == 19;
+	}
+}
