3503cc622446a3b8b5d3f0065c19821f674feb88
Gavin <2749984520@qq.com>
2023-01-30 17:33:55 +0800

    feat(js-connector):support insert_record/update_record/delete_record for write record.

--- connectors-javascript/fly-book-connector/src/main/javascript/io/tapdata/demo/connector/connector.js
+++ connectors-javascript/fly-book-connector/src/main/javascript/io/tapdata/demo/connector/connector.js
@@ -6,7 +6,8 @@ function connection_test(connectionConfig) {
     return [{"test": "Test App ID and App Secret", "code": result.result.code === 0 ? 1 : -1 , "result": result.result.code === 0 ? "Pass" : "those App ID and App Secret are invalid. "}] ;
 }
 var receiveOpenIdMap = {};
-function write_record(connectionConfig, nodeConfig, eventDataList) {
+function insert_record(connectionConfig, nodeConfig, eventDataList) {
+    let succeedDataArr = [];
     for (let index = 0; index < eventDataList.length; index++){
         let event = eventDataList[index].after_data;
         if ('undefined' == event.phone || null == event.phone) log.error('Receive user\'s open id cannot be empty.please make sure param [receiveId] is useful.');
@@ -15,18 +16,18 @@ function write_record(connectionConfig, nodeConfig, eventDataList) {
             let receiveIdData = invoker.invoke("GetOpenIdByPhone", {'userPhone': [event.phone]});
             receiveId = receiveIdData.result.data.user_list[0].user_id;
             if ('undefined' == receiveId || null == receiveId) {            //用户：{{phone}}, 这位用户不在应用的可见范围中，请确保应用的此用户在当前版本下可见，您可在应用版本管理与发布中查看最新版本下的可见范围，如有必要请在创建新的版本并将此用户添加到可见范围。
-                log.warn(' User: ' + phone + ', this user is not in the visible range of the application. Please ensure that this user of the application is visible under the current version. You can view the visible range under the latest version in the application version management and release. If necessary, create a new version and add this user to the visible range. ' + ',message is: ' + connect);
+                log.warn(' User: {}, this user is not in the visible range of the application. Please ensure that this user of the application is visible under the current version. You can view the visible range under the latest version in the application version management and release. If necessary, create a new version and add this user to the visible range, message is: {}',event.phone,event.content);
                 continue;
             }
             receiveOpenIdMap[event.phone] = receiveId;
         }
         if ('undefined' == event.content || null == event.content) log.error('receive message cannot be empty. please make sure param [connect] is useful.');
-        invoker.invoke("flyBookSendMessage",{"content": event.content, "receive_id": receiveId});
+        let writeResult = invoker.invoke("flyBookSendMessage",{"content": event.content, "receive_id": receiveId});
+        if (writeResult.result.code === 0) succeedDataArr.push(eventDataList[index]);
     }
-    return true;
+    return succeedDataArr;
 }
 function update_token(connectionConfig, nodeConfig, apiResponse) {
-    log.warn(''+apiResponse.result.code);
     if (apiResponse.result.code != 99991663 && apiResponse.result.code != 99991661) return null;
     let result = invoker.invokeWithoutIntercept("GetAppToken");
     if (result.result.code === 0) return {"token": result.result.tenant_access_token};
