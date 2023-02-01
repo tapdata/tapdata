95c054d78bc562598bb117c6f97f1ef24fe735ee
Berry <xbsura@gmail.com>
2023-02-01 19:26:10 +0800

    fix: doris batchsize 不生效

--- connectors/doris-connector/src/main/java/io/tapdata/connector/doris/streamload/Constants.java
+++ connectors/doris-connector/src/main/java/io/tapdata/connector/doris/streamload/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
     public static final String LINE_DELIMITER_DEFAULT = "\n";
     public static final String DORIS_DELETE_SIGN = "__DORIS_DELETE_SIGN__";
     public static final String NULL_VALUE = "\\N";
-    public static final int CACHE_BUFFER_SIZE = 256 * 1024;
+    public static final int CACHE_BUFFER_SIZE = 256 * 1024 * 1024;
     public static final int CACHE_BUFFER_COUNT = 3;
 
     public static void main(String[] args) {
