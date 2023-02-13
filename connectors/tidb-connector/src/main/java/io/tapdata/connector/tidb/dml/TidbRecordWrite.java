f9dcd379155fbac3054d575c3966653158ea3137
yujie <1076132789@qq.com>
2023-02-13 19:47:43 +0800

    feat(tidb-connector):tidb as source and target.

--- connectors/tidb-connector/src/main/java/io/tapdata/connector/tidb/dml/TidbRecordWrite.java
+++ connectors/tidb-connector/src/main/java/io/tapdata/connector/tidb/dml/TidbRecordWrite.java
@@ -2,9 +2,11 @@ package io.tapdata.connector.tidb.dml;
 
 import io.tapdata.common.JdbcContext;
 import io.tapdata.common.RecordWriter;
+import io.tapdata.connector.tidb.TidbJdbcRunner;
 import io.tapdata.entity.schema.TapTable;
 
 import java.sql.SQLException;
+import java.util.Collections;
 
 /**
  * @author lemon
@@ -16,5 +18,8 @@ public class TidbRecordWrite extends RecordWriter {
         insertRecorder = new TidbWriteRecorder(connection, tapTable, jdbcContext.getConfig().getDatabase());
         updateRecorder = new TidbWriteRecorder(connection, tapTable, jdbcContext.getConfig().getDatabase());
         deleteRecorder = new TidbWriteRecorder(connection, tapTable, jdbcContext.getConfig().getDatabase());
+    }
+    private boolean makeSureHasUnique(TidbJdbcRunner jdbcContext, TapTable tapTable) {
+        return jdbcContext.queryAllIndexes(Collections.singletonList(tapTable.getId())).stream().anyMatch(v -> (boolean) v.get("is_unique"));
     }
 }
