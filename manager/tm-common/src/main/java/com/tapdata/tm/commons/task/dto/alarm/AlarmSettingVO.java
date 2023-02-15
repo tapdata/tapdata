fecdf36c5056d2d8cdc9456c962667437229c23f
jiuyetx <meteor209@gmail.com>
2023-02-15 19:40:45 +0800

    fix alarm set

---
+++ manager/tm-common/src/main/java/com/tapdata/tm/commons/task/dto/alarm/AlarmSettingVO.java
@@ -0,0 +1,31 @@
+package com.tapdata.tm.commons.task.dto.alarm;
+
+import cn.hutool.core.date.DateUnit;
+import com.tapdata.tm.commons.base.dto.BaseDto;
+import com.tapdata.tm.commons.task.constant.AlarmKeyEnum;
+import com.tapdata.tm.commons.task.constant.AlarmSettingTypeEnum;
+import com.tapdata.tm.commons.task.constant.NotifyEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author jiuyetx
+ * @date 2022/9/5
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AlarmSettingVO implements Serializable {
+    private AlarmSettingTypeEnum type;
+    private boolean open;
+    private AlarmKeyEnum key;
+    private int sort;
+    private List<NotifyEnum> notify;
+    private int interval;
+    private DateUnit unit;
+}
