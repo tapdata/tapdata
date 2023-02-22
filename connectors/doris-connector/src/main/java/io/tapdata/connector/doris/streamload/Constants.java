2cb8e39a36114005b79d09901ad1ca127837241e
jarad0628 <coolgj@163.com>
2023-02-22 20:11:41 +0800

    Fix v2.13 from2.12 (#505)
    * fix: doris batchsize 不生效
    
    * fix: 修复 LINE_DELIMITER_DEFAULT
    
    * fix: KingbaseR6Test remove hba.conf test (#483)
    
    * fix: mysql 测试连接阻塞加载 schema
    
    * feat: 增加 doris 重试配置 (#491)
    
    * fix: big transaction chronicleMap not linked map (#498)
    
    ---------
    
    Co-authored-by: Berry <xbsura@gmail.com>
    Co-authored-by: Berry <berry@tapdata.io>

--- connectors/doris-connector/src/main/java/io/tapdata/connector/doris/streamload/Constants.java
+++ connectors/doris-connector/src/main/java/io/tapdata/connector/doris/streamload/Constants.java
@@ -6,10 +6,10 @@ package io.tapdata.connector.doris.streamload;
  */
 public class Constants {
     public static final String FIELD_DELIMITER_DEFAULT = "||%%||";
-    public static final String LINE_DELIMITER_DEFAULT = "\n";
+    public static final String LINE_DELIMITER_DEFAULT = "||__||";
     public static final String DORIS_DELETE_SIGN = "__DORIS_DELETE_SIGN__";
     public static final String NULL_VALUE = "\\N";
-    public static final int CACHE_BUFFER_SIZE = 256 * 1024;
+    public static final int CACHE_BUFFER_SIZE = 256 * 1024 * 1024;
     public static final int CACHE_BUFFER_COUNT = 3;
 
     public static void main(String[] args) {
