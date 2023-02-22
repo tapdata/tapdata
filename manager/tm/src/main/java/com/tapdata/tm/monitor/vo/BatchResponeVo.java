02b6ff5603a531482a7ecd30d3192b3be8b40aa8
jiuyetx <meteor209@gmail.com>
2023-02-22 14:47:44 +0800

    update batch method

--- manager/tm/src/main/java/com/tapdata/tm/monitor/vo/BatchResponeVo.java
+++
@@ -1,9 +0,0 @@
-package com.tapdata.tm.monitor.vo;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-import java.util.HashMap;
-
-@Schema(description = "批请求 响应Vo")
-public class BatchResponeVo extends HashMap<String, Object> {
-}
