46f7c982e08e734b3991f4703a7d09138b991937
yujie <1076132789@qq.com>
2023-02-27 15:01:27 +0800

    fix(TDsql mysql): FIX TDsql  mysql

--- connectors/tencent-db-mysql-connector/src/main/java/io/tapdata/connector/tencent/db/mysql/TencentDBMySQLConnector.java
+++ connectors/tencent-db-mysql-connector/src/main/java/io/tapdata/connector/tencent/db/mysql/TencentDBMySQLConnector.java
@@ -1,5 +1,7 @@
 package io.tapdata.connector.tencent.db.mysql;
 
+import io.tapdata.common.CommonDbConfig;
+import io.tapdata.connector.mysql.MysqlConnectionTest;
 import io.tapdata.connector.mysql.MysqlConnector;
 import io.tapdata.pdk.apis.annotations.TapConnectorClass;
 import io.tapdata.pdk.apis.context.TapConnectionContext;
@@ -15,16 +17,28 @@ import java.util.function.Consumer;
  **/
 @TapConnectorClass("tencent-db-mysql-spec.json")
 public class TencentDBMySQLConnector extends MysqlConnector {
+	private TapConnectionContext tapConnectionContext;
 
 	@Override
 	public void onStart(TapConnectionContext tapConnectionContext) throws Throwable {
+		this.tapConnectionContext=tapConnectionContext;
 		tapConnectionContext.getConnectionConfig().put("protocolType", "mysql");
 		super.onStart(tapConnectionContext);
 	}
 
 	@Override
 	public ConnectionOptions connectionTest(TapConnectionContext databaseContext, Consumer<TestItem> consumer) {
+
 		databaseContext.getConnectionConfig().put("protocolType", "mysql");
-		return super.connectionTest(databaseContext, consumer);
+		ConnectionOptions connectionOptions = ConnectionOptions.create();
+		CommonDbConfig commonDbConfig = new CommonDbConfig();
+		commonDbConfig.set__connectionType(databaseContext.getConnectionConfig().getString("__connectionType"));
+		try (
+				TencentDBMySQLConnectorTest tencentDBMySQLConnectorTest = new TencentDBMySQLConnectorTest(new MysqlJdbcContext(databaseContext),
+						databaseContext, consumer, commonDbConfig, connectionOptions)
+		) {
+			tencentDBMySQLConnectorTest.testOneByOne();
+			return connectionOptions;
+		}
 	}
 }
