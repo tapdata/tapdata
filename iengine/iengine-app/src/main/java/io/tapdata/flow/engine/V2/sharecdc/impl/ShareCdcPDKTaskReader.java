82390bb2bf895cbab9dc4e7cd0bf5d9f0e36ba5b
sam.p <102567930@qq.com>
2023-03-02 21:29:38 +0800

    fix(iengine): 修复共享挖掘，有可能丢失断点信息的问题

--- iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/sharecdc/impl/ShareCdcPDKTaskReader.java
+++ iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/sharecdc/impl/ShareCdcPDKTaskReader.java
@@ -53,6 +53,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -251,8 +252,11 @@ public class ShareCdcPDKTaskReader extends ShareCdcHZReader implements Serializa
 					partitionTableName,
 					shareCdcContext
 			);
-			this.readThreadPool.submit(readRunner::read);
-			this.readRunners.add(readRunner);
+			try {
+				this.readThreadPool.submit(readRunner::read);
+				this.readRunners.add(readRunner);
+			} catch (RejectedExecutionException ignored) {
+			}
 		}
 		try {
 			poll(logContentConsumer);
