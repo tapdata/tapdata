22f9cf613b624d3bcc0a9f5cac622b78c6170077
shuuufu <shu_fu1201@163.com>
2023-03-16 11:05:21 +0800

    feat: datasource add definition tags

--- manager/tm-common/src/main/java/com/tapdata/tm/commons/schema/DataSourceConnectionDto.java
+++ manager/tm-common/src/main/java/com/tapdata/tm/commons/schema/DataSourceConnectionDto.java
@@ -43,6 +43,7 @@ public class DataSourceConnectionDto extends BaseDto {
         private String definitionGroup;
         private String definitionPdkId;
         private String definitionBuildNumber;
+        private List<String> definitionTags;
 
         /**
          * 用户区分是不是pdk数据源类型
