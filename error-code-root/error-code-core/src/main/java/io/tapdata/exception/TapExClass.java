fee8764c6966f4d6df12b8011822d9b08e91aaae
sam.p <102567930@qq.com>
2023-03-19 17:56:03 +0800

    feat(ErrorCode): Optimize error code
    1. scan error code;
    2. add module prefix in error code.

--- error-code-root/error-code-core/src/main/java/io/tapdata/exception/TapExClass.java
+++ error-code-root/error-code-core/src/main/java/io/tapdata/exception/TapExClass.java
@@ -9,6 +9,7 @@ import java.lang.annotation.Target;
 
 /**
  * Use on exception codes class
+ *
  * @author <a href="mailto:harsen_lin@163.com">Harsen</a>
  * @version v1.0 2022/12/13 15:12 Create
  */
@@ -17,7 +18,11 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface TapExClass {
-    int code();
-    String module();
-    String describe() default "";
+	int code();
+
+	String module();
+
+	String describe() default "";
+
+	String prefix() default "";
 }
