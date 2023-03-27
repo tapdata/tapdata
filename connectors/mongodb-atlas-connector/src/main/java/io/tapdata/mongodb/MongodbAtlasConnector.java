a062c0ff0b745b4a6e0239488d829950bb15b2a2
DGshikun <1754978623@qq.com>
2023-03-27 16:49:51 +0800

    feat: MongodbAtlasConnector

--- connectors/mongodb-atlas-connector/src/main/java/io/tapdata/mongodb/MongodbAtlasConnector.java
+++ connectors/mongodb-atlas-connector/src/main/java/io/tapdata/mongodb/MongodbAtlasConnector.java
@@ -1,6 +1,7 @@
 package io.tapdata.mongodb;
 
 import io.tapdata.entity.logger.TapLogger;
+import io.tapdata.mongodb.atlasTest.MongodbAtlasTest;
 import io.tapdata.pdk.apis.annotations.TapConnectorClass;
 import io.tapdata.pdk.apis.context.TapConnectionContext;
 import io.tapdata.pdk.apis.entity.ConnectionOptions;
@@ -18,7 +19,19 @@ public class MongodbAtlasConnector extends MongodbConnector {
     @Override
     public ConnectionOptions connectionTest(TapConnectionContext connectionContext, Consumer<TestItem> consumer) throws Throwable {
         ConnectionOptions connectionOptions = ConnectionOptions.create();
-
+        try {
+            onStart(connectionContext);
+            try (
+                    MongodbAtlasTest mongodbAtlasTest = new MongodbAtlasTest(mongoConfig, consumer,mongoClient)
+            ) {
+                mongodbAtlasTest.testOneByOne();
+            }
+        } catch (Throwable throwable) {
+            TapLogger.error(TAG,throwable.getMessage());
+            consumer.accept(testItem(TestItem.ITEM_CONNECTION, TestItem.RESULT_FAILED, "Failed, " + throwable.getMessage()));
+        } finally {
+            onStop(connectionContext);
+        }
         return connectionOptions;
     }
 }
