95d7ff0f07e0e74f94904e94a3fddd0f4b3111e3
jarad <coolgj@163.com>
2023-03-31 16:06:04 +0800

    fix: #137635 csv target and source nodeConfig

--- connectors/csv-connector/src/main/java/io/tapdata/connector/csv/config/CsvConfig.java
+++ connectors/csv-connector/src/main/java/io/tapdata/connector/csv/config/CsvConfig.java
@@ -4,18 +4,8 @@ import io.tapdata.common.FileConfig;
 
 public class CsvConfig extends FileConfig {
 
-    private String delimiter;
-
     public CsvConfig() {
         setFileType("csv");
-    }
-
-    public String getDelimiter() {
-        return delimiter;
-    }
-
-    public void setDelimiter(String delimiter) {
-        this.delimiter = delimiter;
     }
 
 }
