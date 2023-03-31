37e54b291f04f41fe217af4f085ad9708a87ce5f
Devin <Devin@tapdata.io>
2023-03-31 17:20:52 +0800

    feat: Submit a new test file

--- auto-test/cases/test_dev_all.py
+++ auto-test/cases/test_dev_all.py
@@ -1,3 +1,3 @@
-# desc: 一个源到目标的无任何数据处理的数据开发任务
+# desc: 一个源到目标的无任何数据处理的全量+增量数据开发任务
 def test(p, car_claim, car_claim_sink):
     p.readFrom(car_claim).writeTo(car_claim_sink)
---
+++ auto-test/cases/test_dev_sy.py
@@ -0,0 +1,6 @@
+# desc: 一个源到目标的无任何数据处理的全量数据开发任务
+
+
+def test(p, car_claim, car_claim_sink):
+    p.config({"type": "initial_sync"})
+    p.readFrom(car_claim).writeTo(car_claim_sink)
