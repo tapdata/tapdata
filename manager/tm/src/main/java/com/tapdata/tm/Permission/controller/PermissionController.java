b8584b3e48f82ddd0013c23e49c710877fd6cba0
Harvey <Zerohyuan@163.com>
2023-04-01 17:27:25 +0800

    fix:#138377 Added internationalization to role management

--- manager/tm/src/main/java/com/tapdata/tm/Permission/controller/PermissionController.java
+++ manager/tm/src/main/java/com/tapdata/tm/Permission/controller/PermissionController.java
@@ -11,14 +11,24 @@ import com.tapdata.tm.Permission.service.PermissionService;
 import com.tapdata.tm.base.controller.BaseController;
 import com.tapdata.tm.base.dto.Filter;
 import com.tapdata.tm.base.dto.ResponseMessage;
+import com.tapdata.tm.utils.MessageUtil;
+import com.tapdata.tm.utils.WebUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import java.util.Locale;
 
 @Tag(name = "Permission", description = "Permission api")
 @RestController
@@ -31,10 +41,21 @@ public class PermissionController extends BaseController {
 	@Operation(summary = "Find all permissions of the model matched by filter from the data source")
 	@GetMapping
 	public ResponseMessage<List<PermissionEntity>> find(
-			@Parameter(in = ParameterIn.QUERY, description = "Filter defining fields, where, sort, skip, and limit - must be a JSON-encoded string (`{\"where\":{\"something\":\"value\"},\"field\":{\"something\":true|false},\"sort\": [\"name desc\"],\"page\":1,\"size\":20}`).")
-			@RequestParam(value = "filter", required = false) String filterJson) {
+					@Parameter(in = ParameterIn.QUERY, description = "Filter defining fields, where, sort, skip, and limit - must be a JSON-encoded string (`{\"where\":{\"something\":\"value\"},\"field\":{\"something\":true|false},\"sort\": [\"name desc\"],\"page\":1,\"size\":20}`).")
+					@RequestParam(value = "filter", required = false) String filterJson, HttpServletRequest request) {
 		Filter filter = parseFilter(filterJson);
-		return success(permissionService.find(filter));
+		List<PermissionEntity> entities = permissionService.find(filter);
+		if (CollectionUtils.isNotEmpty(entities)) {
+			Locale locale = WebUtils.getLocale(request);
+			entities.forEach(e -> {
+				String message = MessageUtil.getMessage(locale, e.getName());
+				if (!StringUtils.equals(message, e.getName())) {
+					e.setDescription(message);
+				}
+			});
+
+		}
+		return success(entities);
 	}
 
 	@Operation(summary = "Count instances of the model matched by where from the data source")
