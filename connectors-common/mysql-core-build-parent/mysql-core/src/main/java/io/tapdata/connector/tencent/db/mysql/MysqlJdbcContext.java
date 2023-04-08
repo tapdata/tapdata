2613c0888e1a4eb1c8c5d7650e02223dc61f3319
jarad <coolgj@163.com>
2023-04-09 00:55:15 +0800

    fix: mariadb timezone bug

--- connectors-common/mysql-core-build-parent/mysql-core/src/main/java/io/tapdata/connector/tencent/db/mysql/MysqlJdbcContext.java
+++ connectors-common/mysql-core-build-parent/mysql-core/src/main/java/io/tapdata/connector/tencent/db/mysql/MysqlJdbcContext.java
@@ -13,7 +13,6 @@ import io.tapdata.pdk.apis.context.TapConnectionContext;
 import org.apache.commons.lang3.StringUtils;
 
 import java.sql.*;
-import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -133,17 +132,10 @@ public class MysqlJdbcContext implements AutoCloseable {
 		String timezone = connectionConfig.getString("timezone");
 		if (StringUtils.isNotBlank(timezone)) {
 			try {
-				ZoneId.of(timezone);
 				timezone = "GMT" + timezone;
 				String serverTimezone = timezone.replace("+", "%2B").replace(":00", "");
 				properties.put("serverTimezone", serverTimezone);
 			} catch (Exception ignored) {
-			}
-		}else {
-			try {
-				properties.put("serverTimezone", this.timezone());
-			} catch (Exception e) {
-				throw new RuntimeException(e);
 			}
 		}
 		StringBuilder propertiesString = new StringBuilder();
