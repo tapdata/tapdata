9daf3622edfd28eea19d6406fcd419b6ab57bcad
Gavin <2749984520@qq.com>
2023-04-13 23:13:37 +0800

    fix(td-sql):ignore shardKey on partition table for discover schema and batch read.

--- connectors/tencent-db-mysql-connector/src/main/java/io/tapdata/connector/tencent/db/core/TDSqlDiscoverSchema.java
+++ connectors/tencent-db-mysql-connector/src/main/java/io/tapdata/connector/tencent/db/core/TDSqlDiscoverSchema.java
@@ -35,58 +35,58 @@ public class TDSqlDiscoverSchema extends MysqlSchemaLoader {
         super(mysqlJdbcContext);
     }
 
-    @Override
-    public void discoverSchema(List<String> filterTable, Consumer<List<TapTable>> consumer, int tableSize) throws Throwable {
-        if (null == consumer) {
-            throw new IllegalArgumentException("Consumer cannot be null");
-        }
-
-        DataMap connectionConfig = tapConnectionContext.getConnectionConfig();
-        String database = connectionConfig.getString("database");
-        List<DataMap> allTables = queryAllTables(database, filterTable);
-        if (CollectionUtils.isEmpty(allTables)) {
-            consumer.accept(null);
-            return;
-        }
-
-        TableFieldTypesGenerator instance = InstanceFactory.instance(TableFieldTypesGenerator.class);
-        DefaultExpressionMatchingMap dataTypesMap = tapConnectionContext.getSpecification().getDataTypesMap();
-
-        try {
-            List<List<DataMap>> tableLists = Lists.partition(allTables, tableSize);
-            tableLists.forEach(tableList -> {
-                List<String> subTableNames = tableList.stream().map(v -> v.getString("TABLE_NAME")).collect(Collectors.toList());
-                String tableNames = StringUtils.join(subTableNames, "','");
-                List<DataMap> columnList = queryAllColumns(database, tableNames);
-                List<DataMap> indexList = queryAllIndexes(database, tableNames);
-
-                Map<String, List<DataMap>> columnMap = columnList.stream().collect(Collectors.groupingBy(t -> t.getString("TABLE_NAME")));
-                Map<String, List<DataMap>> indexMap = indexList.stream().collect(Collectors.groupingBy(t -> t.getString("TABLE_NAME")));
-
-                List<TapTable> tempList = new ArrayList<>();
-                tableList.forEach(subTable -> {
-                    String tableName = subTable.getString("TABLE_NAME");
-                    TapTable tapTable = TapSimplify.table(tableName);
-                    if (columnMap.containsKey(tableName)) {
-                        discoverFields(columnMap.get(tableName), tapTable, instance, dataTypesMap);
-                    }
-                    if (indexMap.containsKey(tableName)) {
-                        tapTable.setIndexList(discoverIndexes(indexMap.get(tableName), tableName));
-                    }
-                    tapTable.setComment(subTable.getString("TABLE_COMMENT"));
-                    tempList.add(tapTable);
-                });
-
-                if (CollectionUtils.isNotEmpty(tempList)) {
-                    consumer.accept(tempList);
-                    tempList.clear();
-                }
-            });
-
-        } catch (Exception e) {
-            throw new CoreException(e.getMessage());
-        }
-    }
+//    @Override
+//    public void discoverSchema(List<String> filterTable, Consumer<List<TapTable>> consumer, int tableSize) throws Throwable {
+//        if (null == consumer) {
+//            throw new IllegalArgumentException("Consumer cannot be null");
+//        }
+//
+//        DataMap connectionConfig = tapConnectionContext.getConnectionConfig();
+//        String database = connectionConfig.getString("database");
+//        List<DataMap> allTables = queryAllTables(database, filterTable);
+//        if (CollectionUtils.isEmpty(allTables)) {
+//            consumer.accept(null);
+//            return;
+//        }
+//
+//        TableFieldTypesGenerator instance = InstanceFactory.instance(TableFieldTypesGenerator.class);
+//        DefaultExpressionMatchingMap dataTypesMap = tapConnectionContext.getSpecification().getDataTypesMap();
+//
+//        try {
+//            List<List<DataMap>> tableLists = Lists.partition(allTables, tableSize);
+//            tableLists.forEach(tableList -> {
+//                List<String> subTableNames = tableList.stream().map(v -> v.getString("TABLE_NAME")).collect(Collectors.toList());
+//                String tableNames = StringUtils.join(subTableNames, "','");
+//                List<DataMap> columnList = queryAllColumns(database, tableNames);
+//                List<DataMap> indexList = queryAllIndexes(database, tableNames);
+//
+//                Map<String, List<DataMap>> columnMap = columnList.stream().collect(Collectors.groupingBy(t -> t.getString("TABLE_NAME")));
+//                Map<String, List<DataMap>> indexMap = indexList.stream().collect(Collectors.groupingBy(t -> t.getString("TABLE_NAME")));
+//
+//                List<TapTable> tempList = new ArrayList<>();
+//                tableList.forEach(subTable -> {
+//                    String tableName = subTable.getString("TABLE_NAME");
+//                    TapTable tapTable = TapSimplify.table(tableName);
+//                    if (columnMap.containsKey(tableName)) {
+//                        discoverFields(columnMap.get(tableName), tapTable, instance, dataTypesMap);
+//                    }
+//                    if (indexMap.containsKey(tableName)) {
+//                        tapTable.setIndexList(discoverIndexes(indexMap.get(tableName), tableName));
+//                    }
+//                    tapTable.setComment(subTable.getString("TABLE_COMMENT"));
+//                    tempList.add(tapTable);
+//                });
+//
+//                if (CollectionUtils.isNotEmpty(tempList)) {
+//                    consumer.accept(tempList);
+//                    //tempList.clear();
+//                }
+//            });
+//
+//        } catch (Exception e) {
+//            throw new CoreException(e.getMessage());
+//        }
+//    }
 
     private List<String> fullPartitionKey(String partitionRegex){
         List<String> keys = new ArrayList<>();
@@ -115,6 +115,7 @@ public class TDSqlDiscoverSchema extends MysqlSchemaLoader {
             Boolean partitionKey = (Boolean)dataMap.get("IS_PARTITION_EXPRESSION");
             TapField field = TapSimplify.field(columnName, columnType);
             field.setPartitionKey(partitionKey);
+            field.setComment(Optional.ofNullable(partitionKey).orElse(false) ? "IS_PARTITION_KEY" : "NOT_PARTITION_KEY");
 
             tableFieldTypesGenerator.autoFill(field, dataTypesMap);
 
@@ -166,6 +167,8 @@ public class TDSqlDiscoverSchema extends MysqlSchemaLoader {
                         List<String> partitionNameArr = fullPartitionKey(partitionName);
                         String columnName = dataMap.getString("COLUMN_NAME");
                         dataMap.put("IS_PARTITION_EXPRESSION", partitionNameArr.contains(columnName));
+                    }else {
+                        dataMap.put("IS_PARTITION_EXPRESSION", false);
                     }
                     columnList.add(dataMap);
                 }
--- connectors/tencent-db-mysql-connector/src/main/java/io/tapdata/connector/tencent/db/core/TDSqlJdbcOneByOneWriter.java
+++ connectors/tencent-db-mysql-connector/src/main/java/io/tapdata/connector/tencent/db/core/TDSqlJdbcOneByOneWriter.java
@@ -3,12 +3,14 @@ package io.tapdata.connector.tencent.db.core;
 import io.tapdata.connector.mysql.writer.MysqlJdbcOneByOneWriter;
 import io.tapdata.connector.tencent.db.mysql.MysqlJdbcContext;
 import io.tapdata.entity.event.dml.TapRecordEvent;
+import io.tapdata.entity.logger.TapLogger;
 import io.tapdata.entity.schema.TapField;
+import io.tapdata.entity.schema.TapTable;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.sql.PreparedStatement;
+import java.util.*;
 
 /**
  * @author GavinXiao
@@ -16,6 +18,7 @@ import java.util.Map;
  * @create 2023/4/13 17:19
  **/
 public class TDSqlJdbcOneByOneWriter extends MysqlJdbcOneByOneWriter {
+    public static final String TAG = TDSqlJdbcOneByOneWriter.class.getSimpleName();
     public TDSqlJdbcOneByOneWriter(MysqlJdbcContext mysqlJdbcContext, Object jdbcCacheMap) throws Throwable {
         super(mysqlJdbcContext, jdbcCacheMap);
     }
@@ -27,10 +30,41 @@ public class TDSqlJdbcOneByOneWriter extends MysqlJdbcOneByOneWriter {
             if (!needAddIntoPreparedStatementValues(field, tapRecordEvent)) {
                 return;
             }
-            if (null == field.getPartitionKey() || !field.getPartitionKey()) {
+//            if (null == field.getPartitionKey() || !field.getPartitionKey()) {
+//                setList.add("`" + fieldName + "`=?");
+//            }
+            if (null == field.getComment() || !"IS_PARTITION_KEY".equals(field.getComment())) {
                 setList.add("`" + fieldName + "`=?");
             }
         });
         return setList;
+    }
+
+    @Override
+    protected int setPreparedStatementValues(TapTable tapTable, TapRecordEvent tapRecordEvent, PreparedStatement preparedStatement) throws Throwable {
+        LinkedHashMap<String, TapField> nameFieldMap = tapTable.getNameFieldMap();
+        int parameterIndex = 1;
+        Map<String, Object> after = getAfter(tapRecordEvent);
+        if (MapUtils.isEmpty(after)) {
+            throw new Exception("Set prepared statement values failed, after is empty: " + tapRecordEvent);
+        }
+        List<String> afterKeys = new ArrayList<>(after.keySet());
+        for (String fieldName : nameFieldMap.keySet()) {
+            TapField tapField = nameFieldMap.get(fieldName);
+            if (null == tapField || (null != tapField.getComment() && "IS_PARTITION_KEY".equals(tapField.getComment()))) {
+                continue;
+            }
+            if (!needAddIntoPreparedStatementValues(tapField, tapRecordEvent)) {
+                continue;
+            }
+            preparedStatement.setObject(parameterIndex++, after.get(fieldName));
+            afterKeys.remove(fieldName);
+        }
+        if (CollectionUtils.isNotEmpty(afterKeys)) {
+            Map<String, Object> missingAfter = new HashMap<>();
+            afterKeys.forEach(k -> missingAfter.put(k, after.get(k)));
+            TapLogger.warn(TAG, "Found fields in after data not exists in schema fields, will skip it: " + missingAfter);
+        }
+        return parameterIndex;
     }
 }
