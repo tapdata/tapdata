b2f24a97916c50dec7447fdcb1042d2b8cd39a6f
sam.p <102567930@qq.com>
2023-04-14 12:07:11 +0800

    modify RegisterMain

--- tapdata-cli/src/main/java/io/tapdata/pdk/cli/RegisterMain.java
+++ tapdata-cli/src/main/java/io/tapdata/pdk/cli/RegisterMain.java
@@ -112,7 +112,7 @@ public class RegisterMain {
 //        String server = System.getProperty("server", "https://uat.cloud.tapdata.net:9443/tm");
         String server = System.getProperty("server", "http://localhost:3000");
         //String server = System.getProperty("server", "http://192.168.1.189:30205");
-        Collections.addAll(postList, "register", "-a", "3324cfdf-7d3e-4792-bd32-571638d4562f", "-ak", "gmA3K4lt3gG6VyXkwAfyLyYBubeUcucU", "-sk", "ylKkPytK7R9834e7BPVbizGrDKroKSzS", "-t", server);
+        Collections.addAll(postList, "register", "-a", "3324cfdf-7d3e-4792-bd32-571638d4562f", "-ak", "", "-sk", "", "-t", server);
         String[] tags = System.getProperty("tags", "all").split(",");
         ConnectorEnums.addByTags(postList, tags);
         Main.registerCommands().execute(postList.toArray(new String[0]));
