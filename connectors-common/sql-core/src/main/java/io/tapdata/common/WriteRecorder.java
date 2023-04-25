b86ebf0ce0c3b71348b6d2e9b42ee9e8824d28ba
jarad <coolgj@163.com>
2023-04-25 17:12:32 +0800

    fix: createTable remove unique index

--- connectors-common/sql-core/src/main/java/io/tapdata/common/WriteRecorder.java
+++ connectors-common/sql-core/src/main/java/io/tapdata/common/WriteRecorder.java
@@ -1,6 +1,8 @@
 package io.tapdata.common;
 
 import io.tapdata.entity.event.dml.TapRecordEvent;
+import io.tapdata.entity.schema.TapIndex;
+import io.tapdata.entity.schema.TapIndexField;
 import io.tapdata.entity.schema.TapTable;
 import io.tapdata.entity.simplify.TapSimplify;
 import io.tapdata.kit.EmptyKit;
@@ -25,7 +27,7 @@ public abstract class WriteRecorder {
     protected final String schema;
     protected List<String> uniqueCondition;
     protected boolean hasPk = false;
-    //    protected boolean uniqueConditionIsIndex = false; //Target table may not have a unique index, used in Postgres
+    protected boolean uniqueConditionIsIndex = false; //Target table may not have a unique index, used in Postgres
     protected String version;
     protected String insertPolicy;
     protected String updatePolicy;
@@ -54,9 +56,9 @@ public abstract class WriteRecorder {
         //2、second priority: analyze table with its indexes
         else {
             uniqueCondition = new ArrayList<>(tapTable.primaryKeys(true));
-//            uniqueConditionIsIndex = EmptyKit.isNotEmpty(tapTable.getIndexList()) && tapTable.getIndexList().stream().filter(TapIndex::isUnique).anyMatch(in ->
-//                    (in.getIndexFields().size() == uniqueCondition.size()) && new HashSet<>(uniqueCondition)
-//                            .containsAll(in.getIndexFields().stream().map(TapIndexField::getName).collect(Collectors.toList())));
+            uniqueConditionIsIndex = EmptyKit.isNotEmpty(tapTable.getIndexList()) && tapTable.getIndexList().stream().filter(TapIndex::isUnique).anyMatch(in ->
+                    (in.getIndexFields().size() == uniqueCondition.size()) && new HashSet<>(uniqueCondition)
+                            .containsAll(in.getIndexFields().stream().map(TapIndexField::getName).collect(Collectors.toList())));
         }
         updatedColumn = allColumn.stream().filter(v -> uniqueCondition.contains(v)).collect(Collectors.toList());
         if (EmptyKit.isEmpty(updatedColumn)) {
