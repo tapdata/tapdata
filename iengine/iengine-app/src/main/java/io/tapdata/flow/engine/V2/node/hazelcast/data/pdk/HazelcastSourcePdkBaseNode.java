17513f2b4634aeaef91ea4cb9d5b8b26a76fd2f2
sam.p <102567930@qq.com>
2023-04-25 18:27:49 +0800

    fix(iengine): #4333 任务停止再启动会导致丢数的问题， mongodb到mongodb， mysql到polar mysql， pg相关的也出现过
    Closes #140054

--- iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastSourcePdkBaseNode.java
+++ iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastSourcePdkBaseNode.java
@@ -88,6 +88,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
