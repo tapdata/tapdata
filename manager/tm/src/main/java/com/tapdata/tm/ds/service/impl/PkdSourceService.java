379d72901a01c6ad8705cad3c0b640494649e31a
jiuyetx <meteor209@gmail.com>
2023-04-27 14:24:19 +0800

    update pdk query sort by pdkAPIBuildNumber

--- manager/tm/src/main/java/com/tapdata/tm/ds/service/impl/PkdSourceService.java
+++ manager/tm/src/main/java/com/tapdata/tm/ds/service/impl/PkdSourceService.java
@@ -198,18 +198,16 @@ public class PkdSourceService {
             case JAR:
                 query.fields().include("jarRid");
                 criteria.and("pdkAPIBuildNumber").lte(pdkBuildNumber);
-                query.with(Sort.by("pdkAPIBuildNumber").descending());
                 break;
             case IMAGE:
                 query.fields().include("icon");
-                query.with(Sort.by("createTime").descending());
                 break;
             case MARKDOWN:
                 query.fields().include("messages");
-                query.with(Sort.by("createTime").descending());
                 break;
             default:
         }
+        query.with(Sort.by("pdkAPIBuildNumber").descending());
 
         DataSourceDefinitionDto one = dataSourceDefinitionService.findOne(query);
 
