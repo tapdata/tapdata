72eb4faf0cd1a67cbb1389c1c0fb6c203312b877
Leon <lirufei0808@gmail.com>
2023-04-28 11:09:31 +0800

    update singletonLock, ignore on cloud full-managements agent

--- iengine/iengine-app/src/main/java/io/tapdata/Schedule/ConnectorManager.java
+++ iengine/iengine-app/src/main/java/io/tapdata/Schedule/ConnectorManager.java
@@ -267,6 +267,13 @@ public class ConnectorManager {
     }*/
 
 		WorkerSingletonLock.check(tapdataWorkDir, (singletonLock) -> {
+			if (StringUtils.isBlank(singletonLock)) {
+				String singletonLockFromEnv = System.getenv("singletonLock");
+				if (StringUtils.isNotBlank(singletonLockFromEnv)) {
+					// Cloud 全托管，永远使用环境变量中提供的 lock
+					return singletonLockFromEnv;
+				}
+			}
 			String newSingletonLock = UUID.randomUUID().toString();
 			String status = clientMongoOperator.upsert(new HashMap<String, Object>() {{
 				put("process_id", instanceNo);
