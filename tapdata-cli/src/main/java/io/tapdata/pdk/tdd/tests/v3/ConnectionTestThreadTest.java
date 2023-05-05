31e10fd3a43f1c638b8a5d11284b458cfe98fc4c
Gavin <2749984520@qq.com>
2023-05-05 15:59:10 +0800

    fix(tdd): connection test case.

--- tapdata-cli/src/main/java/io/tapdata/pdk/tdd/tests/v3/ConnectionTestThreadTest.java
+++ tapdata-cli/src/main/java/io/tapdata/pdk/tdd/tests/v3/ConnectionTestThreadTest.java
@@ -56,7 +56,7 @@ public class ConnectionTestThreadTest extends PDKTestBaseV2 {
     public void alone() throws NoSuchMethodException {
         System.out.println(langUtil.formatLang("checkThreadTest.alone.wait"));
         final int connectionTestTimes = 5;
-        super.execTest((node, testCase) -> {
+        super.execTestConnection((node, testCase) -> {
             StringJoiner beforeBuilder = new StringJoiner("\n");
             List<StackEntity> before = this.printThreads(beforeBuilder);
             this.connectionTest(node, connectionTestTimes, Boolean.FALSE);
