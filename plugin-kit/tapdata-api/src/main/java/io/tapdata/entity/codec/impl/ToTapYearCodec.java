46103e840d79d9cff2ad27a3989493a2284c93af
jarad <coolgj@163.com>
2023-05-10 00:44:41 +0800

    fix: TapYear from Integer

--- plugin-kit/tapdata-api/src/main/java/io/tapdata/entity/codec/impl/ToTapYearCodec.java
+++ plugin-kit/tapdata-api/src/main/java/io/tapdata/entity/codec/impl/ToTapYearCodec.java
@@ -6,16 +6,17 @@ import io.tapdata.entity.codec.ToTapValueCodec;
 import io.tapdata.entity.codec.impl.utils.AnyTimeToDateTime;
 import io.tapdata.entity.schema.type.TapType;
 import io.tapdata.entity.schema.value.DateTime;
-import io.tapdata.entity.schema.value.TapBinaryValue;
-import io.tapdata.entity.schema.value.TapTimeValue;
 import io.tapdata.entity.schema.value.TapYearValue;
 
 @Implementation(value = ToTapValueCodec.class, type = TapDefaultCodecs.TAP_YEAR_VALUE, buildNumber = 0)
 public class ToTapYearCodec implements ToTapValueCodec<TapYearValue> {
     @Override
     public TapYearValue toTapValue(Object value, TapType typeFromSchema) {
+        if (value instanceof Integer) {
+            return new TapYearValue((Integer) value);
+        }
         DateTime dateTime = AnyTimeToDateTime.toDateTime(value);
-        if(dateTime != null) {
+        if (dateTime != null) {
             return new TapYearValue(dateTime);
         }
         return null;
