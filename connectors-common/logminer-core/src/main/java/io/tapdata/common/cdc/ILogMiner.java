5d5c150422240a2d6a82c2493eb4cc557233babd
jarad <coolgj@163.com>
2023-05-23 01:24:28 +0800

    feat: oracle log collect for database

--- connectors-common/logminer-core/src/main/java/io/tapdata/common/cdc/ILogMiner.java
+++ connectors-common/logminer-core/src/main/java/io/tapdata/common/cdc/ILogMiner.java
@@ -3,12 +3,17 @@ package io.tapdata.common.cdc;
 import io.tapdata.entity.schema.TapTable;
 import io.tapdata.entity.utils.cache.KVReadOnlyMap;
 import io.tapdata.pdk.apis.consumer.StreamReadConsumer;
+import io.tapdata.pdk.apis.functions.connector.source.ConnectionConfigWithTables;
 
 import java.util.List;
 
 public interface ILogMiner {
 
     void init(List<String> tableList, KVReadOnlyMap<TapTable> tableMap, Object offsetState, int recordSize, StreamReadConsumer consumer) throws Throwable;
+
+    default void multiInit(List<ConnectionConfigWithTables> connectionConfigWithTables, KVReadOnlyMap<TapTable> tableMap, Object offsetState, int recordSize, StreamReadConsumer consumer) throws Throwable {
+        throw new UnsupportedOperationException();
+    }
 
     void startMiner() throws Throwable;
 
