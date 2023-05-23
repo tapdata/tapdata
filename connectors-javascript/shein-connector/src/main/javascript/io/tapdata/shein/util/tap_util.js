405287e0b9ea8b13c13f325a527d08d0f60305fe
Gavin <2749984520@qq.com>
2023-05-23 18:36:18 +0800

    fix(connectors):remove useless code.

--- connectors-javascript/shein-connector/src/main/javascript/io/tapdata/shein/util/tap_util.js
+++ connectors-javascript/shein-connector/src/main/javascript/io/tapdata/shein/util/tap_util.js
@@ -11,22 +11,15 @@ var OptionalUtil = {
     }
 }
 
-function iterateAllData(apiName, offset, call) {
-    if (OptionalUtil.isEmpty(apiName)) {
-        log.error("Please specify the corresponding paging API name or URL .");
+var globalTableConfig = {
+    //急采
+    "JiCai": {
+        "supportRead": true,
+        "supportWrite": false
+    },
+    //备货
+    "BeiHuo": {
+        "supportRead": true,
+        "supportWrite": false
     }
-    let res;
-    let error;
-    do {
-        let response = invoker.invoke(apiName, offset);
-        res = response.result;
-    } while (call(res, offset, error));
-}
-
-function commandAndConvertData(apiName, params, call){
-    if (OptionalUtil.isEmpty(apiName)) {
-        log.error("Please specify the corresponding paging API name or URL .");
-    }
-    let invokerData = invoker.invoke(apiName, params);
-    return call(invokerData.result);
 }
