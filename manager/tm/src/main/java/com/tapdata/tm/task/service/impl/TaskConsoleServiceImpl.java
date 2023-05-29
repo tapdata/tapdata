d0f509fdc9b1be3d99ed9a4c5fab83bbb057f770
Harvey <Zerohyuan@163.com>
2023-05-29 16:45:00 +0800

    feat: Shared cache tasks support log collector by default

--- manager/tm/src/main/java/com/tapdata/tm/task/service/impl/TaskConsoleServiceImpl.java
+++ manager/tm/src/main/java/com/tapdata/tm/task/service/impl/TaskConsoleServiceImpl.java
@@ -266,7 +266,7 @@ public class TaskConsoleServiceImpl implements TaskConsoleService {
                 logRelation.setCurrentEventTimestamp(taskInfo.getCurrentEventTimestamp());
                 logRelation.setTaskType(taskInfo.getType());
                 logRelation.setCreateDate(taskInfo.getCreateAt());
-                logRelation.setSyncType(taskInfo.getSyncType());
+                logRelation.setSyncType(taskInfo.getShareCache() ? TaskDto.SYNC_TYPE_MEM_CACHE : taskInfo.getSyncType());
 
                 if (Objects.isNull(tableNames)) {
                     logRelation.setTableNum(0);
