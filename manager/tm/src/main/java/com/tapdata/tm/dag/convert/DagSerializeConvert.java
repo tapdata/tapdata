ead4037fc0e3eee54fd9b26c78beac904f8251ab
Harvey <Zerohyuan@163.com>
2023-06-01 18:42:53 +0800

    fix: #145308 Model deduction fails

--- manager/tm/src/main/java/com/tapdata/tm/dag/convert/DagSerializeConvert.java
+++ manager/tm/src/main/java/com/tapdata/tm/dag/convert/DagSerializeConvert.java
@@ -1,11 +1,15 @@
 package com.tapdata.tm.dag.convert;
 
-import com.tapdata.tm.commons.util.JsonUtil;
 import com.tapdata.tm.commons.dag.DAG;
+import com.tapdata.tm.commons.dag.Node;
 import com.tapdata.tm.commons.task.dto.Dag;
+import com.tapdata.tm.commons.util.JsonUtil;
+import org.apache.commons.collections.CollectionUtils;
 import org.bson.Document;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.WritingConverter;
+
+import java.util.List;
 
 
 /**
@@ -17,6 +21,13 @@ import org.springframework.data.convert.WritingConverter;
 public class DagSerializeConvert implements Converter<DAG, Document> {
     @Override
     public Document convert(DAG dag) {
+        List<Node> nodes = dag.getNodes();
+        if (CollectionUtils.isNotEmpty(nodes)) {
+            nodes.forEach(node -> {
+                node.setSchema(null);
+                node.setOutputSchema(null);
+            });
+        }
         Dag dag1 = dag.toDag();
         String json = JsonUtil.toJsonUseJackson(dag1);
         return Document.parse(json);
