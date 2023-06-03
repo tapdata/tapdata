4637b5e4b05f17682f9df559d1aa2182ea227037
sam.p <102567930@qq.com>
2023-06-03 17:37:36 +0800

    fix(tm): 【v3.1重试】任务源节点停止后，任务的重试，未按系统设置的规则进行重试。
    Closes #145272

--- connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/MongodbConnector.java
+++ connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/MongodbConnector.java
@@ -1025,48 +1025,55 @@ public class MongodbConnector extends ConnectorBase {
 	 * @param tapReadOffsetConsumer
 	 */
 	private void batchRead(TapConnectorContext connectorContext, TapTable table, Object offset, int eventBatchSize, BiConsumer<List<TapEvent>, Object> tapReadOffsetConsumer) throws Throwable {
-		List<TapEvent> tapEvents = list();
-		MongoCursor<Document> mongoCursor;
-		MongoCollection<Document> collection = getMongoCollection(table.getId());
-		final int batchSize = eventBatchSize > 0 ? eventBatchSize : 5000;
-		if (offset == null) {
-			mongoCursor = collection.find().sort(Sorts.ascending(COLLECTION_ID_FIELD)).batchSize(batchSize).iterator();
-		} else {
-			MongoBatchOffset mongoOffset = (MongoBatchOffset) offset;//fromJson(offset, MongoOffset.class);
-			Object offsetValue = mongoOffset.value();
-			if (offsetValue != null) {
-				mongoCursor = collection.find(queryCondition(COLLECTION_ID_FIELD, offsetValue)).sort(Sorts.ascending(COLLECTION_ID_FIELD))
-						.batchSize(batchSize).iterator();
-			} else {
-				mongoCursor = collection.find().sort(Sorts.ascending(COLLECTION_ID_FIELD)).batchSize(batchSize).iterator();
-				TapLogger.warn(TAG, "Offset format is illegal {}, no offset value has been found. Final offset will be null to do the batchRead", offset);
-			}
-		}
-
-		Document lastDocument;
-
 		try {
-			while (mongoCursor.hasNext()) {
-				if (!isAlive()) return;
-				lastDocument = mongoCursor.next();
-				tapEvents.add(insertRecordEvent(lastDocument, table.getId()));
-
-				if (tapEvents.size() == eventBatchSize) {
-					Object value = lastDocument.get(COLLECTION_ID_FIELD);
-					batchOffset = new MongoBatchOffset(COLLECTION_ID_FIELD, value);
-					tapReadOffsetConsumer.accept(tapEvents, batchOffset);
-					tapEvents = list();
+			List<TapEvent> tapEvents = list();
+			MongoCursor<Document> mongoCursor;
+			MongoCollection<Document> collection = getMongoCollection(table.getId());
+			final int batchSize = eventBatchSize > 0 ? eventBatchSize : 5000;
+			if (offset == null) {
+				mongoCursor = collection.find().sort(Sorts.ascending(COLLECTION_ID_FIELD)).batchSize(batchSize).iterator();
+			} else {
+				MongoBatchOffset mongoOffset = (MongoBatchOffset) offset;//fromJson(offset, MongoOffset.class);
+				Object offsetValue = mongoOffset.value();
+				if (offsetValue != null) {
+					mongoCursor = collection.find(queryCondition(COLLECTION_ID_FIELD, offsetValue)).sort(Sorts.ascending(COLLECTION_ID_FIELD))
+							.batchSize(batchSize).iterator();
+				} else {
+					mongoCursor = collection.find().sort(Sorts.ascending(COLLECTION_ID_FIELD)).batchSize(batchSize).iterator();
+					TapLogger.warn(TAG, "Offset format is illegal {}, no offset value has been found. Final offset will be null to do the batchRead", offset);
 				}
 			}
-			if (!tapEvents.isEmpty()) {
-				tapReadOffsetConsumer.accept(tapEvents, null);
+
+			Document lastDocument;
+
+			try {
+				while (mongoCursor.hasNext()) {
+					if (!isAlive()) return;
+					lastDocument = mongoCursor.next();
+					tapEvents.add(insertRecordEvent(lastDocument, table.getId()));
+
+					if (tapEvents.size() == eventBatchSize) {
+						Object value = lastDocument.get(COLLECTION_ID_FIELD);
+						batchOffset = new MongoBatchOffset(COLLECTION_ID_FIELD, value);
+						tapReadOffsetConsumer.accept(tapEvents, batchOffset);
+						tapEvents = list();
+					}
+				}
+				if (!tapEvents.isEmpty()) {
+					tapReadOffsetConsumer.accept(tapEvents, null);
+				}
+			} catch (Exception e) {
+				if (!isAlive() && e instanceof MongoInterruptedException) {
+					// ignored
+				} else {
+					throw e;
+				}
 			}
 		} catch (Exception e) {
-			if (!isAlive() && e instanceof MongoInterruptedException) {
-				// ignored
-			} else {
-				throw e;
+			if (e instanceof MongoTimeoutException) {
+				throw new TapPdkTerminateByServerEx(connectorContext.getId(), e);
 			}
+			throw new RuntimeException(e);
 		}
 	}
 
@@ -1105,6 +1112,9 @@ public class MongodbConnector extends ConnectorBase {
 			} catch (Exception ignored) {
 			}
 			mongodbStreamReader = null;
+			if (e instanceof MongoTimeoutException) {
+				throw new TapPdkTerminateByServerEx(connectorContext.getId(), e);
+			}
 			throw new RuntimeException(e);
 		}
 
