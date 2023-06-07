1a3a040932f407ae5af2a7c69187ca39c77bca91
hardy <2545433047@qq.com>
2023-06-07 18:48:05 +0800

    fix: #140379 新增返回字段scopeNames

--- manager/tm/src/main/java/com/tapdata/tm/application/service/ApplicationService.java
+++ manager/tm/src/main/java/com/tapdata/tm/application/service/ApplicationService.java
@@ -7,7 +7,11 @@ import com.tapdata.tm.base.dto.Filter;
 import com.tapdata.tm.base.dto.Page;
 import com.tapdata.tm.base.dto.Where;
 import com.tapdata.tm.base.service.BaseService;
+import com.tapdata.tm.commons.base.convert.ObjectIdDeserialize;
 import com.tapdata.tm.config.security.UserDetail;
+import com.tapdata.tm.role.entity.RoleEntity;
+import com.tapdata.tm.role.repository.RoleRepository;
+import com.tapdata.tm.utils.SpringContextHelper;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
@@ -15,9 +19,11 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @Author:
@@ -37,7 +43,18 @@ public class ApplicationService extends BaseService<ApplicationDto, ApplicationE
 
 
     public Page find(Filter filter, UserDetail userDetail) {
-        return super.find(filter, userDetail);
+        RoleRepository roleRepository = SpringContextHelper.getBean(RoleRepository.class);
+        Map<ObjectId, String> roleMap = roleRepository.findAll(Query.query(Criteria.where("_id").ne(null))).stream()
+                .collect(Collectors.toMap(RoleEntity::getId,RoleEntity::getName,(key1, key2) -> key2));
+        Page<ApplicationDto> pageApplications = super.find(filter, userDetail);
+        pageApplications.getItems().stream().forEach(applicationDto -> {
+            List scopeNames = new ArrayList<>();
+            applicationDto.getScopes().forEach(s->{
+                scopeNames.add(roleMap.get(ObjectIdDeserialize.toObjectId(s.toString())));
+            });
+            applicationDto.setScopeNames(scopeNames);
+        });
+        return pageApplications;
     }
 
     public ApplicationDto updateById(ApplicationDto applicationDto, UserDetail userDetail) {
