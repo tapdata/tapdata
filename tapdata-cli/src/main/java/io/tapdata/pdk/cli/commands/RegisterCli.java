a3c64a278122a996606f6e0a448d54579943d709
Gavin <2749984520@qq.com>
2023-06-12 10:36:56 +0800

    fix(register-cli): add import of scanner.

--- tapdata-cli/src/main/java/io/tapdata/pdk/cli/commands/RegisterCli.java
+++ tapdata-cli/src/main/java/io/tapdata/pdk/cli/commands/RegisterCli.java
@@ -33,6 +33,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.Scanner;
 
 @CommandLine.Command(
         description = "Push PDK jar file into Tapdata",
