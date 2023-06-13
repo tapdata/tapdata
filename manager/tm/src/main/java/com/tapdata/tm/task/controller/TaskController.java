027b38a395b5337a697da8a98211901c819e5279
Gavin <2749984520@qq.com>
2023-06-13 14:59:32 +0800

    fix(js-node):remove access token.

--- manager/tm/src/main/java/com/tapdata/tm/task/controller/TaskController.java
+++ manager/tm/src/main/java/com/tapdata/tm/task/controller/TaskController.java
@@ -1058,17 +1058,12 @@ public class TaskController extends BaseController {
 
     @PostMapping("migrate-js/test-run-rpc")
     @Operation(description = "js节点试运行, 执行试运行后即可获取到试运行结果和试运行日志")
-    public ResponseMessage<Map<String, Object>> testRunRPC(
-            @RequestBody TestRunDto dto,
-            @RequestParam(value = "access_token", required = false) String accessToken,
-            HttpServletRequest request,
-            HttpServletResponse response
-    ) {
+    public ResponseMessage<Map<String, Object>> testRunRPC(@RequestBody TestRunDto dto) {
         Map<String, Object> data = null;
         ResponseMessage<Map<String, Object>> responseMessage = new ResponseMessage<>();
         Map<String, Object> result = null;
         try {
-            data = taskNodeService.testRunJsNodeRPC(dto, getLoginUser(), accessToken);
+            data = taskNodeService.testRunJsNodeRPC(dto, getLoginUser());
             result = (Map<String, Object>)Optional.ofNullable(data.get("data")).orElse(data);
             if (null == result || result.isEmpty()) {
                 throw new CoreException("Can not get data from source,  Please ensure if source connection is valid");
