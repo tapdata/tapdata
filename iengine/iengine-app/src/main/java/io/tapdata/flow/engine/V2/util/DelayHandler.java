2c1c5862922ee1b42593df513d84a495eda410ae
Harvey <Zerohyuan@163.com>
2023-06-20 19:02:33 +0800

    fix(iengine): #146561 The cpu load of css is too high

---
+++ iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/util/DelayHandler.java
@@ -0,0 +1,64 @@
+package io.tapdata.flow.engine.V2.util;
+
+import io.tapdata.observable.logging.ObsLogger;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Supplier;
+
+public class DelayHandler {
+
+
+    private static final long MAX_DELAY = TimeUnit.SECONDS.toMicros(1);
+    private static final long MIN_DELAY = TimeUnit.MICROSECONDS.toMicros(0);
+    private static final long UNIT = TimeUnit.MICROSECONDS.toMicros(10);
+    private static final long WARN_DELAY = TimeUnit.MILLISECONDS.toMicros(10);
+    private final ObsLogger obsLogger;
+    private final AtomicLong succeeded = new AtomicLong(0);
+    private final AtomicLong failed = new AtomicLong(0);
+
+    public DelayHandler(ObsLogger obsLogger) {
+        this.obsLogger = obsLogger;
+    }
+
+    public boolean process(Supplier<Boolean> supplier) {
+        boolean processResult = supplier.get();
+        try {
+            if (processResult) {
+                succeeded.incrementAndGet();
+            } else {
+                failed.incrementAndGet();
+                sleep();
+            }
+        } catch (Throwable ignore) {
+            obsLogger.warn("Delay push failed, ignore processing", ignore);
+        }
+        return processResult;
+
+    }
+
+    private void sleep() throws InterruptedException {
+        long relay = delay();
+        if (relay > MIN_DELAY) {
+            if (obsLogger.isDebugEnabled()) {
+                obsLogger.debug("Successor node processing speed is limited, about to delay {} millisecond", TimeUnit.MICROSECONDS.toMillis(relay));
+            }
+            if (relay > WARN_DELAY) {
+                obsLogger.warn("Successor node processing speed is limited, about to delay {} millisecond", TimeUnit.MICROSECONDS.toMillis(relay));
+            }
+            TimeUnit.MICROSECONDS.sleep(relay);
+        }
+    }
+
+    private long delay() {
+        long relay = failed.get() * UNIT - succeeded.get() * UNIT;
+        if (relay < MIN_DELAY) {
+            relay = 0;
+        }
+        if (relay > MAX_DELAY) {
+            relay = MAX_DELAY;
+        }
+        return relay;
+    }
+
+}
