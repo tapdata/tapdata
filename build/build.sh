5693a9b352e00535bdecc8d49d00ef6d48eaee60
dreamcoin1998 <gaojunbin98@gmail.com>
2023-06-21 17:07:44 +0800

    fix: test cicd

--- build/build.sh
+++ build/build.sh
@@ -23,7 +23,7 @@ if [[ $_in_docker == "" ]]; then
 fi
 
 components=""
-all_components=("plugin-kit" "file-storages" "connectors-common" "manager" "iengine" "connectors" "tapdata-cli")
+all_components=("plugin-kit" "file-storages" "connectors-common" "manager" "iengine" "connectors" "tapdata-cli", "error-code-root")
 package_all_components=("iengine" "manager")
 source_components=("plugin-kit" "file-storages" "connectors-common" "connectors" "tapshell" "build" "tapdata-cli")
 output="package"
