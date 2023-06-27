459658c667c1316833dcc087b0f5d462ddb4f71f
jiuyetx <meteor209@gmail.com>
2023-06-27 12:25:50 +0800

    Revert "add retry MongoSocketReadException"
    This reverts commit 0b575b8fefa1f4a57073aad4de53c6c12237c956.

--- connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/MongodbConnector.java
+++ connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/MongodbConnector.java
@@ -57,7 +57,6 @@ import io.tapdata.entity.simplify.TapSimplify;
 import io.tapdata.entity.utils.DataMap;
 import io.tapdata.entity.utils.InstanceFactory;
 import io.tapdata.entity.utils.ParagraphFormatter;
-import io.tapdata.exception.TapPdkRetryableEx;
 import io.tapdata.exception.TapPdkTerminateByServerEx;
 import io.tapdata.kit.EmptyKit;
 import io.tapdata.mongodb.entity.MongodbConfig;
@@ -830,9 +829,6 @@ public class MongodbConnector extends ConnectorBase {
 		} catch (Throwable e) {
 			if (e instanceof MongoTimeoutException) {
 				throw new TapPdkTerminateByServerEx(connectorContext.getId(), e);
-			}
-			if (e instanceof MongoSocketReadException) {
-				throw new TapPdkRetryableEx(connectorContext.getId(), e);
 			}
 			throw e;
 		}
