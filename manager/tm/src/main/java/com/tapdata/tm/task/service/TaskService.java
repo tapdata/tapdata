a971a7e29d531c0dcc84e6826d8c3408a9c5bc65
jiuyetx <meteor209@gmail.com>
2023-06-28 18:05:17 +0800

    Revert "update runningTaskNum method remove planStartDateFlag query"
    This reverts commit 1e6d1c0b327a935d75999e7a8968573529614b34.

--- manager/tm/src/main/java/com/tapdata/tm/task/service/TaskService.java
+++ manager/tm/src/main/java/com/tapdata/tm/task/service/TaskService.java
@@ -4152,9 +4152,7 @@ public class TaskService extends BaseService<TaskDto, TaskEntity, ObjectId, Task
     public int runningTaskNum(String processId) {
         long workNum = count(Query.query(Criteria.where("agentId").is(processId)
                 .and("is_deleted").ne(true)
-                .and("status").is(TaskDto.STATUS_RUNNING)
-                )
-        );
+                        .orOperator(Criteria.where("status").is(TaskDto.STATUS_RUNNING), Criteria.where("planStartDateFlag").is(true))));
         return (int) workNum;
     }
 
