86abd4a4913824ee6d2b334b622434c0c108e45d
jiuyetx <meteor209@gmail.com>
2023-06-29 16:47:52 +0800

    add WorkerExpireDto JsonProperty

--- manager/tm/src/main/java/com/tapdata/tm/worker/dto/WorkerExpireDto.java
+++ manager/tm/src/main/java/com/tapdata/tm/worker/dto/WorkerExpireDto.java
@@ -1,5 +1,6 @@
 package com.tapdata.tm.worker.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.Date;
@@ -11,4 +12,6 @@ public class WorkerExpireDto {
     private String shareTmUserId;
     private String shareTcmUserId;
     private Date expireTime;
+    @JsonProperty("is_deleted")
+    private boolean is_deleted;
 }
