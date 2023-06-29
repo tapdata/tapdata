bf4b57795e100780f89ecc2c29a2d89642346b75
sam.p <102567930@qq.com>
2023-06-29 16:55:50 +0800

    fix(postgres connector): #4681 PG 共享挖掘报错 null used for required field: "creat_dte", schema type: INT64
    Closes #144915

--- connectors-common/postgres-core/src/main/java/io/tapdata/connector/postgres/converters/GeometryConverter.java
+++ connectors-common/postgres-core/src/main/java/io/tapdata/connector/postgres/converters/GeometryConverter.java
@@ -1,30 +1,34 @@
 package io.tapdata.connector.postgres.converters;
 
-import io.debezium.spi.converter.CustomConverter;
 import io.debezium.spi.converter.RelationalColumn;
-import io.tapdata.kit.EmptyKit;
 import org.apache.kafka.connect.data.SchemaBuilder;
 
+import javax.annotation.Nonnull;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
-public class GeometryConverter implements CustomConverter<SchemaBuilder, RelationalColumn> {
+public class GeometryConverter extends BaseTapdataConverter {
 
-    private SchemaBuilder geometrySchema;
     private final List<String> geometryTypes = Arrays.asList("point", "line", "lseg", "box", "path", "polygon", "circle");
 
     @Override
-    public void configure(Properties props) {
-        geometrySchema = SchemaBuilder.string().name(props.getProperty("schema.name"));
+    SchemaBuilder initSchemaBuilder(Properties props) {
+        return SchemaBuilder.string().name(props.getProperty("schema.name"));
     }
 
     @Override
-    public void converterFor(RelationalColumn column,
-                             ConverterRegistration<SchemaBuilder> registration) {
+    Object initDefaultValue() {
+        return "";
+    }
 
-        if (geometryTypes.contains(column.typeName())) {
-            registration.register(geometrySchema, x -> EmptyKit.isNull(x) ? null : x.toString());
-        }
+    @Override
+    boolean needConvert(RelationalColumn column) {
+        return geometryTypes.contains(column.typeName());
+    }
+
+    @Override
+    Object convert(@Nonnull Object data) {
+        return data.toString();
     }
 }
