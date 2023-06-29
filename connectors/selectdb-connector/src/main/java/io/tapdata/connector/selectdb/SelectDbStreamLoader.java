59c707f18dceaacabc270a41909588fa49b5988a
DGshikun <63392552+DGshikun@users.noreply.github.com>
2023-06-29 15:55:51 +0800

    Fix selectdb release3.1 (#1084)
    * fix:SelectDb Retry strategy.
    
    * fix:SelectDb upload retry
    
    * fix:Delete SelectDb TapPdkRetryableEx

--- connectors/selectdb-connector/src/main/java/io/tapdata/connector/selectdb/SelectDbStreamLoader.java
+++ connectors/selectdb-connector/src/main/java/io/tapdata/connector/selectdb/SelectDbStreamLoader.java
@@ -16,7 +16,6 @@ import io.tapdata.entity.event.dml.TapRecordEvent;
 import io.tapdata.entity.event.dml.TapUpdateRecordEvent;
 import io.tapdata.entity.logger.TapLogger;
 import io.tapdata.entity.schema.TapTable;
-import io.tapdata.exception.TapPdkRetryableEx;
 import io.tapdata.pdk.apis.context.TapConnectorContext;
 import io.tapdata.pdk.apis.entity.WriteListResult;
 import okhttp3.Response;
@@ -119,12 +118,12 @@ public class SelectDbStreamLoader extends Throwable {
         int statusCode = response.code();
         try {
             if (!(statusCode >= 200 && statusCode < 300) || null == selectDBCopyIntoLog.get("State") || null == selectDBCopyIntoLog.get("JobId")) {
-                throw new TapPdkRetryableEx(connectorContext.getId(), new Throwable("HttpCode: " + statusCode + " Response.body: " + response.body() + " Response: " + response + " State: " + selectDBCopyIntoLog.get("State") + " JobId: " + selectDBCopyIntoLog.get("JobId")));
+                throw new CoreException(connectorContext.getId(), new Throwable("HttpCode: " + statusCode + " Response.body: " + response.body() + " Response: " + response + " State: " + selectDBCopyIntoLog.get("State") + " JobId: " + selectDBCopyIntoLog.get("JobId")));
             }
         } catch (Throwable e) {
             catchCount++;
             if (catchCount > 20) {
-                throw new TapPdkRetryableEx(connectorContext.getId(), new Throwable("HttpCode: " + statusCode + " Response.body: " + response.body() + " Response: " + response + " State: " + selectDBCopyIntoLog.get("State") + " JobId: " + selectDBCopyIntoLog.get("JobId")));
+                throw new CoreException(connectorContext.getId(), new Throwable("HttpCode: " + statusCode + " Response.body: " + response.body() + " Response: " + response + " State: " + selectDBCopyIntoLog.get("State") + " JobId: " + selectDBCopyIntoLog.get("JobId")));
             }
             sleep(150000);
             connectorContext.getLog().warn("Data source upload retry: {}", catchCount);
