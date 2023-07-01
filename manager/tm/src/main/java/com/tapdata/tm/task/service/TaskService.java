d953665b286834fc2e9b8a4042f8bd502302e28c
jiuyetx <meteor209@gmail.com>
2023-07-01 23:07:02 +0800

    fix task run limit

--- manager/tm/src/main/java/com/tapdata/tm/task/service/TaskService.java
+++ manager/tm/src/main/java/com/tapdata/tm/task/service/TaskService.java
@@ -4162,7 +4162,7 @@ public class TaskService extends BaseService<TaskDto, TaskEntity, ObjectId, Task
     public int runningTaskNum(String processId, UserDetail userDetail) {
         long workNum = count(Query.query(Criteria.where("agentId").is(processId)
                 .and("is_deleted").ne(true)
-                        .orOperator(Criteria.where("status").is(TaskDto.STATUS_RUNNING),
+                        .orOperator(Criteria.where("status").in(TaskDto.STATUS_RUNNING, TaskDto.STATUS_SCHEDULING),
                                 Criteria.where("planStartDateFlag").is(true),
                                 Criteria.where("crontabExpressionFlag").is(true)
                         )), userDetail);
