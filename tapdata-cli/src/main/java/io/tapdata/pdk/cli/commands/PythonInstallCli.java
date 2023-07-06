58c646cd20665e3f3ac4d0ed0e76a53b95224848
Gavin <2749984520@qq.com>
2023-07-06 19:39:52 +0800

    fix(plugin-kit): remove plugin to exec cli.

--- tapdata-cli/src/main/java/io/tapdata/pdk/cli/commands/PythonInstallCli.java
+++ tapdata-cli/src/main/java/io/tapdata/pdk/cli/commands/PythonInstallCli.java
@@ -218,7 +218,7 @@ public class PythonInstallCli extends CommonCli {
     //2. 解压jython-standalone-2.7.2.jar -> jython-standalone-2.7.2-item/
     //3. 将class目录写入到jython-standalone-2.7.2-item/Lib/
     //4. 压缩jython-standalone-2.7.2-item/ -> jython-standalone-2.7.2.jar
-    public Integer execute() throws Exception {
+    public Integer execute1() throws Exception {
         if (null == selfJarPath) {
             TapLogger.error(TAG, "Miss tapdata-cli-1.0-SNAPSHOT.jar path");
             return -1;
@@ -360,6 +360,147 @@ public class PythonInstallCli extends CommonCli {
         return 0;
     }
 
+    public Integer execute() throws Exception {
+        if (null == selfJarPath) {
+            TapLogger.error(TAG, "Miss tapdata-cli-1.0-SNAPSHOT.jar path");
+            return -1;
+        }
+        if (null == pyJarPath) {
+            TapLogger.error(TAG, "Miss jython-standalone-2.7.2.jar path");
+            return -1;
+        }
+        if (null == packagesPath){
+            TapLogger.error(TAG, "Miss package path");
+            return -1;
+        }
+        File self = new File(selfJarPath);
+        if (null == self || !self.exists() || !self.isFile()){
+            TapLogger.error(TAG, "Miss script-engine-module.jar path");
+            return -2;
+        }
+
+        final String jarPath = pyJarPath.endsWith(jarName) ? pyJarPath : pyJarPath + jarName;
+        final String libPath = (pyJarPath.endsWith(jarName) ? pyJarPath.replace(jarName, "") : pyJarPath ) + "Lib";
+        File file = new File(jarPath);
+        if (null == file || !file.exists() || !file.isFile()){
+            TapLogger.error(TAG, "Miss jython-standalone.jar path: " + jarPath);
+            return -2;
+        }
+        File path = new File(packagesPath);
+        if (null == path || !path.isDirectory()) {
+            TapLogger.error(TAG, "Miss package path: " + packagesPath);
+            return -2;
+        }
+
+        final String libJarName = FilenameUtils.concat(pyJarPath, "Lib.jar");
+        final String libPathName = FilenameUtils.concat(pyJarPath, "temp_engine");
+
+        String batPath = pyJarPath + "cmd_bat.bat";
+        File bat = new File(batPath);
+        try {
+            //依次执行命令
+            File[] files = path.listFiles();
+            if (null != files && files.length > 0) {
+                for (File packageItem : files) {
+                    String absolutePath = packageItem.getAbsolutePath();
+                    if (!packageItem.isDirectory()) {
+                        String name = packageItem.getName();
+                        if (null == name || "".equals(name.trim())) continue;
+                        String[] split = name.split("\\.");
+                        if (split.length <= 0 ) continue;
+                        try {
+                            ZipUtils.unzip(packageItem.getAbsolutePath(), path.getAbsolutePath()+ "\\" + split[0]);
+                            if (packageItem.exists()) {
+                                FileUtils.deleteQuietly(packageItem);
+                            }
+                        } catch (Exception e) {
+                            TapLogger.warn(TAG, "Can not unzip file: {}", packageItem.getAbsolutePath());
+                            continue;
+                        }
+                    }
+                    String setUpPyPath = packageItem.getAbsolutePath() + (absolutePath.endsWith("\\") ? "" : "\\") + "setup.py";
+                    File setup = null;
+                    try {
+                        setup = new File(setUpPyPath);
+                    }catch (Exception e){
+                        TapLogger.error(TAG, "Can not find file {}, error: {}", setUpPyPath, e.getMessage());
+                    }
+                    //C:\Users\Gavin'Xiao\.m2\repository\org\python\jython-standalone\2.7.2
+                    if (null == setup || !setup.exists() || !setup.isFile()) {
+                        continue;
+                    }
+
+                    try {
+                        try(Writer writer = new FileWriter(bat)) {
+                            writer.write(
+                                    "cd "+ packageItem.getAbsolutePath() + " \r\n"+
+                                            String.format(
+                                                    "java -jar %s %s install"
+                                                    , path(jarPath)
+                                                    , packageItem.getAbsolutePath() + "/setup.py")
+                            );
+                        } catch (IOException e) {
+                            TapLogger.warn(TAG, "Can't create bat file {}, error: {}", batPath, e.getMessage());
+                        }
+                        // java -jar /usr/local/lib/jython-standalone-2.7.2.jar setup.py install
+                        cmdRunJar(
+                                new File(packageItem.getAbsolutePath()),
+                                String.format("java -jar %s %s install"
+                                        , path(jarPath)
+                                        , "setup.py")
+//                            "cmd -c start " + path(batPath)
+                                //"cd "+ packageItem.getAbsolutePath() + " & "+
+                                //String.format(
+                                //    "java -jar %s %s install"
+                                //    , path(jarPath)
+                                //    , path(setUpPyPath) )
+                        );
+                    } catch (Exception e){
+                        TapLogger.warn(TAG, "Can't import {}, error: {}", setUpPyPath, e.getMessage());
+                    }
+                }
+
+                File libFiles = new File(libPath);
+                try {
+                    try {
+                        ZipUtils.unzip(file.getAbsolutePath(), libPathName);
+                    }catch (Exception e){
+                        TapLogger.error(TAG, "Can not zip from " + self.getAbsolutePath() + " to " + libPathName);
+                        return -3;
+                    }
+
+                    FileUtils.copyToDirectory(libFiles, new File(libPathName + "\\Lib\\"));
+
+                    //压缩
+                    if (file.exists())
+                        FileUtils.deleteQuietly(file);
+                    File atomicFile = new File(file.getAbsolutePath());
+                    try (OutputStream fos = new FileOutputStream(atomicFile)) {
+                        ZipUtils.zip(libPathName, fos);
+                        System.out.println("zip jar successfully. " + self.getAbsolutePath());
+                    } catch (Exception e) {
+                        System.out.println("zip " + self.getAbsolutePath() + " jar failed, msg: " + e.getMessage());
+                    }
+                } finally {
+                    File temp = new File(libJarName);
+                    if (temp.exists())
+                        FileUtils.deleteQuietly(temp);
+                    File temp1 = new File(libPathName);
+                    if (temp1.exists())
+                        FileUtils.deleteQuietly(temp1);
+                    if (bat.exists())
+                        FileUtils.deleteQuietly(bat);
+                }
+
+            } else {
+                TapLogger.warn(TAG, "Not fund any import package file");
+            }
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+            CommonUtils.logError(TAG, "Class modify failed", throwable);
+        }
+        return 0;
+    }
     // try {
     //                    File libFile = new File(libPath);
     //                    if (null != libFile && libFile.isDirectory()) {
