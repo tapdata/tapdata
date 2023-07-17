9beeb5352ed0c21b6c447d3bb63c28f9f301339a
Gavin <2749984520@qq.com>
2023-07-18 00:57:46 +0800

    fix(sybase): cdc can not release process.

--- connectors/sybase-connector/src/main/java/io/tapdata/sybase/cdc/service/ExecCommand.java
+++ connectors/sybase-connector/src/main/java/io/tapdata/sybase/cdc/service/ExecCommand.java
@@ -30,7 +30,7 @@ class ExecCommand implements CdcStep<CdcRoot> {
     private final static String EXPORT_JAVA_HOME = "export JAVA_TOOL_OPTIONS=\"-Duser.language=en\"";
     private final static String START_CDC = "$pocCliPath$/bin/tapdata_sybase_cdc_replicant $commandType$ $pocPath$/config/sybase2csv/src_sybasease.yaml $pocPath$/config/sybase2csv/dst_localstorage.yaml --general $pocPath$/config/sybase2csv/general.yaml --filter $pocPath$/config/sybase2csv/filter_sybasease.yaml --extractor $pocPath$/config/sybase2csv/ext_sybasease.yaml --id $taskId$ --replace $overwriteType$ --verbose";
 
-    private final static String KILL_SHELL = "ps -ef|grep 'tapdata_sybase_cdc_replicant'|awk '{print $2}'|xargs kill -15";
+    private final static String KILL_SHELL = "";//= "ps -ef|grep 'tapdata_sybase_cdc_replicant'|awk '{print $2}'|xargs kill -15";
 
     protected ExecCommand(CdcRoot root, CommandType commandType, OverwriteType overwriteType) {
         this.root = root;
@@ -56,7 +56,7 @@ class ExecCommand implements CdcStep<CdcRoot> {
             String[] cmds = new String[]{
                     "/bin/sh",
                     "-c",
-                    KILL_SHELL + "; " + EXPORT_JAVA_HOME + "; " + cmd
+                     EXPORT_JAVA_HOME + "; " + cmd
             };
             Runtime runtime = Runtime.getRuntime();
             Process exec = runtime.exec(cmds);
@@ -66,21 +66,67 @@ class ExecCommand implements CdcStep<CdcRoot> {
             Class<? extends Process> aClass = exec.getClass();
             StringJoiner joiner = new StringJoiner(",");
             try {
-                if (!exec.isAlive()) {
-                    throw new CoreException(Utils.readFromInputStream(exec.getErrorStream(), StandardCharsets.UTF_8));
-                }
                 exec.exitValue();
-                throw new CoreException(Utils.readFromInputStream(exec.getErrorStream(), StandardCharsets.UTF_8));
-            } catch (IOException ignore){}
+                throw new CoreException("Cdc tool can not running, fail to get stream data");//Utils.readFromInputStream(exec.getErrorStream(), StandardCharsets.UTF_8));
+            } catch (Exception ignore){}
 
             KVMap<Object> stateMap = root.getContext().getStateMap();
             stateMap.put("tableOverType", OverwriteType.RESUME.getType());
+            try {
+                if ("java.lang.UNIXProcess".equals(name)) {
+                    Field[] declaredFields = aClass.getDeclaredFields();
+                    for (Field declaredField : declaredFields) {
+                        declaredField.setAccessible(true);
+                        joiner.add(declaredField.getName());
+                    }
+                    root.getContext().getLog().info("All Fields: , {}, {}", aClass.getName(), joiner.toString());
+
+                    Field pid = aClass.getDeclaredField("pid");
+                    pid.setAccessible(true);
+                    cdcPid = pid.getLong(pid);
+                    stateMap.put("cdcPid", cdcPid);
+                } else if ("java.lang.ProcessImpl".equals(name)) {
+                    Field pid = aClass.getDeclaredField("handle");
+                    pid.setAccessible(true);
+                    cdcPid = (Integer)pid.get(pid);
+                    stateMap.put("cdcPid", cdcPid);
+                } else {
+                    root.getContext().getLog().info("Cdc tool is running, but can not get it's pid, {}, {}", aClass.getName());
+                }
+            } catch (Exception ignore){
+            }
+            if (cdcPid > 0) {
+                root.getContext().getLog().info("Cdc tool is running which pid is {}", cdcPid);
+            } else {
+                root.getContext().getLog().info("Cdc tool is running, but can not get it's pid, {}, {}", aClass.getName());
+            }
+            root.getContext().getLog().info("All Fields: ", joiner.toString());
+
         } catch (Exception e) {
             throw new CoreException("Command exec failed, unable to start cdc command: {}, msg: {}", cmd, e.getMessage());
         } finally {
             root.getContext().getLog().info("You can go to {}/config/sybase2csv/trace/{}/trace.log to view the log information generated during the corresponding cdc execution",
                     sybasePocPath, root.getCdcId());
         }
+
         return this.root;
+    }
+
+    private String shellOutput(InputStream inputStream){
+        Scanner br = null;
+        StringBuilder builder = new StringBuilder();
+        try {
+            br = new Scanner(new InputStreamReader(inputStream));
+            String line = null;
+            while (br.hasNextLine()) {
+                line = br.nextLine();
+                builder.append(line).append("\n");
+            }
+        } finally {
+            if (null != br) {
+                br.close();
+            }
+        }
+        return br.toString();
     }
 }
