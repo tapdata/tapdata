c48541801f58c2161cda8c740952a9155e02fe26
jiuyetx <meteor209@gmail.com>
2023-07-17 15:11:01 +0800

    #147870 fix cron task query

--- manager/tm/src/main/java/com/tapdata/tm/task/service/TaskService.java
+++ manager/tm/src/main/java/com/tapdata/tm/task/service/TaskService.java
@@ -3805,7 +3805,6 @@ public class TaskService extends BaseService<TaskDto, TaskEntity, ObjectId, Task
     public void startPlanCronTask() {
         Criteria migrateCriteria = Criteria.where("crontabExpressionFlag").is(true)
                 .and("type").is(TaskDto.TYPE_INITIAL_SYNC)
-                .and("crontabScheduleMsg").is(null)
                 .and("crontabExpression").exists(true)
                 .and("is_deleted").is(false)
                 .andOperator(Criteria.where("status").nin(TaskDto.STATUS_EDIT,TaskDto.STATUS_STOPPING,
