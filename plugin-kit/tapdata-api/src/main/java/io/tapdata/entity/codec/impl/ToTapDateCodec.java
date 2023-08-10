54dab2fcc1194d88da67371dec6730023370c09a
jarad <coolgj@163.com>
2023-08-10 10:47:59 +0800

    fix: TapDate with String type

--- plugin-kit/tapdata-api/src/main/java/io/tapdata/entity/codec/impl/ToTapDateCodec.java
+++ plugin-kit/tapdata-api/src/main/java/io/tapdata/entity/codec/impl/ToTapDateCodec.java
@@ -6,21 +6,21 @@ import io.tapdata.entity.codec.ToTapValueCodec;
 import io.tapdata.entity.codec.impl.utils.AnyTimeToDateTime;
 import io.tapdata.entity.schema.type.TapType;
 import io.tapdata.entity.schema.value.DateTime;
-import io.tapdata.entity.schema.value.TapDateTimeValue;
 import io.tapdata.entity.schema.value.TapDateValue;
-
-import java.util.Date;
 
 @Implementation(value = ToTapValueCodec.class, type = TapDefaultCodecs.TAP_DATE_VALUE, buildNumber = 0)
 public class ToTapDateCodec implements ToTapValueCodec<TapDateValue> {
     @Override
     public TapDateValue toTapValue(Object value, TapType typeFromSchema) {
-        if(value instanceof DateTime) {
+        if (value instanceof DateTime) {
             return new TapDateValue((DateTime) value);
+        }
+        if (value instanceof String) {
+            return new TapDateValue(AnyTimeToDateTime.withDateStr((String) value));
         }
         DateTime dateTime = AnyTimeToDateTime.toDateTime(value);
         TapDateValue tapDateValue = null;
-        if(dateTime != null) {
+        if (dateTime != null) {
             tapDateValue = new TapDateValue(dateTime);
         }
 
