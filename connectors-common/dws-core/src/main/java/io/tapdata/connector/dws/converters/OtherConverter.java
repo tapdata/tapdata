fe4884554d86a8a1b5704ab1ee60afe92b7ec674
mnianqi <mnq0626@163.com>
2023-08-17 20:17:05 +0800

    feature(dws): 适配dws数据源
    Closes #148156

---
+++ connectors-common/dws-core/src/main/java/io/tapdata/connector/dws/converters/OtherConverter.java
@@ -0,0 +1,33 @@
+package io.tapdata.connector.dws.converters;
+
+import io.debezium.spi.converter.RelationalColumn;
+import org.apache.kafka.connect.data.SchemaBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+public class OtherConverter extends BaseTapdataConverter {
+
+    private final List<String> otherTypes = Arrays.asList("bit", "varbit", "tsvector", "tsquery", "regproc", "regprocedure", "regoper", "regoperator", "regclass", "regtype", "regconfig", "regdictionary", "pg_lsn");
+
+    @Override
+    SchemaBuilder initSchemaBuilder(Properties props) {
+        return SchemaBuilder.string().name(props.getProperty("schema.name"));
+    }
+
+    @Override
+    Object initDefaultValue() {
+        return "";
+    }
+
+    @Override
+    boolean needConvert(RelationalColumn column) {
+        return otherTypes.contains(column.typeName());
+    }
+
+    @Override
+    Object convert(Object data) {
+        return data.toString();
+    }
+}
