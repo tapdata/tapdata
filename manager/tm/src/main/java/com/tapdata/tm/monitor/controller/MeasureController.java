b6d0c98745ec0a38ab0b0843eb3bc6d375bcf26d
harsen <harsen@tapdata.io>
2023-08-23 22:54:06 +0800

    fix: User binding is ignored because it is required to support data authorization.

--- manager/tm/src/main/java/com/tapdata/tm/monitor/controller/MeasureController.java
+++ manager/tm/src/main/java/com/tapdata/tm/monitor/controller/MeasureController.java
@@ -3,6 +3,7 @@ package com.tapdata.tm.monitor.controller;
 import com.tapdata.tm.base.controller.BaseController;
 import com.tapdata.tm.base.dto.Page;
 import com.tapdata.tm.base.dto.ResponseMessage;
+import com.tapdata.tm.config.security.UserDetail;
 import com.tapdata.tm.monitor.dto.BatchRequestDto;
 import com.tapdata.tm.monitor.dto.TableSyncStaticDto;
 import com.tapdata.tm.monitor.param.AggregateMeasurementParam;
@@ -80,7 +81,9 @@ public class MeasureController extends BaseController {
     public ResponseMessage<Page<TableSyncStaticVo>> querySyncStatic (@RequestParam String taskRecordId,
                                                                      @RequestParam(defaultValue = "1") Integer page,
                                                                      @RequestParam(defaultValue = "20") Integer size) {
-        return success(measurementServiceV2.querySyncStatic(new TableSyncStaticDto(taskRecordId, page, size), getLoginUser()));
+			UserDetail userDetail = getLoginUser();
+			userDetail.setFreeAuth();
+			return success(measurementServiceV2.querySyncStatic(new TableSyncStaticDto(taskRecordId, page, size), userDetail));
     }
 
     @Operation(summary = "表同步状态统计")
