96f8a8df57e3b6fc488fd31f8afd7aaca4e74abc
harsen <harsen@tapdata.io>
2023-08-24 16:19:50 +0800

    style: Show current product type of settings in start logs

--- manager/tm/src/main/java/com/tapdata/tm/config/component/ProductComponent.java
+++ manager/tm/src/main/java/com/tapdata/tm/config/component/ProductComponent.java
@@ -1,6 +1,8 @@
 package com.tapdata.tm.config.component;
 
 import com.tapdata.tm.Settings.service.SettingsService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +11,7 @@ import java.util.Objects;
 
 @Component
 public class ProductComponent {
+	private final static Logger logger = LogManager.getLogger(ProductComponent.class);
 
   @Autowired
   private SettingsService settingsService;
@@ -24,6 +27,8 @@ public class ProductComponent {
       buildProfile = "DAAS";
     }
     this.isCloud = buildProfile.equals("CLOUD") || buildProfile.equals("DRS") || buildProfile.equals("DFS");
+
+		logger.info("Current product type settings is {}.", buildProfile);
   }
 
   public boolean isCloud() {
