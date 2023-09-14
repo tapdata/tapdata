802a22385814503b6993df7b85713fc294a97098
ljvv7 <fes504200361@qq.com>
2023-09-14 17:21:30 +0800

    fix engine restart

--- manager/tm/src/main/java/com/tapdata/tm/worker/service/WorkerService.java
+++ manager/tm/src/main/java/com/tapdata/tm/worker/service/WorkerService.java
@@ -601,8 +601,9 @@ public class WorkerService extends BaseService<WorkerDto, Worker, ObjectId, Work
             throw new BizException("IllegalArgument", "processId or workerType can't be empty.");
         }
         if(worker.getPingTime()!=null&&worker.getPingTime()==1){
-            log.info("engine is stop");
+            log.info("The engine{} has stopped,WorkerType{}",worker.getProcessId(),worker.getWorkerType());
         }else{
+            log.info("The engine{} set pingTime,WorkerType{}",worker.getProcessId(),worker.getWorkerType());
             worker.setPingTime(System.currentTimeMillis());
         }
         Object buildProfile = settingsService.getByCategoryAndKey(CategoryEnum.SYSTEM, KeyEnum.BUILD_PROFILE).getValue();
