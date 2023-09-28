c45967922500e39970e13b3391cc9c7097815f89
sam.p <102567930@qq.com>
2023-09-28 18:06:11 +0800

    fix(iengine): Resume merge node clear data when reset task

--- iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/processor/HazelcastMergeNode.java
+++ iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/processor/HazelcastMergeNode.java
@@ -976,10 +976,9 @@ public class HazelcastMergeNode extends HazelcastProcessorBaseNode {
 	}
 
 	public static void clearCache(Node<?> node) {
-		// When the task is reset, cached data will not be automatically deleted. If you want to clean it, use manual methods
-		/*if (!(node instanceof MergeTableNode)) return;
+		if (!(node instanceof MergeTableNode)) return;
 		ExternalStorageDto externalStorage = ExternalStorageUtil.getExternalStorage(node);
-		recursiveClearCache(externalStorage, ((MergeTableNode) node).getMergeProperties(), HazelcastUtil.getInstance());*/
+		recursiveClearCache(externalStorage, ((MergeTableNode) node).getMergeProperties(), HazelcastUtil.getInstance());
 	}
 
 	private static void recursiveClearCache(ExternalStorageDto externalStorageDto, List<MergeTableProperties> mergeTableProperties, HazelcastInstance hazelcastInstance) {
