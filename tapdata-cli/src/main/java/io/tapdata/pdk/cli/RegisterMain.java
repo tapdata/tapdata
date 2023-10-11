1dd6591636332b6052c5fdb121a80ce5e9194c99
不掉发的程序员 <2749984520@qq.com>
2023-10-11 17:15:22 +0800

    fix(sybase): move sybase connector to enterprise

--- tapdata-cli/src/main/java/io/tapdata/pdk/cli/RegisterMain.java
+++ tapdata-cli/src/main/java/io/tapdata/pdk/cli/RegisterMain.java
@@ -83,7 +83,6 @@ public class RegisterMain {
         Ali1688(BASE_PATH + "connectors/dist/ali1688-connector-v1.0-SNAPSHOT.jar", "all", "ali1688"),
         Temu(BASE_PATH + "connectors/dist/temu-connector-v1.0-SNAPSHOT.jar", "all", "temu"),
         GreenPlum(BASE_PATH + "connectors/dist/greenplum-connector-v1.0-SNAPSHOT.jar", "all", "greenplum", "basic", "jdbc"),
-        Sybase(BASE_PATH + "connectors/dist/sybase-connector-v1.0-SNAPSHOT.jar", "all", "sybase", "basic", "jdbc"),
         ;
 
         private final String path;
