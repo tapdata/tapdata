0ca7704c0a79effde25ec813257368251b0833b9
sam.p <102567930@qq.com>
2023-10-17 17:16:42 +0800

    feat(tm): [HA] 数据源名字过长, 导致共享挖掘任务报错
    Closes #149022

---
+++ manager/tm-common/src/main/java/com/tapdata/tm/shareCdcTableMapping/ShareCdcTableMappingDto.java
@@ -0,0 +1,39 @@
+package com.tapdata.tm.shareCdcTableMapping;
+
+import com.tapdata.tm.commons.base.dto.BaseDto;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.apache.commons.lang3.StringUtils;
+
+
+/**
+ * shareCdcTableMapping
+ */
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Data
+public class ShareCdcTableMappingDto extends BaseDto {
+
+	public static final String VERSION_V1 = "v1";
+	public static final String VERSION_V2 = "v2";
+
+	private String sign;
+	private String version = VERSION_V1;
+	private String tableName;
+	private String externalStorageTableName;
+	private String shareCdcTaskId;
+	private String connectionId;
+
+	public String genSign() {
+		if (StringUtils.isBlank(shareCdcTaskId))
+			throw new IllegalArgumentException("Share cdc task id cannot be blank");
+		if (StringUtils.isBlank(connectionId)) throw new IllegalArgumentException("Connection id cannot be blank");
+		if (StringUtils.isBlank(tableName)) throw new IllegalArgumentException("Table name cannot be blank");
+		return String.join("_", shareCdcTaskId, connectionId, tableName);
+	}
+
+	public static String genSign(String taskId, String connId, String tableName) {
+		return String.join("_", taskId, connId, tableName);
+	}
+}
