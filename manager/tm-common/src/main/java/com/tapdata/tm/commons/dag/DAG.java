084f167a9e56212cf00618e8dc12af368633cce7
GavinXiao <2749984520@qq.com>
2023-10-30 16:42:23 +0800

    fix(disabled-node):remove useless class

--- manager/tm-common/src/main/java/com/tapdata/tm/commons/dag/DAG.java
+++ manager/tm-common/src/main/java/com/tapdata/tm/commons/dag/DAG.java
@@ -6,7 +6,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.tapdata.tm.commons.dag.check.DAGCheckUtil;
-import com.tapdata.tm.commons.dag.check.DAGNodeCheckItem;
 import com.tapdata.tm.commons.dag.nodes.DatabaseNode;
 import com.tapdata.tm.commons.dag.process.JoinProcessorNode;
 import com.tapdata.tm.commons.dag.process.MigrateProcessorNode;
