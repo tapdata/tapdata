6ed1574b9813fca0198970ee23f1b5fa0a269772
lemon <lemon@tapdata.io>
2023-11-07 13:06:35 +0800

    fix test scene

--- iengine/iengine-app/src/main/test/ConnectorNode/ConnectorNodeBase.java
+++ iengine/iengine-app/src/main/test/ConnectorNode/ConnectorNodeBase.java
@@ -10,37 +10,19 @@ import java.lang.reflect.Field;
 
 public class ConnectorNodeBase {
 
-    protected static ConnectorNode sqlConnectorNode = null;
+    protected  ConnectorNode sqlConnectorNode = null;
     protected static TapTable myTapTable = null;
 
     protected static ConnectorNode mongoConnectorNode = null;
 
-    @Before
-    public void initSqlConnectorNode() throws NoSuchFieldException, IllegalAccessException {
-        ConnectorNode connectorNode = new ConnectorNode();
+
+    public void initConnectorNode(ConnectorNode connectorNode,TapNodeSpecification tapNodeSpecification) throws NoSuchFieldException, IllegalAccessException {
         TapNodeInfo tapNodeInfo = new TapNodeInfo();
-        TapNodeSpecification tapNodeSpecification = new TapNodeSpecification();
-        tapNodeSpecification.setId("mysql");
         tapNodeInfo.setTapNodeSpecification(tapNodeSpecification);
         invokeValueForFiled(ConnectorNode.class,"tapNodeInfo",connectorNode,tapNodeInfo,true);
-        sqlConnectorNode = connectorNode;
-
-        TapTable table = new TapTable();
-        table.setId("testID");
-        table.setName("testID");
-        myTapTable = table;
     }
 
-    @Before
-    public void initMongodbConnectorNode() throws NoSuchFieldException, IllegalAccessException {
-        ConnectorNode connectorNode = new ConnectorNode();
-        TapNodeInfo tapNodeInfo = new TapNodeInfo();
-        TapNodeSpecification tapNodeSpecification = new TapNodeSpecification();
-        tapNodeSpecification.setId("mongodb");
-        tapNodeInfo.setTapNodeSpecification(tapNodeSpecification);
-        invokeValueForFiled(ConnectorNode.class,"tapNodeInfo",connectorNode,tapNodeInfo,true);
-        mongoConnectorNode = connectorNode;
-    }
+
 
 
     public void invokeValueForFiled(Class clazz, String filedName, ConnectorNode connectorNode, Object object, boolean superClass) throws NoSuchFieldException, IllegalAccessException {
