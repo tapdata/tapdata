38bfa5ff104ed871a9bff4b987b19389e1c64125
GavinXiao <2749984520@qq.com>
2023-11-08 10:30:49 +0800

    fix(PythonUtils): add two unit test case.

--- iengine/iengine-app/src/main/test/io/tapdata/observable/metric/py/PythonUtilsTest.java
+++ iengine/iengine-app/src/main/test/io/tapdata/observable/metric/py/PythonUtilsTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
@@ -41,14 +42,8 @@ public class PythonUtilsTest {
 
     @Test
     public void testGetPythonConfig() {
-        String map = "{\"key1\": \"name\", \"key2\": \"id\"}";
         File file = new File("temp.json");
-        try (BufferedWriter out = new BufferedWriter(new FileWriter(file))){
-            out.write(map);
-        } catch (Exception e) {
-            return;
-        }
-
+        createFile(file);
         try {
             Map<String, Object> pythonConfig = PythonUtils.getPythonConfig(file);
             Assert.assertEquals(2, pythonConfig.size());
@@ -56,6 +51,59 @@ public class PythonUtilsTest {
             Assert.assertTrue(pythonConfig.containsKey("key2"));
             Assert.assertEquals("name", pythonConfig.get("key1"));
             Assert.assertEquals("id", pythonConfig.get("key2"));
+        } finally {
+            PythonUtils.deleteFile(file, new TapLog());
+        }
+    }
+
+    private void createFile(File file) {
+        String map = "{\"key1\": \"name\", \"key2\": \"id\"}";
+        try (BufferedWriter out = new BufferedWriter(new FileWriter(file))){
+            out.write(map);
+        } catch (Exception ignore) { }
+    }
+
+    @Test
+    public void testCopyFile() {
+        File file = new File("temp.json");
+        try {
+            createFile(file);
+            if (file.exists()) {
+                File fileTargetPath = new File("temp");
+                try {
+                    try {
+                        PythonUtils.copyFile(file, fileTargetPath);
+                    } catch (Exception ignore) {}
+                    Assert.assertTrue(fileTargetPath.exists());
+                    Assert.assertTrue(fileTargetPath.isDirectory());
+                } finally {
+                    PythonUtils.deleteFile(fileTargetPath, new TapLog());
+                }
+            }
+        } finally {
+            PythonUtils.deleteFile(file, new TapLog());
+        }
+    }
+
+    @Test
+    public void testSaveTempFile() {
+        File file = new File("temp.json");
+        try {
+            createFile(file);
+            if (file.exists()) {
+                File fileTarget = new File("temp_1.json");
+                try (FileInputStream inputStream = new FileInputStream(file)) {
+                    try {
+                        PythonUtils.saveTempZipFile(inputStream, "temp_1.json", new TapLog());
+                    } catch (Exception ignore) {}
+                    Assert.assertTrue(fileTarget.exists());
+                    Assert.assertTrue(fileTarget.isFile());
+                } catch (IOException e) {
+
+                } finally {
+                    PythonUtils.deleteFile(fileTarget, new TapLog());
+                }
+            }
         } finally {
             PythonUtils.deleteFile(file, new TapLog());
         }
