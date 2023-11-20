8725e356981c652c3736e44f8ea793f88e61afe7
GavinXiao <2749984520@qq.com>
2023-11-20 15:24:43 +0800

    fix(QPS): write record with outputSizeSpeed

--- iengine/modules/observable-module/src/main/java/io/tapdata/observable/metric/handler/DataNodeSampleHandler.java
+++ iengine/modules/observable-module/src/main/java/io/tapdata/observable/metric/handler/DataNodeSampleHandler.java
@@ -337,7 +337,6 @@ public class DataNodeSampleHandler extends AbstractNodeSampleHandler {
 			average.add(total, processCompleteAt - streamProcessStartTs);
 		});
 		Optional.ofNullable(outputSizeSpeed).ifPresent(speed -> speed.add(recorder.getMemorySize()));
-		Optional.ofNullable(inputSizeSpeed).ifPresent(speed -> speed.add(recorder.getMemorySize()));
 	}
 
 
@@ -353,6 +352,7 @@ public class DataNodeSampleHandler extends AbstractNodeSampleHandler {
 		Optional.ofNullable(inputDeleteCounter).ifPresent(counter -> counter.inc(recorder.getDeleteTotal()));
 		Optional.ofNullable(inputOthersCounter).ifPresent(counter -> counter.inc(recorder.getOthersTotal()));
 		Optional.ofNullable(inputSpeed).ifPresent(speed -> speed.add(recorder.getTotal()));
+		Optional.ofNullable(inputSizeSpeed).ifPresent(speed -> speed.add(recorder.getMemorySize()));
 	}
 
 	public void handleCDCHeartbeatWriteAspect(List<TapdataEvent> tapdataEvents) {
