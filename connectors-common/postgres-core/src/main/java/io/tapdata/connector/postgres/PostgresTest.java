7e7db257657705cd03e64c4d2220ac932e92cc94
sam.p <102567930@qq.com>
2023-12-06 18:56:56 +0800

    fix(postgres): 作为目标时，测试连接中写入等权限检测改为非必须通过

--- connectors-common/postgres-core/src/main/java/io/tapdata/connector/postgres/PostgresTest.java
+++ connectors-common/postgres-core/src/main/java/io/tapdata/connector/postgres/PostgresTest.java
@@ -4,14 +4,13 @@ import com.google.common.collect.Lists;
 import io.tapdata.common.CommonDbTest;
 import io.tapdata.connector.postgres.config.PostgresConfig;
 import io.tapdata.entity.simplify.TapSimplify;
+import io.tapdata.kit.EmptyKit;
 import io.tapdata.kit.ErrorKit;
 import io.tapdata.pdk.apis.entity.TestItem;
 import org.postgresql.Driver;
 
 import java.sql.Connection;
-import java.util.List;
-import java.util.Properties;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
@@ -92,4 +91,30 @@ public class PostgresTest extends CommonDbTest {
     protected final static String PG_LOG_PLUGIN_CREATE_TEST = "SELECT pg_create_logical_replication_slot('%s','%s')";
     protected final static String PG_LOG_PLUGIN_DROP_TEST = "select pg_drop_replication_slot(a.slot_name) " +
             "from (select * from pg_replication_slots where slot_name like 'test_tapdata_%') a;";
+
+    @Override
+    protected Boolean testWritePrivilege() {
+        try {
+            List<String> sqls = new ArrayList<>();
+            String schemaPrefix = EmptyKit.isNotEmpty(commonDbConfig.getSchema()) ? ("\"" + commonDbConfig.getSchema() + "\".") : "";
+            if (jdbcContext.queryAllTables(Arrays.asList(TEST_WRITE_TABLE, TEST_WRITE_TABLE.toUpperCase())).size() > 0) {
+                sqls.add(String.format(TEST_DROP_TABLE, schemaPrefix + TEST_WRITE_TABLE));
+            }
+            //create
+            sqls.add(String.format(TEST_CREATE_TABLE, schemaPrefix + TEST_WRITE_TABLE));
+            //insert
+            sqls.add(String.format(TEST_WRITE_RECORD, schemaPrefix + TEST_WRITE_TABLE));
+            //update
+            sqls.add(String.format(TEST_UPDATE_RECORD, schemaPrefix + TEST_WRITE_TABLE));
+            //delete
+            sqls.add(String.format(TEST_DELETE_RECORD, schemaPrefix + TEST_WRITE_TABLE));
+            //drop
+            sqls.add(String.format(TEST_DROP_TABLE, schemaPrefix + TEST_WRITE_TABLE));
+            jdbcContext.batchExecute(sqls);
+            consumer.accept(testItem(TestItem.ITEM_WRITE, TestItem.RESULT_SUCCESSFULLY, TEST_WRITE_SUCCESS));
+        } catch (Exception e) {
+            consumer.accept(testItem(TestItem.ITEM_WRITE, TestItem.RESULT_SUCCESSFULLY_WITH_WARN, e.getMessage()));
+        }
+        return true;
+    }
 }
