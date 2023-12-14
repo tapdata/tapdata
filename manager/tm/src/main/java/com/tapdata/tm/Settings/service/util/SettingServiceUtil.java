468b3c8c8620125c752ba6488e1242cc2606b41d
GavinXiao <2749984520@qq.com>
2023-12-14 10:11:13 +0800

    fix(SettingServiceUtil): remove useless codes

--- manager/tm/src/main/java/com/tapdata/tm/Settings/service/util/SettingServiceUtil.java
+++ manager/tm/src/main/java/com/tapdata/tm/Settings/service/util/SettingServiceUtil.java
@@ -10,22 +10,14 @@ import java.util.Objects;
 
 public class SettingServiceUtil {
     public static List<SettingsDto> copyProperties(String decode, List<Settings> settingsList) {
+        boolean needDecode = !"1".equals(decode);
         List<SettingsDto> settingsDtoList = new ArrayList<>();
         if (null == settingsList || settingsList.isEmpty()) return settingsDtoList;
-        settingsList.stream().filter(Objects::nonNull).forEach(settings -> {
-            if (!"1".equals(decode)){
-                if("smtp.server.password".equals(settings.getKey())) {
-                    settings.setValue("*****");
-                }
-            } else {
-                //todo 解密方法
-                //settingsList.stream().filter(settings -> {
-                //    if ("smtp.server.password".equals(settings.getKey())) {
-                //        settings.setValue(EncrptAndDencryUtil.Decrypt(String.valueOf(settings.getValue())));
-                //        return true;
-                //    }
-                //    return false;
-                //}).collect(Collectors.toList());
+        settingsList.stream()
+                .filter(Objects::nonNull)
+                .forEach(settings -> {
+            if (needDecode && "smtp.server.password".equals(settings.getKey())) {
+                settings.setValue("*****");
             }
             SettingsDto settingsDto = BeanUtil.copyProperties(settings, SettingsDto.class);
             settingsDtoList.add(settingsDto);
