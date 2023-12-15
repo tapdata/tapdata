7419bbc7ba1d67789a5cd1de6fbc18f8d891d14d
sam.p <102567930@qq.com>
2023-12-15 14:57:28 +0800

    test(iengine-app): 单元测试，增加从resources文件夹读取json文件的工具方法

--- iengine/iengine-app/src/test/java/base/BaseTest.java
+++ iengine/iengine-app/src/test/java/base/BaseTest.java
@@ -1,12 +1,18 @@
 package base;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.tapdata.constant.ConfigurationCenter;
 import com.tapdata.constant.ConnectorConstant;
+import com.tapdata.constant.JSONUtil;
 import com.tapdata.mongo.ClientMongoOperator;
+import io.tapdata.MockTaskUtil;
 import io.tapdata.common.SettingService;
 import io.tapdata.observable.logging.ObsLogger;
 import io.tapdata.pdk.core.utils.CommonUtils;
 import org.junit.jupiter.api.BeforeAll;
+
+import java.io.IOException;
+import java.net.URL;
 
 import static org.mockito.Mockito.mock;
 
@@ -29,5 +35,33 @@ public abstract class BaseTest {
 		mockClientMongoOperator = mock(ClientMongoOperator.class);
 		mockSettingService = mock(SettingService.class);
 		mockConfigurationCenter = mock(ConfigurationCenter.class);
+	}
+
+	protected <T> T json2Pojo(String pathInResources, Class<T> toClazz) {
+		URL taskJsonFileURL = MockTaskUtil.class.getClassLoader().getResource(pathInResources);
+		if (null == taskJsonFileURL) {
+			throw new RuntimeException(String.format("Cannot get url: '%s', check your json file name and path is correct", pathInResources));
+		}
+		T t;
+		try {
+			t = JSONUtil.json2POJO(taskJsonFileURL, toClazz);
+		} catch (IOException e) {
+			throw new RuntimeException("Parse json string to [" + toClazz.getName() + "] failed, url: " + taskJsonFileURL, e);
+		}
+		return t;
+	}
+
+	protected <T> T json2Pojo(String pathInResources, TypeReference<T> typeReference) {
+		URL taskJsonFileURL = MockTaskUtil.class.getClassLoader().getResource(pathInResources);
+		if (null == taskJsonFileURL) {
+			throw new RuntimeException(String.format("Cannot get url: '%s', check your json file name and path is correct", pathInResources));
+		}
+		T t;
+		try {
+			t = JSONUtil.json2POJO(taskJsonFileURL, typeReference);
+		} catch (IOException e) {
+			throw new RuntimeException("Parse json string to [" + typeReference.getType().getTypeName() + "] failed, url: " + taskJsonFileURL, e);
+		}
+		return t;
 	}
 }
