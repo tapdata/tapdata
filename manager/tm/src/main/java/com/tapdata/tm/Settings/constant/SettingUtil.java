8ed341dc40805053bdde79f2d6fd549ea1c343fe
hardy <2545433047@qq.com>
2024-01-03 11:23:00 +0800

    fix: SettingUtil npe

--- manager/tm/src/main/java/com/tapdata/tm/Settings/constant/SettingUtil.java
+++ manager/tm/src/main/java/com/tapdata/tm/Settings/constant/SettingUtil.java
@@ -1,6 +1,7 @@
 package com.tapdata.tm.Settings.constant;
 
 import com.tapdata.tm.Settings.service.SettingsService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +15,9 @@ public class SettingUtil {
     }
 
     public static String getValue(String category, String key) {
-        return String.valueOf(settingsService.getByCategoryAndKey(category, key));
+        if(StringUtils.isBlank(category) || StringUtils.isBlank(key)) return null;
+        Object value = settingsService.getByCategoryAndKey(category, key);
+        return value != null ? String.valueOf(value):null ;
     }
 
 }
--- manager/tm/src/test/java/com/tapdata/tm/Settings/constant/SettingUtilTest.java
+++ manager/tm/src/test/java/com/tapdata/tm/Settings/constant/SettingUtilTest.java
@@ -21,4 +21,21 @@ class SettingUtilTest {
         when(service.getByCategoryAndKey(anyString(),anyString())).thenReturn("result");
         assertThat(SettingUtil.getValue("category", "key")).isEqualTo("result");
     }
+
+    @Test
+    void testGetValue_inputIsNull(){
+        assertThat(SettingUtil.getValue(null, "key")).isNull();
+        assertThat(SettingUtil.getValue("category", null)).isNull();
+        assertThat(SettingUtil.getValue(null, null)).isNull();
+        assertThat(SettingUtil.getValue("", "key")).isNull();
+        assertThat(SettingUtil.getValue("category", "")).isNull();
+        assertThat(SettingUtil.getValue("", "")).isNull();
+    }
+
+    @Test
+    void testGetValue_valueIsNull(){
+        SettingUtil settingUtil = new SettingUtil(service);
+        when(service.getByCategoryAndKey(anyString(),anyString())).thenReturn(null);
+        assertThat(SettingUtil.getValue("category", "key")).isNull();
+    }
 }
