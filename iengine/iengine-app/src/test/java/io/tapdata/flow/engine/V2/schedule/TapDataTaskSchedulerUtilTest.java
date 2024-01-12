6257741d21374f36730ff2b37dc1c80784c3430c
mnianqi <139107099+mnianqi@users.noreply.github.com>
2024-01-12 10:53:58 +0800

    fix: fix for retry start time
    Closes #150661

--- iengine/iengine-app/src/test/java/io/tapdata/flow/engine/V2/schedule/TapDataTaskSchedulerUtilTest.java
+++ iengine/iengine-app/src/test/java/io/tapdata/flow/engine/V2/schedule/TapDataTaskSchedulerUtilTest.java
@@ -29,7 +29,7 @@ public class TapDataTaskSchedulerUtilTest extends BaseTest {
             taskId = new ObjectId().toHexString();
             taskDto = mock(TaskDto.class);
             timestamp = System.currentTimeMillis();
-            when(taskDto.getTaskRetryStartTime()).thenReturn(timestamp);
+            when(taskDto.getTaskRetryStartTimeFlag()).thenReturn(timestamp);
             mongoOperator = mock(ClientMongoOperator.class);
             when(mongoOperator.findOne(any(Query.class), anyString(), any(Class.class))).thenReturn(taskDto);
         }
