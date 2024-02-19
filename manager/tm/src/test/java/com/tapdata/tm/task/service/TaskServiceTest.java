cc0943451b32cb5272ca1f234618dcd5c642faee
ljvv7 <fes504200361@qq.com>
2024-02-19 20:45:46 +0800

    fix(TM):rm import emtpy str

--- manager/tm/src/test/java/com/tapdata/tm/task/service/TaskServiceTest.java
+++ manager/tm/src/test/java/com/tapdata/tm/task/service/TaskServiceTest.java
@@ -968,6 +968,14 @@ public class TaskServiceTest {
             String targetPath = taskService.getEmbeddedDocumentPath("parentPath", setting);
             assertEquals("parentPath.abc",targetPath);
         }
+        @DisplayName("test parent path is not empty string,embeddedPaht is empty str")
+        @Test
+        void test4(){
+            Map<String,String> setting=new HashMap<>();
+            setting.put("embeddedPath","");
+            String targetPath = taskService.getEmbeddedDocumentPath("parentPath", setting);
+            assertEquals("parentPath",targetPath);
+        }
     }
 
 
