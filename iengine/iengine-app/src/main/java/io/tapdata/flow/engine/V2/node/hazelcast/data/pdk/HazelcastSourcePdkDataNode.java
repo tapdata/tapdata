2ba0f9bd964d7c9fc1d72b3652ba933ac4e32f26
jarad <coolgj@163.com>
2024-02-19 12:11:14 +0800

    fix: initial sync with Update and Delete event

--- iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastSourcePdkDataNode.java
+++ iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastSourcePdkDataNode.java
@@ -21,7 +21,10 @@ import io.tapdata.common.sharecdc.ShareCdcUtil;
 import io.tapdata.entity.codec.filter.TapCodecsFilterManager;
 import io.tapdata.entity.event.TapEvent;
 import io.tapdata.entity.event.ddl.index.TapCreateIndexEvent;
+import io.tapdata.entity.event.dml.TapDeleteRecordEvent;
 import io.tapdata.entity.event.dml.TapInsertRecordEvent;
+import io.tapdata.entity.event.dml.TapRecordEvent;
+import io.tapdata.entity.event.dml.TapUpdateRecordEvent;
 import io.tapdata.entity.schema.TapField;
 import io.tapdata.entity.schema.TapIndex;
 import io.tapdata.entity.schema.TapIndexField;
@@ -1102,10 +1105,10 @@ public class HazelcastSourcePdkDataNode extends HazelcastSourcePdkBaseNode {
 			return;
 		}
 		TapEvent lastEvent = tapEvents.get(tapEvents.size() - 1);
-		flushPollingCDCOffset((TapInsertRecordEvent) lastEvent);
+		flushPollingCDCOffset((TapRecordEvent) lastEvent);
 	}
 
-	private void flushPollingCDCOffset(TapInsertRecordEvent tapEvent) {
+	private void flushPollingCDCOffset(TapRecordEvent tapEvent) {
 		if (!isPollingCDC(getNode())) {
 			return;
 		}
@@ -1116,9 +1119,16 @@ public class HazelcastSourcePdkDataNode extends HazelcastSourcePdkBaseNode {
 			streamOffsetMap.put(tableName, new HashMap<>());
 		}
 		Map tablePollingCDCOffset = (Map) streamOffsetMap.get(tableName);
-		Map<String, Object> after = tapEvent.getAfter();
+		Map<String, Object> data;
+		if (tapEvent instanceof TapInsertRecordEvent) {
+			data = ((TapInsertRecordEvent) tapEvent).getAfter();
+		} else if (tapEvent instanceof TapUpdateRecordEvent) {
+			data = ((TapUpdateRecordEvent) tapEvent).getAfter();
+		} else {
+			data = ((TapDeleteRecordEvent) tapEvent).getBefore();
+		}
 		for (String conditionField : conditionFields) {
-			Object value = after.get(conditionField);
+			Object value = data.get(conditionField);
 			tablePollingCDCOffset.put(conditionField, value);
 		}
 		TapCodecsFilterManager connecotrCodecsFilterManger = getConnectorNode().getCodecsFilterManager();
