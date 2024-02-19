99ca7a08f2053f3d3e3a156b4a3eedf88b9914bc
ljvv7 <fes504200361@qq.com>
2024-02-19 14:11:10 +0800

    fix(TM):add unit case

--- manager/tm/src/main/java/com/tapdata/tm/task/service/TaskService.java
+++ manager/tm/src/main/java/com/tapdata/tm/task/service/TaskService.java
@@ -2950,28 +2950,34 @@ public class TaskService extends BaseService<TaskDto, TaskEntity, ObjectId, Task
                     joinKeys.add(joinKey);
                 }
             }
-            for (String columnKey : parentColumns.keySet()) {
-                Map<String, Object> column = (Map<String, Object>) parentColumns.get(columnKey);
-                Map<String, Object> foreignKey = (Map<String, Object>) column.get("foreignKey");
-                if (foreignKey == null) {
-                    continue;
-                }
-                if (((String) foreignKey.get("table")).equals(tpTable)) {
-                    Map<String, String> joinKey = new HashMap<>();
-                    joinKey.put("source", renameFields.get(tpTable).get(((String) foreignKey.get("column"))).get("target").toString());
-                    if (parent.get("targetPath").equals("")) {
-                        joinKey.put("target", parentRenameFields.get(columnKey).get("target").toString());
-                    } else {
-                        joinKey.put("target", parent.get("targetPath") + "." + currentRenameFields.get((String) foreignKey.get("column")).get("target").toString());
-                    }
-                    joinKeys.add(joinKey);
-                }
-            }
+            parentColumnsFindJoinKeys(parent, renameFields, parentColumns, tpTable, joinKeys);
             childNode.put("joinKeys", joinKeys);
             genProperties(childNode, contentMapping, relationshipsMapping, full, sourceToJS, renameFields);
             childrenNode.add(childNode);
         }
         parent.put("children", childrenNode);
+    }
+
+    protected void parentColumnsFindJoinKeys(Map<String, Object> parent, Map<String, Map<String, Map<String, Object>>> renameFields, Map<String, Object> parentColumns, String tpTable, List<Map<String, String>> joinKeys) {
+        Map<String, Map<String, Object>> parentRenameFields = renameFields.get((String) parent.get("tableName"));
+        for (String columnKey : parentColumns.keySet()) {
+            Map<String, Object> column = (Map<String, Object>) parentColumns.get(columnKey);
+            Map<String, Object> foreignKey = (Map<String, Object>) column.get("foreignKey");
+            if (foreignKey == null) {
+                continue;
+            }
+
+            if (((String) foreignKey.get("table")).equals(tpTable)) {
+                Map<String, String> joinKey = new HashMap<>();
+                joinKey.put("source", renameFields.get(tpTable).get(((String) foreignKey.get("column"))).get("target").toString());
+                if (parent.get("targetPath").equals("")) {
+                    joinKey.put("target", parentRenameFields.get(columnKey).get("target").toString());
+                } else {
+                    joinKey.put("target", parent.get("targetPath") + "." + parentRenameFields.get(columnKey).get("target").toString());
+                }
+                joinKeys.add(joinKey);
+            }
+        }
     }
 
     private String replaceId(String id, Map<String, String> globalIdMap) {
