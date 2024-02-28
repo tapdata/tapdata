88cf9195f17aa706d29155a7ae832869c4979951
mnianqi <139107099+mnianqi@users.noreply.github.com>
2024-02-28 15:54:14 +0800

    fix: refresh pdk connector jar when exists file but not complete
    Closes #151194

--- iengine/iengine-common/src/main/java/io/tapdata/flow/engine/V2/util/PdkUtil.java
+++ iengine/iengine-common/src/main/java/io/tapdata/flow/engine/V2/util/PdkUtil.java
@@ -121,16 +121,13 @@ public class PdkUtil {
 			return null;
 		}
 		MessageDigest digest = null;
-		FileInputStream in = null;
 		byte buffer[] = new byte[1024];
 		int len;
-		try{
+		try (FileInputStream in = new FileInputStream(file)){
 			digest = MessageDigest.getInstance("MD5");
-			in = new FileInputStream(file);
 			while(-1 != (len = in.read(buffer,0,1024))){
 				digest.update(buffer,0,len);
 			}
-			in.close();
 		}catch(Exception e){
 			e.printStackTrace();
 			return null;
