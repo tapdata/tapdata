c30a34bf3759d79a89a4690101136c30b6415c22
GavinXiao <2749984520@qq.com>
2024-03-11 23:23:56 +0800

    feat(hash-verify): hash by async

---
+++ iengine/iengine-app/src/main/java/io/tapdata/inspect/util/InspectJobUtil.java
@@ -0,0 +1,40 @@
+package io.tapdata.inspect.util;
+
+import com.tapdata.constant.ConnectorConstant;
+import com.tapdata.entity.inspect.InspectDataSource;
+import com.tapdata.tm.commons.util.MetaType;
+import io.tapdata.entity.schema.TapTable;
+import io.tapdata.entity.utils.DataMap;
+import io.tapdata.inspect.InspectTaskContext;
+import io.tapdata.pdk.apis.entity.QueryOperator;
+import io.tapdata.pdk.apis.entity.TapAdvanceFilter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InspectJobUtil {
+
+    public static TapAdvanceFilter wrapFilter(List<QueryOperator> srcConditions) {
+        TapAdvanceFilter tapAdvanceFilter = TapAdvanceFilter.create();
+        tapAdvanceFilter.setOperators(srcConditions);
+        DataMap match = new DataMap();
+        if (null != srcConditions) {
+            srcConditions.stream().filter(op-> op.getOperator() == 5).forEach(op->match.put(op.getKey(), op.getValue()));
+        }
+        tapAdvanceFilter.setMatch(match);
+        return tapAdvanceFilter;
+    }
+
+    public static TapTable getTapTable(InspectDataSource inspectDataSource, InspectTaskContext inspectTaskContext) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("connectionId", inspectDataSource.getConnectionId());
+        params.put("metaType", MetaType.table.name());
+        params.put("tableName", inspectDataSource.getTable());
+        TapTable tapTable = null == inspectTaskContext ? null : inspectTaskContext.getClientMongoOperator().findOne(params, ConnectorConstant.METADATA_INSTANCE_COLLECTION + "/metadata/v2", TapTable.class);
+        if (null == tapTable) {
+            tapTable = new TapTable(inspectDataSource.getTable());
+        }
+        return tapTable;
+    }
+}
