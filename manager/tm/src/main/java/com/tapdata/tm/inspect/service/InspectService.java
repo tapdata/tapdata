4102f9a17f3458d6bcbada8811fc6cefcb4d4614
GavinXiao <2749984520@qq.com>
2024-03-18 10:58:26 +0800

    fix(hash-verify): remove useless code

--- manager/tm/src/main/java/com/tapdata/tm/inspect/service/InspectService.java
+++ manager/tm/src/main/java/com/tapdata/tm/inspect/service/InspectService.java
@@ -630,7 +630,6 @@ public class InspectService extends BaseService<InspectDto, InspectEntity, Objec
             String json = JsonUtil.toJsonUseJackson(inspectDto);
             Map<String, Object> data = JsonUtil.parseJson(json, Map.class);
             data.put("type", "data_inspect");
-            data.put("timing_cache", data.get("timing"));
             data.remove("timing");
             //data里面要放需要校验的数据
             MessageQueueDto messageQueueDto = new MessageQueueDto();
