6e7de30112fef8fed2e769a0db3a4a52ffa064df
ljvv7 <fes504200361@qq.com>
2024-03-19 13:56:24 +0800

    fix:remove non-use import

--- iengine/api/src/main/java/io/tapdata/loglistener/TapLogger.java
+++ iengine/api/src/main/java/io/tapdata/loglistener/TapLogger.java
@@ -1,9 +1,6 @@
 package io.tapdata.loglistener;
 
-import com.tapdata.tm.commons.schema.MonitoringLogsDto;
-import jdk.jfr.internal.LogTag;
 
-import java.util.concurrent.Callable;
 
 public interface TapLogger {
     void info(String message, Object... params);
