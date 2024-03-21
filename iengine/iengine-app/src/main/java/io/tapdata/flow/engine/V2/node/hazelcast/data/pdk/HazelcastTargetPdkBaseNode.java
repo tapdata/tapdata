2eb4d6474aff1de86d3006851cfad325241f0bc3
jarad0628 <coolgj@163.com>
2024-03-21 23:42:49 +0800

    fix: #151147 compare with pks and uniqueConditions

--- iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastTargetPdkBaseNode.java
+++ iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastTargetPdkBaseNode.java
@@ -1177,17 +1177,20 @@ public abstract class HazelcastTargetPdkBaseNode extends HazelcastPdkBaseNode {
 		}
 	}
 
-	protected boolean usePkAsUpdateConditions(Collection<String> updateConditions, Collection<String> pks) {
-		if (pks == null) {
-			pks = Collections.emptySet();
-		}
-		for (String updateCondition : updateConditions) {
-			if (!pks.contains(updateCondition)) {
-				return false;
-			}
-		}
-		return true;
-	}
+    protected boolean usePkAsUpdateConditions(Collection<String> updateConditions, Collection<String> pks) {
+        if (pks == null) {
+            pks = Collections.emptySet();
+        }
+        if (pks.size() != updateConditions.size()) {
+            return false;
+        }
+        for (String updateCondition : updateConditions) {
+            if (!pks.contains(updateCondition)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 	protected CheckExactlyOnceWriteEnableResult enableExactlyOnceWrite() {
 		// Check whether the table supports exactly once write
