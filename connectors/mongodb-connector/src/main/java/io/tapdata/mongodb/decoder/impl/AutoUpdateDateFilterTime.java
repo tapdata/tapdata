3e1bff8073a4517a3a012a6dc8f822be5ed192a0
GavinXiao <2749984520@qq.com>
2024-03-21 18:38:38 +0800

    feat(custom-field-verify): #151275 Data validation supports automatic value updates for filtering conditions

--- connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/decoder/impl/AutoUpdateDateFilterTime.java
+++ connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/decoder/impl/AutoUpdateDateFilterTime.java
@@ -2,8 +2,8 @@ package io.tapdata.mongodb.decoder.impl;
 
 import io.tapdata.entity.error.CoreException;
 import io.tapdata.mongodb.decoder.CustomSQLObject;
-import io.tapdata.util.DateUtil;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
@@ -11,47 +11,37 @@ import java.util.Optional;
 
 public class AutoUpdateDateFilterTime implements CustomSQLObject<Object, Map<String, Object>> {
     public static final String DYNAMIC_DATE = "$dynamicDate";
-    public static final String FORMAT = "format";
-    public static final String TO_STRING = "toString";
+    public static final String FORMAT = "customFormat";
+    public static final String TO_STRING_FORMAT = "toStringFormat";
     public static final String SUBTRACT = "subtract";
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
 
     @Override
     public Object execute(Object functionObj, Map<String, Object> curMap) {
-        Object filter = null;
-        boolean toString = false;
-        long subtract = 0;
+        Object format = null;
+        Object toStringFormat = false;
+        int subtract = 0;
         if (functionObj instanceof Map) {
-            filter = ((Map<String, Object>) functionObj).get(FORMAT);
-            toString = Boolean.TRUE.equals(((Map<String, Object>) functionObj).get(TO_STRING));
+            format = ((Map<String, Object>) functionObj).get(FORMAT);
+            toStringFormat = ((Map<String, Object>) functionObj).get(TO_STRING_FORMAT);
             Object sub = Optional.ofNullable(((Map<String, Object>) functionObj).get(SUBTRACT)).orElse(0);
             if (sub instanceof Number) {
-                subtract = ((Number)sub).longValue();
+                subtract = ((Number)sub).intValue();
             } else {
                 try {
-                    subtract = Long.parseLong(String.valueOf(sub));
+                    subtract = Integer.parseInt(String.valueOf(sub));
                 } catch (Exception ignore) {
-                    throw new IllegalArgumentException("a subtract value is illegal value, \"subtract\" must be a number");
+                    throw new IllegalArgumentException("a subtract value is illegal value, \"" + SUBTRACT + "\" must be a number");
                 }
             }
         }
-        if (null == filter) {
-            throw new IllegalArgumentException("a format value is empty, \"format\" is a required field");
+        if (null != format && !(format instanceof String)) {
+            throw new IllegalArgumentException("a customFormat value is illegal value, \"" + FORMAT + "\" must be a date time format string, such as: \"yyyy-MM-dd HH:mm:ss\"");
         }
-
-
-        Long timestamp = null;
-        String filterValue = String.valueOf(filter);
-
-        if (filter instanceof Number) {
-            timestamp = ((Number) filter).longValue();
-        } else if (filterValue.matches("^[0-9]*$")) {
-            timestamp = Long.parseLong(filterValue);
+        if (null != toStringFormat && !(toStringFormat instanceof String)) {
+            throw new IllegalArgumentException("a toStringFormat value is illegal value, \"" + TO_STRING_FORMAT + "\" must be a date time format string, such as: \"yyyy-MM-dd HH:mm:ss\"");
         }
-
-        if (null == timestamp) {
-            return covertTime(filterValue, subtract, toString);
-        }
-        return covertTimestamp(timestamp, subtract);
+        return covertTime((String) format, subtract, (String) toStringFormat);
     }
 
     @Override
@@ -59,61 +49,32 @@ public class AutoUpdateDateFilterTime implements CustomSQLObject<Object, Map<Str
         return DYNAMIC_DATE;
     }
 
-    protected Object covertTimestamp(Long dateTime, long subtract) {
-        if (null == dateTime) {
-            throw new CoreException("Illegal argument in function: {}, wrong value: {}, the correct key value pairs should be as follows: \"\": {an timestamp which more than zero or a data time string}",
-                    getFunctionName(), dateTime, getFunctionName());
-        }
-        return covert(new Date(dateTime), subtract);
-    }
-
-    protected Object covertTime(String dateTime, long subtract, boolean toString) {
-        dateTime = replaceDate(dateTime);
-        String dateFormat = DateUtil.determineDateFormat(dateTime);
-        if (null == dateFormat) {
-            throw new CoreException("Illegal argument in function: {}, wrong value: {}, the correct key value pairs should be as follows: \"\": {\"yyyy-dd-MM hh:mm:ss[.SSSSSS]\"}",
-                    getFunctionName(), dateTime, getFunctionName());
-        }
-        Object parse = DateUtil.parse(dateTime);
-        Date covert = covert(parse, subtract);
-        String dateStr = DateUtil.timeStamp2Date(String.valueOf(covert.getTime()), dateFormat);
-        if (toString) {
-            return dateStr;
-        }
-        return covert;
-    }
-
-    protected String replaceDate(String format) {
+    protected Object covertTime(String dateTime, int subtract, String toStringFormat) {
         Calendar calendar = Calendar.getInstance();
-        format = format(format, "%y", String.valueOf(calendar.get(Calendar.YEAR)));
-        format = format(format, "%M", String.valueOf(calendar.get(Calendar.MONTH)+1));
-        format = format(format, "%d", String.valueOf(calendar.get(Calendar.DATE)));
-        format = format(format, "%h", String.valueOf(calendar.get(Calendar.HOUR)));
-        format = format(format, "%m", String.valueOf(calendar.get(Calendar.YEAR)));
-        format = format(format, "%s", String.valueOf(calendar.get(Calendar.SECOND)));
-        format = format(format, "%S", String.valueOf(calendar.get(Calendar.MILLISECOND)));
-        return format;
-    }
-
-    protected String format(String str, String format, String value) {
-        if (str.contains(format)) {
-            str = str.replace(format, value);
+        calendar.add(Calendar.MILLISECOND, -subtract);
+        Date formatDate = calendar.getTime();
+        if (null != dateTime) {
+            try {
+                SimpleDateFormat sdf = new SimpleDateFormat(dateTime);
+                String dateFormatStr = sdf.format(formatDate);
+                sdf.applyPattern(DEFAULT_DATE_FORMAT);
+                formatDate = sdf.parse(dateFormatStr);
+            } catch (Exception e) {
+                throw new CoreException("Illegal argument in function: {}, wrong value: {}, the correct key value pairs should be as follows: \"{}\": \"yyyy-dd-MM hh:mm:ss[.SSSSSS]\", error message: {}",
+                        getFunctionName(), dateTime, FORMAT, e.getMessage());
+            }
         }
-        return str;
-    }
 
-    protected Date covert(Object parse, long subtract) {
-        if (parse instanceof Date) {
-            return calculateDate((Date)parse, subtract);
-        } else {
-            throw new CoreException("UNKnow date time format string: " + parse);
+        if (null != toStringFormat) {
+            try {
+                SimpleDateFormat sdf = new SimpleDateFormat(toStringFormat);
+                return sdf.format(formatDate);
+            } catch (Exception e) {
+                throw new CoreException("Illegal argument in function: {}, {}'s wrong value: {}, error message: {}",
+                        getFunctionName(), toStringFormat, TO_STRING_FORMAT, e.getMessage());
+            }
         }
+        return formatDate;
     }
 
-    protected Date calculateDate(Date parse, long subtract) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(parse);
-        calendar.add(Calendar.MILLISECOND, -1 * ((Long)subtract).intValue());
-        return calendar.getTime();
-    }
 }
