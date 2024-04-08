6be4aea74f394d799f306e5d3c7cc75463838e23
GavinXiao <2749984520@qq.com>
2024-04-08 22:23:15 +0800

    fix(agent-group): unit test faild

--- manager/tm/src/test/java/com/tapdata/tm/task/service/TaskServiceImplTest.java
+++ manager/tm/src/test/java/com/tapdata/tm/task/service/TaskServiceImplTest.java
@@ -4,12 +4,13 @@ import com.tapdata.tm.agent.service.AgentGroupService;
 import com.tapdata.tm.commons.task.dto.Message;
 import com.tapdata.tm.commons.task.dto.TaskDto;
 import com.tapdata.tm.config.security.UserDetail;
-import com.tapdata.tm.utils.Lists;
+import com.tapdata.tm.task.service.utils.TaskServiceUtil;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
@@ -21,6 +22,7 @@ import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -51,18 +53,6 @@ class TaskServiceImplTest {
             when(taskDto.getId()).thenReturn(mock(ObjectId.class));
 
             when(taskService.findById(any(ObjectId.class))).thenReturn(temp);
-            when(taskDto.getAccessNodeType()).thenReturn(null);
-
-            when(taskDto.getAccessNodeProcessId()).thenReturn("id");
-
-            when(temp.getAccessNodeType()).thenReturn(null);
-            doNothing().when(taskDto).setAccessNodeType(null);
-
-            when(temp.getAccessNodeProcessId()).thenReturn(null);
-            doNothing().when(taskDto).setAccessNodeProcessId(null);
-
-            when(agentGroupService.getProcessNodeListWithGroup(temp, user)).thenReturn(mock(List.class));
-            doNothing().when(taskDto).setAccessNodeProcessIdList(anyList());
 
             doNothing().when(taskService).checkTaskInspectFlag(taskDto);
             doNothing().when(taskService).checkDagAgentConflict(taskDto,user,true);
@@ -72,23 +62,14 @@ class TaskServiceImplTest {
             when(taskService.confirmById(taskDto, user, true)).thenCallRealMethod();
         }
 
-        void assertVerify(int getIdTimes, int findByIdTimes,
-                          int getAccessNodeTypeTimes,
-                          int getAndSetTimes) {
-            Assertions.assertDoesNotThrow(() -> taskService.confirmById(taskDto, user, true));
+        void assertVerify(int getIdTimes, int findByIdTimes) {
+            try(MockedStatic<TaskServiceUtil> tsu = mockStatic(TaskServiceUtil.class)) {
+                tsu.when(() -> TaskServiceUtil.copyAccessNodeInfo(temp, taskDto, user, agentGroupService)).then(a->null);
+                Assertions.assertDoesNotThrow(() -> taskService.confirmById(taskDto, user, true));
+                tsu.verify(() -> TaskServiceUtil.copyAccessNodeInfo(temp, taskDto, user, agentGroupService), times(findByIdTimes));
+            }
             verify(taskDto, times(getIdTimes)).getId();
             verify(taskService, times(findByIdTimes)).findById(any(ObjectId.class));
-            verify(taskDto, times(getAccessNodeTypeTimes)).getAccessNodeType();
-
-            verify(temp, times(getAndSetTimes)).getAccessNodeType();
-            verify(taskDto, times(getAndSetTimes)).setAccessNodeType(null);
-
-            verify(temp, times(getAndSetTimes)).getAccessNodeProcessId();
-            verify(taskDto, times(getAndSetTimes)).setAccessNodeProcessId(null);
-
-            verify(agentGroupService, times(getAndSetTimes)).getProcessNodeListWithGroup(temp, user);
-            verify(taskDto, times(getAndSetTimes)).setAccessNodeProcessIdList(anyList());
-
             verify(taskService, times(1)).checkTaskInspectFlag(taskDto);
             verify(taskService, times(1)).checkDagAgentConflict(taskDto, user, true);
             verify(taskService, times(1)).checkDDLConflict(taskDto);
@@ -97,22 +78,17 @@ class TaskServiceImplTest {
 
         @Test
         void testNormal() {
-            assertVerify(2, 1, 1, 1);
+            assertVerify(2, 1);
         }
         @Test
         void testNullObjectId() {
             when(taskDto.getId()).thenReturn(null);
-            assertVerify(1, 0, 0, 0);
-        }
-        @Test
-        void testNullTemp() {
-            when(taskService.findById(any(ObjectId.class))).thenReturn(null);
-            assertVerify(2, 1, 0, 0);
+            assertVerify(1, 0);
         }
         @Test
         void testGetAccessNodeTypeNotBlank() {
             when(taskDto.getAccessNodeType()).thenReturn("type");
-            assertVerify(2, 1, 1, 0);
+            assertVerify(2, 1);
         }
     }
 
