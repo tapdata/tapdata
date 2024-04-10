786b72c97ba0a084e6112e82da49e8c7b210e86e
hardy <2545433047@qq.com>
2024-04-10 18:13:25 +0800

    TAP-2127 After the connection is created it is invalid and cannot be used in the task.You need to manually test the connection again

--- manager/tm/src/test/java/com/tapdata/tm/ds/service/impl/DataSourceServiceImplTest.java
+++ manager/tm/src/test/java/com/tapdata/tm/ds/service/impl/DataSourceServiceImplTest.java
@@ -1,15 +1,30 @@
 package com.tapdata.tm.ds.service.impl;
 
 
+import com.tapdata.tm.Settings.constant.SettingUtil;
 import com.tapdata.tm.commons.schema.DataSourceConnectionDto;
+import com.tapdata.tm.config.security.UserDetail;
+import com.tapdata.tm.discovery.service.DefaultDataDirectoryService;
+import com.tapdata.tm.ds.entity.DataSourceEntity;
+import com.tapdata.tm.ds.repository.DataSourceRepository;
+import com.tapdata.tm.permissions.DataPermissionHelper;
+import com.tapdata.tm.worker.service.WorkerService;
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.doNothing;
@@ -77,5 +92,56 @@ class DataSourceServiceImplTest {
             verify(updateDto, times(1)).isAccessNodeTypeEmpty();
         }
 
+    }
+    @Nested
+    class Copy{
+        DataSourceRepository dataSourceRepository = mock(DataSourceRepository.class);
+        DataSourceServiceImpl dataSourceService = new DataSourceServiceImpl(dataSourceRepository);
+        WorkerService workerService;
+        @BeforeEach
+        void before(){
+            workerService = mock(WorkerService.class);
+            ReflectionTestUtils.setField(dataSourceService,"workerService",workerService);
+            ReflectionTestUtils.setField(dataSourceService,"defaultDataDirectoryService",mock(DefaultDataDirectoryService.class));
+        }
+        @Test
+        void test(){
+            try(MockedStatic<SettingUtil> settingUtilMockedStatic = Mockito.mockStatic(SettingUtil.class);
+                MockedStatic<DataPermissionHelper> dataPermissionHelperMockedStatic = Mockito.mockStatic(DataPermissionHelper.class)){
+                settingUtilMockedStatic.when(()->SettingUtil.getValue(anyString(),anyString())).thenReturn("true");
+                DataSourceEntity dataSourceEntity = new DataSourceEntity();
+                dataSourceEntity.setConfig(new HashMap<>());
+                when(dataSourceRepository.findById(any(),any(UserDetail.class))).thenReturn(Optional.of(dataSourceEntity));
+                when(dataSourceRepository.save(any(),any())).thenReturn(mock(DataSourceEntity.class));
+                dataPermissionHelperMockedStatic.when(()->DataPermissionHelper.convert(any(),any())).thenAnswer(invocationOnMock -> {
+                    return null;
+                });
+                when(workerService.findAvailableAgent(any())).thenReturn(null);
+                DataSourceConnectionDto result = dataSourceService.copy(mock(UserDetail.class),new ObjectId().toHexString(),"test");
+                Assertions.assertNull(result.getConfig());
+            }
+
+        }
+    }
+
+    @Nested
+    class add{
+        DataSourceRepository dataSourceRepository = mock(DataSourceRepository.class);
+        DataSourceServiceImpl dataSourceService = mock(DataSourceServiceImpl.class, Answers.CALLS_REAL_METHODS);
+        WorkerService workerService;
+        @BeforeEach
+        void before(){
+            workerService = mock(WorkerService.class);
+            ReflectionTestUtils.setField(dataSourceService,"workerService",workerService);
+            ReflectionTestUtils.setField(dataSourceService,"repository",dataSourceRepository);
+            ReflectionTestUtils.setField(dataSourceService,"defaultDataDirectoryService",mock(DefaultDataDirectoryService.class));
+        }
+        @Test
+        void test() {
+            doNothing().when(dataSourceService).beforeSave(any(),any());
+            when(dataSourceRepository.save(any(),any())).thenReturn(mock(DataSourceEntity.class));
+            DataSourceConnectionDto result = dataSourceService.add(new DataSourceConnectionDto(),mock(UserDetail.class));
+            Assertions.assertNull(result.getConfig());
+        }
     }
 }
