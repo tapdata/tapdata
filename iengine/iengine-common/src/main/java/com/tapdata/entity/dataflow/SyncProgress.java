8c706d579f3cb6faf5359d39b9cc08b91d9fb7df
GavinXiao <2749984520@qq.com>
2024-04-10 15:17:48 +0800

    feat(table-offset): add unit test case

--- iengine/iengine-common/src/main/java/com/tapdata/entity/dataflow/SyncProgress.java
+++ iengine/iengine-common/src/main/java/com/tapdata/entity/dataflow/SyncProgress.java
@@ -13,8 +13,8 @@ public class SyncProgress implements Serializable, Comparable<SyncProgress> {
 	private static final long serialVersionUID = 5599838762323297718L;
 	public static final String TASK_BATCH_TABLE_OFFSET_POINT = "task_batch_table_offset_point";
 	public static final String TASK_BATCH_TABLE_OFFSET_STATUS = "task_batch_table_offset_status";
-	public static final String TABLE_BATCH_STATUS_OVER = "over";
-	public static final String TABLE_BATCH_STATUS_RUNNING = "running";
+	public static final String OVER = "over";
+	public static final String RUNNING = "running";
 
 	@Deprecated
 	private String offset;
@@ -156,27 +156,31 @@ public class SyncProgress implements Serializable, Comparable<SyncProgress> {
 	}
 
 	public boolean batchIsOverOfTable(String tableId) {
-		if (null == batchOffsetObj) return false;
-		Object batchOffset = ((Map<String, Object>) batchOffsetObj).get(tableId);
-		if (batchOffset instanceof Map && ((Map<String, Object>)batchOffset).containsKey(TASK_BATCH_TABLE_OFFSET_STATUS)) {
+		Object batchOffset = getTableOffsetInfo(tableId);
+		if (isBatchOffsetMap(batchOffset, TASK_BATCH_TABLE_OFFSET_STATUS)) {
 			//86迭代新功能--全量表同步断点
-			Map<String, Object> offsetInfo = (Map<String, Object>)batchOffset;
-			return TABLE_BATCH_STATUS_OVER.equals(offsetInfo.get(TASK_BATCH_TABLE_OFFSET_STATUS));
+			return OVER.equals(((Map<?, ?>)batchOffset).get(TASK_BATCH_TABLE_OFFSET_STATUS));
 		}
-
 		//历史数据
 		return false;
 	}
 
-	public Object getBatchOffsetOfTable(String tableId) {
-		if (null == batchOffsetObj) {
-			return null;
+	protected Object getTableOffsetInfo(String tableId) {
+		if (batchOffsetObj instanceof Map) {
+			return ((Map<?, ?>) batchOffsetObj).get(tableId);
 		}
-		Object batchOffset = ((Map<String, Object>) batchOffsetObj).get(tableId);
-		if (batchOffset instanceof Map && ((Map<String, Object>)batchOffset).containsKey(TASK_BATCH_TABLE_OFFSET_POINT)) {
+		return batchOffsetObj;
+	}
+
+	protected boolean isBatchOffsetMap(Object batchOffset, String key) {
+		return batchOffset instanceof Map && ((Map<?, ?>)batchOffset).containsKey(key);
+	}
+
+	public Object getBatchOffsetOfTable(String tableId) {
+		Object batchOffset = getTableOffsetInfo(tableId);
+		if (isBatchOffsetMap(batchOffset, TASK_BATCH_TABLE_OFFSET_POINT)) {
 			//86迭代新功能--全量表同步断点
-			Map<String, Object> offsetInfo = (Map<String, Object>) batchOffset;
-			return offsetInfo.get(TASK_BATCH_TABLE_OFFSET_POINT);
+			return ((Map<?, ?>) batchOffset).get(TASK_BATCH_TABLE_OFFSET_POINT);
 		}
 
 		//历史数据
@@ -184,20 +188,20 @@ public class SyncProgress implements Serializable, Comparable<SyncProgress> {
 	}
 
 	public void updateBatchOffset(String tableId, Object offset, String isOverTag) {
-		Map<String, Object> batchOffsetObjTemp = (Map<String, Object>) putIfAbsentBatchOffsetObj();
+		Map<String, Object> batchOffsetObjTemp = putIfAbsentBatchOffsetObj();
 		Map<String, Object> tableOffsetObjTemp = (Map<String, Object>) batchOffsetObjTemp.computeIfAbsent(tableId, k -> new HashMap<String, Object>());
 		tableOffsetObjTemp.put(TASK_BATCH_TABLE_OFFSET_POINT, offset);
 		tableOffsetObjTemp.put(TASK_BATCH_TABLE_OFFSET_STATUS, isOverTag);
 	}
 
-	public Object putIfAbsentBatchOffsetObj() {
+	public Map<String, Object> putIfAbsentBatchOffsetObj() {
 		Map<String, Object> batchOffsetObjTemp = (Map<String, Object>) batchOffsetObj;
 		if (null == batchOffsetObjTemp) {
 			batchOffsetObjTemp = new HashMap<>();
 			setBatchOffsetObj(batchOffsetObjTemp);
 		}
 
-		return batchOffsetObj;
+		return batchOffsetObjTemp;
 	}
 
 	public Object getBatchOffsetObj() {
