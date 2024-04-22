0afc99ac7107990454f730b170672b3b3d365a7d
ljvv7 <fes504200361@qq.com>
2024-04-22 16:00:28 +0800

    feat(tm):remove guangFa not use code

--- manager/tm-common/src/main/java/com/tapdata/tm/utils/GuangFaUtils.java
+++ manager/tm-common/src/main/java/com/tapdata/tm/utils/GuangFaUtils.java
@@ -73,39 +73,6 @@ public class GuangFaUtils {
 		return false;
 	}
 
-	public static void generateSourceMetadataInstances(List<MetadataInstancesDto> metadataList, Node node, DataSourceConnectionDto sourceConnectionDto, List<String> customKafkaQualifiedNames) {
-		if (null == sourceConnectionDto) return;
-
-		if (node instanceof TableNode) {
-			// 开发任务
-			List<Node> targets = node.getDag().getTargets();
-			if (null == targets || targets.isEmpty()) throw new RuntimeException("not found target node");
-
-			Node targetNode = targets.get(0);
-			if (targetNode instanceof TableNode) {
-				String tableName = ((TableNode) targetNode).getTableName();
-				MetadataInstancesDto metadataInstancesDto = getMetadaInstancesDto(metadataList, targetNode, tableName);
-				if (null == metadataInstancesDto) throw new RuntimeException("not found metadata instance");
-				if (customKafkaQualifiedNames.isEmpty()) throw new RuntimeException("not found kafka qualified name");
-
-				String qualifiedName = customKafkaQualifiedNames.get(0);
-				metadataList.add(GuangFaUtils.parse2SourceMetadaInstancesDo(sourceConnectionDto, qualifiedName, metadataInstancesDto, node));
-				return;
-			}
-
-			throw new RuntimeException("not support kafka target node type: " + targetNode.getType());
-		} else if (node instanceof DatabaseNode) {
-//			// 复制任务
-//			for (String name : customKafkaQualifiedNames) {
-//				String tableName = CustomKafkaUtils.getQualifiedNameTableName(sourceConnectionDto, name);
-//				MetadataInstancesDto metadaInstancesDto = getMetadaInstancesDto(metadataList, tableName);
-//				if (null == metadaInstancesDto) throw new RuntimeException("not found metadata instance");
-//				metadataList.add(CustomKafkaUtils.parse2SourceMetadaInstancesDo(sourceConnectionDto, tableName, metadaInstancesDto, node));
-//			}
-//			return;
-		}
-		throw new RuntimeException("not support kafka source node type: " + node.getType());
-	}
 	public static void updateSourceMetadataInstances(List<MetadataInstancesDto> metadataList, Node node, DataSourceConnectionDto sourceConnectionDto, List<String> customKafkaQualifiedNames, BiFunction<MetadataInstancesDto, String, MetadataInstancesDto> fun) {
 		if (null == sourceConnectionDto) return;
 		if (node instanceof TableNode) {
@@ -190,51 +157,4 @@ public class GuangFaUtils {
 		return isKafkaNode && !nodeIsTarget && enableUseTargetSchema;
 	}
 
-	public static List<String> customKafkaGetTable(DatabaseNode databaseNode) {
-
-		List<LinkedHashMap> targetTableNames = (ArrayList) databaseNode.getNodeConfig().get("table_names");
-		List<String> tableNames = new ArrayList<>();
-		for (LinkedHashMap<String, String> linkedHashMap : targetTableNames) {
-			tableNames.add(linkedHashMap.get("name"));
-		}
-		return tableNames;
-	}
-	public static String getQualifiedNameTableName(DataSourceConnectionDto dataSourceConnectionDto, String qualifiedNames) {
-		String prefix = String.format("T_%s_%s_%s_", dataSourceConnectionDto.getDefinitionPdkId(), dataSourceConnectionDto.getDefinitionGroup(), dataSourceConnectionDto.getDefinitionVersion());
-		String tableName = qualifiedNames.substring(prefix.length());
-		tableName = tableName.substring(0, tableName.lastIndexOf("_"));
-		tableName = tableName.substring(0, tableName.lastIndexOf("_"));
-		return tableName;
-	}
-	public static MetadataInstancesDto parse2SourceMetadaInstancesDo(DataSourceConnectionDto sourceConnectionDto, String qualifiedName, MetadataInstancesDto metadataInstancesDto, Node node) {
-		String tableName = GuangFaUtils.getQualifiedNameTableName(sourceConnectionDto, qualifiedName);
-		MetadataInstancesDto resultDto = ObjectUtil.clone(metadataInstancesDto);
-
-		// 设置数据源
-		SourceDto kafkaSourceNode = new SourceDto();
-		BeanUtils.copyProperties(sourceConnectionDto, kafkaSourceNode);
-		kafkaSourceNode.set_id(sourceConnectionDto.getId().toHexString());
-		resultDto.setSource(kafkaSourceNode);
-
-		resultDto.setId(null);
-		resultDto.setName(tableName);
-		resultDto.setPdkGroup(sourceConnectionDto.getDefinitionGroup());
-		resultDto.setPdkVersion(sourceConnectionDto.getDefinitionVersion());
-		resultDto.setPdkId(sourceConnectionDto.getDefinitionPdkId());
-		resultDto.setFields(metadataInstancesDto.getFields());
-		resultDto.setMetaType(metadataInstancesDto.getMetaType());
-		resultDto.setOriginalName(tableName);
-		resultDto.setAncestorsName(tableName);
-		resultDto.setNodeId(node.getId());
-		resultDto.setDatabaseId(sourceConnectionDto.getId().toHexString());
-		resultDto.setQualifiedName(qualifiedName);
-		if (node instanceof TableNode) {
-			TableNode tableNode = (TableNode) node;
-			resultDto.setConnectionId(tableNode.getConnectionId());
-		} else {
-			DatabaseNode databaseNode = (DatabaseNode) node;
-			resultDto.setConnectionId(databaseNode.getConnectionId());
-		}
-		return resultDto;
-	}
 }
