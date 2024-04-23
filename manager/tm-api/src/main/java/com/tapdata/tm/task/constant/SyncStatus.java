1dff2eb937391d834326b148faf0fb9a350cb335
GavinXiao <2749984520@qq.com>
2024-04-23 14:04:58 +0800

    fix(sornal): remove useless code

--- manager/tm-api/src/main/java/com/tapdata/tm/task/constant/SyncStatus.java
+++ manager/tm-api/src/main/java/com/tapdata/tm/task/constant/SyncStatus.java
@@ -1,16 +1,8 @@
 package com.tapdata.tm.task.constant;
 
 public class SyncStatus {
+    private SyncStatus(){}
     public static final String SYN_STATUS = "syncStatus";
 
     public static final String NORMAL = "";
-
-//    /** Full sync ongoing*/
-//    public static final String FULLING = "fulling";
-//    /** Full sync completed*/
-//    public static final String FULL_COMPLETED = "full_completed";
-//    public static final String FULL_FAILED = "full_failed";
-//    /** Incremental sync ongoing*/
-//    public static final String INCREMENTAL = "incremental";
-//    public static final String INCREMENTAL_FAILED = "incremental_failed";
 }
