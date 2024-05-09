3a7842840b9e293eb610aa641ef2dc8e33fef2ae
GavinXiao <2749984520@qq.com>
2024-05-09 11:05:24 +0800

    feat(dynamic-table-name): DynamicTableStage

--- manager/tm-common/src/main/java/com/tapdata/tm/commons/dag/dynamic/DynamicTableNameByDate.java
+++ manager/tm-common/src/main/java/com/tapdata/tm/commons/dag/dynamic/DynamicTableNameByDate.java
@@ -2,31 +2,29 @@ package com.tapdata.tm.commons.dag.dynamic;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Optional;
 
 /**
  * dynamic table name:
- *  Directly add the suffix based on the current time: ${oldTableName}_yyyy_mm_dd
+ *  Directly add the suffix based on the current time:
+ *      [${PREFIX}${couplingSymbols}]${oldTableName}[${couplingSymbols}${SUFFIX}]
+ *      default couplingSymbols: _
+ *      default couplingLocation: SUFFIX
+ *      default dynamic char: yyyy_MM_dd
  * @author Gavin'Xiao
  * https://github.com/11000100111010101100111
  * */
 class DynamicTableNameByDate extends DynamicTableStage {
-    public static final String FORMAT = "%s%s%s";
+    public static final String DATE_FORMAT = "yyyy_MM_dd";
     public DynamicTableNameByDate(String tableName, DynamicTableConfig dynamicRule) {
         super(tableName, dynamicRule);
     }
 
     @Override
     public DynamicTableResult genericTableName() {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd");
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
         String dateFormatStr = sdf.format(new Date());
-        DynamicTableResult result = DynamicTableResult.of().withOldTableName(tableName);
-        String couplingSymbols = Optional.ofNullable(dynamicRule.getCouplingSymbols()).orElse(DEFAULT_COUPLING_SYMBOLS);
-        if (CouplingLocation.PREFIX.equals(dynamicRule.getCouplingLocation())) {
-            result.withDynamicName(String.format(FORMAT, dateFormatStr, couplingSymbols, tableName));
-        } else {
-            result.withDynamicName(String.format(FORMAT, tableName, couplingSymbols, dateFormatStr));
-        }
-        return result;
+        return DynamicTableResult.of()
+                .withOldTableName(tableName)
+                .withDynamicName(genericWithCouplingLocationAndCouplingSymbols(dateFormatStr));
     }
 }
