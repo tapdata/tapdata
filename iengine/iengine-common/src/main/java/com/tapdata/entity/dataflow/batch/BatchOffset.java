521fc34b36f98e4e0bb05a05f42160a1a0b5fdbf
GavinXiao <2749984520@qq.com>
2024-05-20 14:52:00 +0800

    fix(BatchOffset): BatchOffset with Serializable

--- iengine/iengine-common/src/main/java/com/tapdata/entity/dataflow/batch/BatchOffset.java
+++ iengine/iengine-common/src/main/java/com/tapdata/entity/dataflow/batch/BatchOffset.java
@@ -8,7 +8,8 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class BatchOffset implements Cloneable {
+public class BatchOffset implements Serializable, Cloneable {
+    private static final long serialVersionUID = 5599838762323297718L;
 
     public BatchOffset() {
     }
