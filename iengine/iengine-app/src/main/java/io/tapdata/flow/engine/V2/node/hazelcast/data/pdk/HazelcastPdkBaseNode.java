042cff093ef22b4ebfa6a458814de5a0938c30e0
ljvv7 <fes504200361@qq.com>
2024-05-21 19:01:04 +0800

    fix(iengine): Replacing functionRetryStatus constants

--- iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastPdkBaseNode.java
+++ iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastPdkBaseNode.java
@@ -79,6 +79,8 @@ public abstract class HazelcastPdkBaseNode extends HazelcastDataBaseNode {
 
 	protected Integer readBatchSize;
 	protected Integer increaseReadSize;
+
+	public static final String FUNCTION_RETRY_STATUS = "functionRetryStatus";
 	private static final String DOUBLE_ACTIVE = "doubleActive";
 	protected TapRecordSkipDetector skipDetector;
 	private PdkStateMap pdkStateMap;
@@ -150,10 +152,10 @@ public abstract class HazelcastPdkBaseNode extends HazelcastDataBaseNode {
 	public void signFunctionRetry(String taskId) {
 		CommonUtils.ignoreAnyError(() -> {
 			Update update = new Update();
-			update.set("functionRetryStatus", TaskDto.RETRY_STATUS_RUNNING);
+			update.set(FUNCTION_RETRY_STATUS, TaskDto.RETRY_STATUS_RUNNING);
 			update.set("taskRetryStartTime", System.currentTimeMillis());
-			Criteria functionRetryStatusExists = Criteria.where("functionRetryStatus").exists(false);
-			Criteria functionRetryStatusNone = Criteria.where("functionRetryStatus").is(TaskDto.RETRY_STATUS_NONE);
+			Criteria functionRetryStatusExists = Criteria.where(FUNCTION_RETRY_STATUS).exists(false);
+			Criteria functionRetryStatusNone = Criteria.where(FUNCTION_RETRY_STATUS).is(TaskDto.RETRY_STATUS_NONE);
 			Query query = Query.query(Criteria.where("_id").is(new ObjectId(taskId))
 					.orOperator(functionRetryStatusExists, functionRetryStatusNone));
 			clientMongoOperator.update(query, update, ConnectorConstant.TASK_COLLECTION);
@@ -163,7 +165,7 @@ public abstract class HazelcastPdkBaseNode extends HazelcastDataBaseNode {
 	public void cleanFuctionRetry(String taskId) {
 		CommonUtils.ignoreAnyError(() -> {
 			Update update = new Update();
-			update.set("functionRetryStatus", TaskDto.RETRY_STATUS_NONE);
+			update.set(FUNCTION_RETRY_STATUS, TaskDto.RETRY_STATUS_NONE);
 			update.set("taskRetryStartTime", 0);
 			clientMongoOperator.update(Query.query(Criteria.where("_id").is(new ObjectId(taskId))), update, ConnectorConstant.TASK_COLLECTION);
 		}, "Faild to clean function retry status");
