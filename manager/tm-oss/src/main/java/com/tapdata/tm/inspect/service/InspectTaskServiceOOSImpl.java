080f9d81be957f02a5955aed4e8fd049bbe2a200
GavinXiao <2749984520@qq.com>
2024-05-27 15:29:02 +0800

    test(nspect-permissions): method does not override or implement a method from a supertype

--- manager/tm-oss/src/main/java/com/tapdata/tm/inspect/service/InspectTaskServiceOOSImpl.java
+++ manager/tm-oss/src/main/java/com/tapdata/tm/inspect/service/InspectTaskServiceOOSImpl.java
@@ -1,6 +1,5 @@
 package com.tapdata.tm.inspect.service;
 
-import com.tapdata.tm.base.dto.Field;
 import com.tapdata.tm.base.dto.Where;
 import com.tapdata.tm.base.exception.BizException;
 import com.tapdata.tm.commons.schema.DataSourceConnectionDto;
@@ -8,19 +7,12 @@ import com.tapdata.tm.commons.task.dto.TaskDto;
 import com.tapdata.tm.config.security.UserDetail;
 import com.tapdata.tm.inspect.dto.InspectDto;
 import com.tapdata.tm.webhook.enums.ConstVariable;
-import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.function.Supplier;
 
 @Service
 public class InspectTaskServiceOOSImpl implements InspectTaskService {
-    @Override
-    public Supplier<InspectDto> dataPermissionFindById(ObjectId inspectId, Field fields) {
-        throw new BizException(ConstVariable.TA_OSS_NON_SUPPORT_FUNCTION_EXCEPTION);
-    }
-
     @Override
     public InspectDto inspectTaskRun(Where where, InspectDto updateDto, UserDetail user) {
         throw new BizException(ConstVariable.TA_OSS_NON_SUPPORT_FUNCTION_EXCEPTION);
