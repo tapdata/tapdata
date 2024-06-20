ad2d7df973e14a3ab66d8f25dbb5bd5ac7e9e3c8
hardy <2545433047@qq.com>
2024-06-20 18:48:52 +0800

    TAP-2842:The cache task enables heartbeat tasks and mining tasks

---
+++ manager/tm-common/src/test/java/com/tapdata/tm/utils/ConnHeartbeatUtilsTest.java
@@ -0,0 +1,53 @@
+package com.tapdata.tm.utils;
+
+import com.tapdata.tm.commons.task.dto.ParentTaskDto;
+import com.tapdata.tm.commons.task.dto.TaskDto;
+import com.tapdata.tm.commons.util.ConnHeartbeatUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class ConnHeartbeatUtilsTest {
+    @Nested
+    class CheckTask{
+        @Test
+        void testCacheTask(){
+            Assertions.assertTrue(ConnHeartbeatUtils.checkTask(ParentTaskDto.TYPE_INITIAL_SYNC_CDC, TaskDto.SYNC_TYPE_MEM_CACHE));
+        }
+
+        @Test
+        void testCacheTask_initial_sync(){
+            Assertions.assertFalse(ConnHeartbeatUtils.checkTask(ParentTaskDto.TYPE_INITIAL_SYNC, TaskDto.SYNC_TYPE_MEM_CACHE));
+        }
+
+        @Test
+        void testMigrateTask(){
+            Assertions.assertTrue(ConnHeartbeatUtils.checkTask(ParentTaskDto.TYPE_INITIAL_SYNC_CDC, TaskDto.SYNC_TYPE_MIGRATE));
+        }
+
+        @Test
+        void testMigrateTask_initial_sync(){
+            Assertions.assertFalse(ConnHeartbeatUtils.checkTask(ParentTaskDto.TYPE_INITIAL_SYNC, TaskDto.SYNC_TYPE_MIGRATE));
+        }
+
+        @Test
+        void testSyncTask(){
+            Assertions.assertTrue(ConnHeartbeatUtils.checkTask(ParentTaskDto.TYPE_INITIAL_SYNC_CDC, TaskDto.SYNC_TYPE_SYNC));
+        }
+
+        @Test
+        void testSyncTask_initial_sync(){
+            Assertions.assertFalse(ConnHeartbeatUtils.checkTask(ParentTaskDto.TYPE_INITIAL_SYNC, TaskDto.SYNC_TYPE_SYNC));
+        }
+
+        @Test
+        void testLogCollectorTask(){
+            Assertions.assertTrue(ConnHeartbeatUtils.checkTask(ParentTaskDto.TYPE_INITIAL_SYNC_CDC, TaskDto.SYNC_TYPE_LOG_COLLECTOR));
+        }
+
+        @Test
+        void testLogCollectorTask_initial_sync(){
+            Assertions.assertFalse(ConnHeartbeatUtils.checkTask(ParentTaskDto.TYPE_INITIAL_SYNC, TaskDto.SYNC_TYPE_LOG_COLLECTOR));
+        }
+    }
+}
