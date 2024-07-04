6f83b32582bef488a83e66d7377f382b81af5ccf
ljvv7 <fes504200361@qq.com>
2024-07-04 15:09:59 +0800

    fix(TM):TAP-116 add unit case

--- manager/tm-api/src/test/java/com/tapdata/tm/base/aop/MeasureAOPTest.java
+++ manager/tm-api/src/test/java/com/tapdata/tm/base/aop/MeasureAOPTest.java
@@ -9,9 +9,14 @@ import com.tapdata.tm.alarm.service.AlarmService;
 import com.tapdata.tm.commons.task.constant.AlarmKeyEnum;
 import com.tapdata.tm.commons.task.dto.TaskDto;
 import com.tapdata.tm.commons.task.dto.alarm.AlarmRuleDto;
+import com.tapdata.tm.config.security.UserDetail;
 import com.tapdata.tm.message.constant.Level;
 import com.tapdata.tm.task.service.TaskService;
 import com.tapdata.tm.user.service.UserService;
+import com.tapdata.tm.utils.MongoUtils;
+import io.tapdata.common.sample.request.Sample;
+import io.tapdata.common.sample.request.SampleRequest;
+import org.aspectj.lang.JoinPoint;
 import org.bson.types.ObjectId;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
@@ -149,6 +154,78 @@ public class MeasureAOPTest {
                 return null;
             }).when(alarmService).save(any());
             measureAOP.taskIncrementDelayAlarm(taskDto, taskId, 3, alarmRuleDto);
+        }
+    }
+    @Nested
+    class AddAgentMeasurementTest{
+        private TaskService taskService;
+        private AlarmService alarmService;
+        private UserService userService;
+        private MeasureAOP measureAOP;
+        private TaskDto taskDto;
+        UserDetail userDetail;
+
+        AlarmRuleDto alarmRuleDto;
+
+        public String taskId = "6682d405494d8d2a5c957f38";
+        public String userId = "testUserId";
+
+
+        @BeforeEach
+        void setUp() {
+            taskService = mock(TaskService.class);
+            alarmService = mock(AlarmService.class);
+            userService = mock(UserService.class);
+            measureAOP = mock(MeasureAOP.class);
+            ReflectionTestUtils.setField(measureAOP, "taskService", taskService);
+            ReflectionTestUtils.setField(measureAOP, "alarmService", alarmService);
+            ReflectionTestUtils.setField(measureAOP, "userService", userService);
+
+            taskDto = new TaskDto();
+            taskDto.setId(MongoUtils.toObjectId(taskId));
+            when(taskService.findByTaskId(any())).thenReturn(taskDto);
+
+            alarmRuleDto = getAlarmRuleDto();
+            List<AlarmRuleDto> alarmRuleDtoList=new ArrayList<>();
+            alarmRuleDtoList.add(alarmRuleDto);
+
+            Map<String, List<AlarmRuleDto>> ruleMap=new HashMap<>();
+            ruleMap.put(taskId,alarmRuleDtoList);
+            when(alarmService.getAlarmRuleDtos(any())).thenReturn(ruleMap);
+
+            userDetail = mock(UserDetail.class);
+            when(userService.loadUserById(any())).thenReturn(userDetail);
+
+
+        }
+        @DisplayName("test addAgentMeasurement when sample type is task and have alarmRule is TASK_INCREMENT_DELAY")
+        @Test
+        void test1(){
+            when(alarmService.checkOpen(taskDto,null, AlarmKeyEnum.TASK_INCREMENT_DELAY, null, userDetail)).thenReturn(true);
+            List<SampleRequest> list = new ArrayList<>();
+            SampleRequest sampleRequest=new SampleRequest();
+
+            Map<String, String> tags =new HashMap<>();
+            tags.put("type","task");
+            tags.put("taskId",taskId);
+            sampleRequest.setTags(tags);
+
+            Map<String,Number> vs=new HashMap<>();
+            vs.put("replicateLag",20000);
+            Sample sample=new Sample();
+            sample.setVs(vs);
+            sampleRequest.setSample(sample);
+
+            list.add(sampleRequest);
+
+            JoinPoint joinPoint = mock(JoinPoint.class);
+
+            Object[] objects = new Object[10];
+            objects[0]=list;
+            when(joinPoint.getArgs()).thenReturn(objects);
+            doCallRealMethod().when(measureAOP).addAgentMeasurement(joinPoint);
+            measureAOP.addAgentMeasurement(joinPoint);
+            verify(measureAOP,times(1)).taskIncrementDelayAlarm(taskDto,taskId, 20000, alarmRuleDto);
         }
     }
 
