1c3f31fdf7498f2ac776aa8300430ac7b5bdc721
Gavin'Xiao <2749984520@qq.com>
2024-07-08 11:00:24 +0800

    test(TAP-3657): duplicate code

--- manager/tm-api/src/test/java/com/tapdata/tm/utils/MailUtilsTest.java
+++ manager/tm-api/src/test/java/com/tapdata/tm/utils/MailUtilsTest.java
@@ -7,9 +7,6 @@ import com.tapdata.tm.Settings.service.SettingsService;
 import com.tapdata.tm.message.constant.MsgTypeEnum;
 import com.tapdata.tm.message.constant.SystemEnum;
 import com.tapdata.tm.message.service.BlacklistService;
-import com.tapdata.tm.utils.MailUtils;
-import com.tapdata.tm.utils.SendStatus;
-import io.tapdata.entity.error.CoreException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -21,19 +18,36 @@ import org.mockito.MockedConstruction;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import javax.mail.*;
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.anyList;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class MailUtilsTest {
     @Mock
@@ -199,13 +213,15 @@ class MailUtilsTest {
         Transport transport;
 
         MailUtils mu;
+        Logger log;
         @BeforeEach
-        void init() throws MessagingException {
+        void init() throws MessagingException, UnsupportedEncodingException {
             host = "127.0.0.1";
             port = 9999;
             user = "tapdata";
             sendAddress = "127.0.0.1";
             password = "123456";
+            log = mock(Logger.class);
 
             mu = mock(MailUtils.class);
             ReflectionTestUtils.setField(mu, "host", host);
@@ -215,12 +231,17 @@ class MailUtilsTest {
             ReflectionTestUtils.setField(mu, "password", password);
 
             transport = mock(Transport.class);
+            mockSlf4jLog(mu, log);
 
+            when(mu.emailSession()).thenCallRealMethod();
+            when(mu.message(any(Session.class), anyList(), anyString(), anyString())).thenCallRealMethod();
             when(mu.sendValidateCodeForResetPWD(anyString(), anyString(), anyString())).thenCallRealMethod();
         }
 
         @Test
         void testSendValidateCodeForResetPWD() throws MessagingException {
+            when(mu.connectSMTP(any(Session.class))).thenCallRealMethod();
+            doCallRealMethod().when(mu).closeTransport(any(Transport.class));
             doNothing().when(transport).connect(anyString(), anyInt(), anyString(), anyString());
             doNothing().when(mu).initMailConfig();
             doNothing().when(transport).close();
@@ -249,6 +270,9 @@ class MailUtilsTest {
 
         @Test
         void testSendValidateCodeForResetPWDException() throws MessagingException {
+            doNothing().when(log).error(anyString(), any(Exception.class));
+            when(mu.connectSMTP(any(Session.class))).thenCallRealMethod();
+            doCallRealMethod().when(mu).closeTransport(any(Transport.class));
             doNothing().when(transport).connect(anyString(), anyInt(), anyString(), anyString());
             doNothing().when(mu).initMailConfig();
             doNothing().when(transport).close();
@@ -277,8 +301,11 @@ class MailUtilsTest {
                 Assertions.assertNotNull(sendStatus);
             }
         }
+
         @Test
         void testSendValidateCodeForResetPWDCloseException() throws MessagingException {
+            when(mu.connectSMTP(any(Session.class))).thenCallRealMethod();
+            doCallRealMethod().when(mu).closeTransport(any(Transport.class));
             doNothing().when(transport).connect(anyString(), anyInt(), anyString(), anyString());
             doNothing().when(mu).initMailConfig();
             doAnswer(a -> {
@@ -305,10 +332,12 @@ class MailUtilsTest {
                 Assertions.assertNotNull(sendStatus);
             }
         }
+
         @Test
         void testSendValidateCodeForResetPWDTransportIsNull() throws MessagingException {
+            doNothing().when(log).error(anyString(), any(Exception.class));
+            doCallRealMethod().when(mu).closeTransport(null);
             doNothing().when(mu).initMailConfig();
-            //doNothing().when(transport).sendMessage(any(MimeMessage.class), any(Address[].class));
             Session session = mock(Session.class);
             InternetAddress[] internetAddressList = new InternetAddress[0];
             Address[] addresses = new Address[0];
@@ -321,16 +350,58 @@ class MailUtilsTest {
                     doNothing().when(mk).setContent(anyString(), anyString());
                     doNothing().when(mk).setSentDate(any(Date.class));
                     doAnswer(a -> {
-                        throw new Exception("");
+                        throw new MessagingException("");
                     }).when(mk).saveChanges();
                     when(mk.getAllRecipients()).thenReturn(addresses);
                 })) {
                 s.when(() -> Session.getDefaultInstance(any(Properties.class))).thenReturn(session);
                 SendStatus sendStatus = mu.sendValidateCodeForResetPWD("", "", "");
                 Assertions.assertNotNull(sendStatus);
-            } catch (Exception e) {
-
             }
+        }
+
+        @Test
+        void testNotPort() throws MessagingException {
+            when(mu.connectSMTP(any(Session.class))).thenCallRealMethod();
+            doCallRealMethod().when(mu).closeTransport(any(Transport.class));
+            ReflectionTestUtils.setField(mu, "port", null);
+            doNothing().when(transport).connect(anyString(), anyString(), anyString());
+            doNothing().when(mu).initMailConfig();
+            doNothing().when(transport).close();
+            doNothing().when(transport).sendMessage(any(MimeMessage.class), any(Address[].class));
+            Session session = mock(Session.class);
+            InternetAddress[] internetAddressList = new InternetAddress[0];
+            Address[] addresses = new Address[0];
+
+            doNothing().when(session).setDebug(true);
+            when(session.getTransport("smtp")).thenReturn(transport);
+            try(MockedStatic<Session> s = mockStatic(Session.class);
+                MockedConstruction<InternetAddress> i = mockConstruction(InternetAddress.class, (ic, c) -> {});
+                MockedConstruction<MimeMessage> m = mockConstruction(MimeMessage.class, (mk,c) -> {
+                    doNothing().when(mk).setFrom(any(InternetAddress.class));
+                    doNothing().when(mk).setRecipients(Message.RecipientType.TO, internetAddressList);
+                    doNothing().when(mk).setContent(anyString(), anyString());
+                    doNothing().when(mk).setSentDate(any(Date.class));
+                    doNothing().when(mk).saveChanges();
+                    when(mk.getAllRecipients()).thenReturn(addresses);
+                })) {
+                s.when(() -> Session.getDefaultInstance(any(Properties.class))).thenReturn(session);
+                SendStatus sendStatus = mu.sendValidateCodeForResetPWD("", "", "");
+                Assertions.assertNotNull(sendStatus);
+            }
+        }
+    }
+
+    public void mockSlf4jLog(Object mockTo, Logger log) {
+        try {
+            Field logF = mockTo.getClass().getDeclaredField("log");
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(logF, logF.getModifiers() & ~Modifier.FINAL);
+            logF.setAccessible(true);
+            logF.set(mockTo, log);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
