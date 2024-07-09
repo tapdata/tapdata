b01db43f10209c02b3ac7a7a9cc39392d8be076d
ljvv7 <fes504200361@qq.com>
2024-07-10 01:23:54 +0800

    fix(iengine):TAP-2953  fix listStream do not return

--- iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastSourcePartitionReadDataNode.java
+++ iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastSourcePartitionReadDataNode.java
@@ -578,13 +578,13 @@ public class HazelcastSourcePartitionReadDataNode extends HazelcastSourcePdkData
 //				}
 //			}
 			if (events != null && !events.isEmpty()) {
-				events.stream().map(event -> {
+				events = events.stream().map(event -> {
 					if (null == event.getTime()) {
 						throw new NodeException("Invalid TapEvent, `TapEvent.time` should be NonNUll").context(getProcessorBaseContext()).event(event);
 					}
 					event.addInfo("eventId", UUID.randomUUID().toString());
-					return cdcDelayCalculation.filterAndCalcDelay(event, times -> AspectUtils.executeAspect(SourceCDCDelayAspect.class, () -> new SourceCDCDelayAspect().delay(times).dataProcessorContext(dataProcessorContext)),this.dataProcessorContext.getTaskDto().getSyncType());
-				});
+					return cdcDelayCalculation.filterAndCalcDelay(event, times -> AspectUtils.executeAspect(SourceCDCDelayAspect.class, () -> new SourceCDCDelayAspect().delay(times).dataProcessorContext(dataProcessorContext)), this.dataProcessorContext.getTaskDto().getSyncType());
+				}).collect(Collectors.toList());
 
 				if (streamReadFuncAspect != null) {
 					AspectUtils.accept(streamReadFuncAspect.state(StreamReadFuncAspect.STATE_STREAMING_READ_COMPLETED).getStreamingReadCompleteConsumers(), events);
---
+++ iengine/iengine-app/src/test/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastSourcePartitionReadDataNodeTest.java
@@ -0,0 +1,53 @@
+package io.tapdata.flow.engine.V2.node.hazelcast.data.pdk;
+
+import base.hazelcast.BaseHazelcastNodeTest;
+import com.tapdata.entity.SyncStage;
+import com.tapdata.entity.TapdataEvent;
+import com.tapdata.entity.TapdataHeartbeatEvent;
+import com.tapdata.tm.commons.cdcdelay.CdcDelay;
+import io.tapdata.entity.event.TapEvent;
+import io.tapdata.entity.event.dml.TapUpdateRecordEvent;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.mockito.Mockito.*;
+
+public class HazelcastSourcePartitionReadDataNodeTest extends BaseHazelcastNodeTest {
+    private HazelcastSourcePartitionReadDataNode hazelcastSourcePartitionReadDataNode;
+    private CdcDelay cdcDelay;
+
+
+    @BeforeEach
+    void setUp() {
+        super.allSetup();
+        Logger logger = mock(Logger.class);
+        cdcDelay=mock(CdcDelay.class);
+        hazelcastSourcePartitionReadDataNode=mock(HazelcastSourcePartitionReadDataNode.class);
+        ReflectionTestUtils.setField(hazelcastSourcePartitionReadDataNode, "logger", logger);
+        ReflectionTestUtils.setField(hazelcastSourcePartitionReadDataNode, "cdcDelayCalculation", cdcDelay);
+        ReflectionTestUtils.setField(hazelcastSourcePartitionReadDataNode,"dataProcessorContext", dataProcessorContext);
+    }
+    @DisplayName("test filterAndCalcDelay table is heartBeat")
+    @Test
+    void test1() {
+        List<TapEvent> tapEvents = new ArrayList<>();
+        TapUpdateRecordEvent tapUpdateRecordEvent = new TapUpdateRecordEvent();
+        tapUpdateRecordEvent.setTableId("testTableId");
+        tapUpdateRecordEvent.setTime(System.currentTimeMillis());
+        tapEvents.add(tapUpdateRecordEvent);
+        doCallRealMethod().when(hazelcastSourcePartitionReadDataNode).handleStreamEventsReceived(tapEvents, null);
+        List<TapdataEvent> tapdataEvents=new ArrayList<>();
+        TapdataHeartbeatEvent heartbeatEvent=new TapdataHeartbeatEvent();
+        tapdataEvents.add(heartbeatEvent);
+        when(hazelcastSourcePartitionReadDataNode.wrapTapdataEvent(tapEvents, SyncStage.CDC, null)).thenReturn(tapdataEvents);
+        hazelcastSourcePartitionReadDataNode.handleStreamEventsReceived(tapEvents, null);
+        verify(cdcDelay,times(1)).filterAndCalcDelay(any(),any(),any());
+    }
+
+
+}
