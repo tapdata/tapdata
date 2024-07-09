d20367b8ee739146c49252c83b49c38df09957fa
ljvv7 <fes504200361@qq.com>
2024-07-09 15:13:51 +0800

    fix(iengine):TAP-2953 add unit case

--- manager/tm-common/src/test/java/com/tapdata/tm/commons/cdcdelay/CdcDelayTest.java
+++ manager/tm-common/src/test/java/com/tapdata/tm/commons/cdcdelay/CdcDelayTest.java
@@ -2,13 +2,18 @@ package com.tapdata.tm.commons.cdcdelay;
 
 import com.tapdata.tm.commons.task.dto.TaskDto;
 import com.tapdata.tm.commons.util.ConnHeartbeatUtils;
+import io.tapdata.entity.event.TapEvent;
+import io.tapdata.entity.event.control.HeartbeatEvent;
 import io.tapdata.entity.event.dml.TapUpdateRecordEvent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.*;
 import java.util.function.Consumer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CdcDelayTest {
     private CdcDelay cdcDelay;
@@ -30,7 +35,8 @@ public class CdcDelayTest {
         List<String> tables=new ArrayList<>();
         tables.add("testTableId");
         cdcDelay.addHeartbeatTable(tables);
-        cdcDelay.filterAndCalcDelay(tapUpdateRecordEvent,consumer, TaskDto.SYNC_TYPE_SYNC);
+        TapEvent tapEvent = cdcDelay.filterAndCalcDelay(tapUpdateRecordEvent, consumer, TaskDto.SYNC_TYPE_SYNC);
+        assertEquals(true,tapEvent instanceof HeartbeatEvent);
     }
     @DisplayName("test filterAndCalcDelay LOG_COLLECTOR Task")
     @Test
@@ -46,6 +52,23 @@ public class CdcDelayTest {
         List<String> tables=new ArrayList<>();
         tables.add("testTableId");
         cdcDelay.addHeartbeatTable(tables);
-        cdcDelay.filterAndCalcDelay(tapUpdateRecordEvent,consumer, TaskDto.SYNC_TYPE_LOG_COLLECTOR);
+        TapEvent tapEvent = cdcDelay.filterAndCalcDelay(tapUpdateRecordEvent, consumer, TaskDto.SYNC_TYPE_LOG_COLLECTOR);
+        assertEquals(true,tapEvent instanceof TapUpdateRecordEvent);
+    }
+    @DisplayName("test filterAndCalcDelay not heartbeat event")
+    @Test
+    void test3(){
+        ReflectionTestUtils.setField(cdcDelay,"lastUpdated",System.currentTimeMillis()-5000L*1000);
+        TapUpdateRecordEvent tapUpdateRecordEvent=new TapUpdateRecordEvent();
+        Map<String,Object> after=new HashMap<>();
+        after.put("ts",new Date());
+        after.put("id","recordId");
+        tapUpdateRecordEvent.setReferenceTime(System.currentTimeMillis());
+        tapUpdateRecordEvent.setAfter(after);
+        tapUpdateRecordEvent.setTableId("testTable");
+        Consumer<Long> consumer=(time)->{
+        };
+        TapEvent tapEvent = cdcDelay.filterAndCalcDelay(tapUpdateRecordEvent, consumer, TaskDto.SYNC_TYPE_LOG_COLLECTOR);
+        assertEquals(tapUpdateRecordEvent,tapEvent);
     }
 }
