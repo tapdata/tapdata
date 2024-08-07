099fd9120fb6f2292060d24f3a5adbcad09fa1d3
Leon <lirufei0808@gmail.com>
2024-08-07 12:35:35 +0800

    Optimize the logic of asynchronous file deletion

---
+++ manager/tm/src/main/java/com/tapdata/tm/schedule/DeleteFileSchedule.java
@@ -0,0 +1,32 @@
+package com.tapdata.tm.schedule;
+
+import com.tapdata.tm.file.service.FileService;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lg&lt;lirufei0808@gmail.com&gt;
+ * create at 2024/8/2 16:48
+ */
+@Slf4j
+@Component
+public class DeleteFileSchedule {
+    @Autowired
+    @Setter
+    private FileService fileService;
+
+    /**
+     * clean up files that need to be deleted
+     */
+    //@Scheduled(cron = "0 0 2 * * ?")
+    @Scheduled(cron = "0/5 * * * * ?")
+    @SchedulerLock(name = "DatabaseTypeSchedule.cleanUpForDatabaseTypes", lockAtMostFor = "PT1H", lockAtLeastFor = "PT1H")
+    public void cleanUpForDatabaseTypes() {
+        log.info("clean up files that need to be deleted");
+        fileService.cleanupWaitingDeleteFiles();
+    }
+}
