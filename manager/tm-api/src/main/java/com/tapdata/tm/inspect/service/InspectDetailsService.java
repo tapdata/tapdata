154f0d6deabdda9e7a282aa282b9b90ec504bbb4
hardy <2545433047@qq.com>
2024-08-15 15:54:33 +0800

    fix:TAP-4465 Data verification supports export of difference fields.

--- manager/tm-api/src/main/java/com/tapdata/tm/inspect/service/InspectDetailsService.java
+++ manager/tm-api/src/main/java/com/tapdata/tm/inspect/service/InspectDetailsService.java
@@ -2,6 +2,7 @@ package com.tapdata.tm.inspect.service;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.tapdata.tm.base.service.BaseService;
 import com.tapdata.tm.config.security.UserDetail;
 import com.tapdata.tm.inspect.bean.Source;
@@ -20,9 +21,7 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.zip.ZipOutputStream;
 
 public abstract class InspectDetailsService extends BaseService<InspectDetailsDto, InspectDetailsEntity, ObjectId, InspectDetailsRepository> {
@@ -67,6 +66,7 @@ public abstract class InspectDetailsService extends BaseService<InspectDetailsDt
             if (!"failed".equals(statsTmp.getResult())) {
                 return;
             }
+            if(!inspectDetails.getFullField())compareDifferenceFields(inspectDetailsDtoTmp);
             JSONObject jsonObjectTmp = new JSONObject();
             jsonObjectTmp.put("source", inspectDetailsDtoTmp.getSource());
             jsonObjectTmp.put("target", inspectDetailsDtoTmp.getTarget());
@@ -99,7 +99,38 @@ public abstract class InspectDetailsService extends BaseService<InspectDetailsDt
                 jsonArrayData.add(jsonObjectTmp);
             }
         });
-        stream.write(jsonArray.toString().getBytes());
+        stream.write(JSONObject.toJSONString(jsonArray, SerializerFeature.WriteMapNullValue).getBytes());
+    }
+
+    protected void compareDifferenceFields(InspectDetailsDto inspectDetailsDtoTmp){
+        Map<String, Object> source = inspectDetailsDtoTmp.getSource();
+        Map<String, Object> target = inspectDetailsDtoTmp.getTarget();
+        Set<String> allKeys = new HashSet<>(source.keySet());
+        allKeys.addAll(target.keySet());
+        Map<String, Object> difSource = new HashMap<>();
+        Map<String, Object> difTarget = new HashMap<>();
+        for (String key : allKeys) {
+            Object sourceValue = source.get(key);
+            Object targetValue = target.get(key);
+
+            if (!equals(sourceValue, targetValue)) {
+                difSource.put(key, sourceValue);
+                difTarget.put(key, targetValue);
+            }
+        }
+        inspectDetailsDtoTmp.setSource(difSource);
+        inspectDetailsDtoTmp.setTarget(difTarget);
+    }
+
+
+    protected boolean equals(Object o1, Object o2) {
+        if (o1 == null && o2 == null) {
+            return true;
+        }
+        if (o1 == null || o2 == null) {
+            return false;
+        }
+        return o1.equals(o2);
     }
 
 
