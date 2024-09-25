9bce674508b8d2a739155f5c3f0bafa6b0316672
mnianqi <mnq0626@163.com>
2024-09-25 19:17:26 +0800

    fix: TAP-4584 Real Time Data Hub FDM Table status error
    (cherry picked from commit d8a1ed2ae6ca786239cbf4003ec3df91acb247df)

--- manager/tm/src/main/java/com/tapdata/tm/metadatainstance/service/MetadataInstancesServiceImpl.java
+++ manager/tm/src/main/java/com/tapdata/tm/metadatainstance/service/MetadataInstancesServiceImpl.java
@@ -2238,7 +2238,8 @@ public class MetadataInstancesServiceImpl extends MetadataInstancesService{
                 .and(IS_DELETED).ne(true)
                 .and(SOURCE_ID).is(connectionId)
                 .and(SOURCE_TYPE).is(SourceTypeEnum.SOURCE.name())
-                .and(TASK_ID).exists(false);
+                .and(TASK_ID).exists(false)
+                .and("createSource").ne("job_analyze");
         Query query = new Query(criteria);
         long count = count(query, user);
         return count > 0;
