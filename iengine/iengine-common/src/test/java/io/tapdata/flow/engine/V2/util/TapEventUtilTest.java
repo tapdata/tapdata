180f9bdc0e5280461e5057c5bff947b9043263f1
mnianqi <mnq0626@163.com>
2024-09-29 13:43:48 +0800

    fix: TAP-4676 remove SendMailResponseDto and change ad to ldap in settingService

--- iengine/iengine-common/src/test/java/io/tapdata/flow/engine/V2/util/TapEventUtilTest.java
+++ iengine/iengine-common/src/test/java/io/tapdata/flow/engine/V2/util/TapEventUtilTest.java
@@ -17,9 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
