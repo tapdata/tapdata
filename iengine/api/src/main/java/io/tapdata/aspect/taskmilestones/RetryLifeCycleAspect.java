f51963b7280367291ef02df66ce6e90685ae8d32
Leon <lirufei0808@gmail.com>
2024-11-05 20:38:42 +0800

    update RetryLifeCycleAspect

--- iengine/api/src/main/java/io/tapdata/aspect/taskmilestones/RetryLifeCycleAspect.java
+++ iengine/api/src/main/java/io/tapdata/aspect/taskmilestones/RetryLifeCycleAspect.java
@@ -12,8 +12,6 @@ import java.util.Map;
  * @author lg&lt;lirufei0808@gmail.com&gt;
  * create at 2024/11/4 11:36
  */
-@EqualsAndHashCode(callSuper = true)
-@Data
 public class RetryLifeCycleAspect extends DataFunctionAspect<RetryLifeCycleAspect> {
 
     private boolean retrying;
@@ -25,4 +23,76 @@ public class RetryLifeCycleAspect extends DataFunctionAspect<RetryLifeCycleAspec
     private String retryOp;
     private Boolean success;
     private Map<String, Object> retryMetadata;
+
+    public boolean isRetrying() {
+        return retrying;
+    }
+
+    public void setRetrying(boolean retrying) {
+        this.retrying = retrying;
+    }
+
+    public Long getRetryTimes() {
+        return retryTimes;
+    }
+
+    public void setRetryTimes(Long retryTimes) {
+        this.retryTimes = retryTimes;
+    }
+
+    public Long getStartRetryTs() {
+        return startRetryTs;
+    }
+
+    public void setStartRetryTs(Long startRetryTs) {
+        this.startRetryTs = startRetryTs;
+    }
+
+    public Long getEndRetryTs() {
+        return endRetryTs;
+    }
+
+    public void setEndRetryTs(Long endRetryTs) {
+        this.endRetryTs = endRetryTs;
+    }
+
+    public Long getNextRetryTs() {
+        return nextRetryTs;
+    }
+
+    public void setNextRetryTs(Long nextRetryTs) {
+        this.nextRetryTs = nextRetryTs;
+    }
+
+    public Long getTotalRetries() {
+        return totalRetries;
+    }
+
+    public void setTotalRetries(Long totalRetries) {
+        this.totalRetries = totalRetries;
+    }
+
+    public String getRetryOp() {
+        return retryOp;
+    }
+
+    public void setRetryOp(String retryOp) {
+        this.retryOp = retryOp;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public Map<String, Object> getRetryMetadata() {
+        return retryMetadata;
+    }
+
+    public void setRetryMetadata(Map<String, Object> retryMetadata) {
+        this.retryMetadata = retryMetadata;
+    }
 }
