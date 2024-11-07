5e9cc98227f2124e17e2478112c9ef0843326ea0
ljvv7 <fes504200361@qq.com>
2024-11-07 17:38:06 +0800

    fix(iengine):add unit case

--- iengine/iengine-common/src/test/java/com/tapdata/cache/AbstractCacheServiceTest.java
+++ iengine/iengine-common/src/test/java/com/tapdata/cache/AbstractCacheServiceTest.java
@@ -1,10 +1,25 @@
 package com.tapdata.cache;
 
+import com.tapdata.entity.dataflow.DataFlowCacheConfig;
 import com.tapdata.mongo.ClientMongoOperator;
+import com.tapdata.mongo.HttpClientMongoOperator;
+import io.tapdata.error.ShareCacheExCode_20;
+import io.tapdata.exception.TapCodeException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.locks.Lock;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
 
 public class AbstractCacheServiceTest {
     class TestCacheService extends AbstractCacheService{
@@ -34,8 +49,21 @@ public class AbstractCacheServiceTest {
             return null;
         }
     }
+    @DisplayName("test encode cache name for exception")
     @Test
-    void test1(){
-
+    void test1() {
+        Map<String, String> cacheStatusMap = new HashMap<>();
+        HttpClientMongoOperator httpClientMongoOperator = mock(HttpClientMongoOperator.class);
+        TestCacheService testCacheService = new TestCacheService(httpClientMongoOperator, cacheStatusMap);
+        Function<String, DataFlowCacheConfig> supplier = testCacheService.cacheConfigMap.getSupplier();
+        try (MockedStatic<URLEncoder> urlEncoderMockedStatic = mockStatic(URLEncoder.class);) {
+            urlEncoderMockedStatic.when(() -> {
+                URLEncoder.encode("t1", "UTF-8");
+            }).thenThrow(new UnsupportedEncodingException());
+            TapCodeException tapCodeException = assertThrows(TapCodeException.class, () -> {
+                supplier.apply("t1");
+            });
+            assertEquals(ShareCacheExCode_20.ENCODE_CACHE_NAME,tapCodeException.getCode());
+        }
     }
 }
