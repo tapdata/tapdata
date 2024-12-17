bc37ca78914f2c1dce97bbc9dca62d4f0030bf1e
mnianqi <mnq0626@163.com>
2024-12-17 11:20:39 +0800

    feat: TAP-5290 add unit test

--- manager/tm/src/test/java/com/tapdata/tm/userLog/service/UserLogServiceImplTest.java
+++ manager/tm/src/test/java/com/tapdata/tm/userLog/service/UserLogServiceImplTest.java
@@ -5,6 +5,7 @@ import com.tapdata.tm.userLog.constant.Modular;
 import com.tapdata.tm.userLog.constant.Operation;
 import com.tapdata.tm.userLog.constant.UserLogType;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
@@ -15,12 +16,29 @@ public class UserLogServiceImplTest {
     void beforeEach() {
         userLogService = mock(UserLogServiceImpl.class);
     }
-    @Test
-    void testAddUserLogWithSystemStart() {
-        UserDetail userDetail = mock(UserDetail.class);
-        doCallRealMethod().when(userLogService).addUserLog(Modular.USER, Operation.CREATE, userDetail, null, "", true);
-        userLogService.addUserLog(Modular.USER, Operation.CREATE, userDetail, null, "", true);
-        verify(userLogService, times(1)).addUserLog(Modular.USER, Operation.CREATE, userDetail, null, UserLogType.USER_OPERATION, "", null, false, true);
+    @Nested
+    class testAddUserLogWithSystemStart {
+        @Test
+        void testBoolean() {
+            UserDetail userDetail = mock(UserDetail.class);
+            doCallRealMethod().when(userLogService).addUserLog(Modular.USER, Operation.CREATE, userDetail, null, "", true);
+            userLogService.addUserLog(Modular.USER, Operation.CREATE, userDetail, null, "", true);
+            verify(userLogService, times(1)).addUserLog(Modular.USER, Operation.CREATE, userDetail, null, UserLogType.USER_OPERATION, "", null, false, true);
+        }
+        @Test
+        void testNotNull() {
+            UserDetail userDetail = mock(UserDetail.class);
+            doCallRealMethod().when(userLogService).addUserLog(Modular.USER, Operation.CREATE, userDetail, null, "", "");
+            userLogService.addUserLog(Modular.USER, Operation.CREATE, userDetail, null, "", "");
+            verify(userLogService, times(1)).addUserLog(Modular.USER, Operation.CREATE, userDetail, null, UserLogType.USER_OPERATION, "", null, false, true);
+        }
+        @Test
+        void testIsNull() {
+            UserDetail userDetail = mock(UserDetail.class);
+            doCallRealMethod().when(userLogService).addUserLog(Modular.USER, Operation.CREATE, userDetail, null, "", null);
+            userLogService.addUserLog(Modular.USER, Operation.CREATE, userDetail, null, "", null);
+            verify(userLogService, times(1)).addUserLog(Modular.USER, Operation.CREATE, userDetail, null, UserLogType.USER_OPERATION, "", null, false, false);
+        }
     }
 
 }
