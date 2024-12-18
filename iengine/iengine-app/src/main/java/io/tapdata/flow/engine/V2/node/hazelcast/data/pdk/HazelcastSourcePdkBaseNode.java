e36ca2659ab3f290cbf915895915bfeed775e762
mnianqi <mnq0626@163.com>
2024-12-18 10:56:30 +0800

    feat: TAP-5170 When selecting a table with only a primary key and matching it with a regular expression to synchronize, the task of adding a table without a primary key will keep logging the discovery of the new table

--- iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastSourcePdkBaseNode.java
+++ iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastSourcePdkBaseNode.java
@@ -219,6 +219,7 @@ public abstract class HazelcastSourcePdkBaseNode extends HazelcastPdkBaseNode {
 	private int toTapValueBatchSize;
 	protected Boolean syncSourcePartitionTableEnable;
     protected final NoPrimaryKeyVirtualField noPrimaryKeyVirtualField = new NoPrimaryKeyVirtualField();
+	private Map<List<String>, Integer> loggedTableMap = new ConcurrentHashMap<>();
 
 	public HazelcastSourcePdkBaseNode(DataProcessorContext dataProcessorContext) {
 		super(dataProcessorContext);
@@ -933,7 +934,9 @@ public abstract class HazelcastSourcePdkBaseNode extends HazelcastPdkBaseNode {
 		if (CollectionUtils.isNotEmpty(addList)) {
 			List<String> loadedTableNames;
 			final List<String> noPrimaryKeyTableNames = new ArrayList<>();
-			obsLogger.info("Found new table(s): " + addList);
+			if (!loggedTableMap.containsKey(addList)) {
+				obsLogger.info("Found new table(s): " + addList);
+			}
 			List<TapTable> addTapTables = new ArrayList<>();
 			List<TapdataEvent> tapdataEvents = new ArrayList<>();
 			// Load new table schema
@@ -991,6 +994,7 @@ public abstract class HazelcastSourcePdkBaseNode extends HazelcastPdkBaseNode {
 				if (filterTableByNoPrimaryKey.apply(tapTable)) {
 					logger.warn("Ignore DDL no primary key table '{}'", tapTable.getId());
 					noPrimaryKeyTableNames.add(tapTable.getId());
+					return;
 				}
 				addTapTables.add(tapTable);
 			});
@@ -999,7 +1003,10 @@ public abstract class HazelcastSourcePdkBaseNode extends HazelcastPdkBaseNode {
 					addTapTables.forEach(tapTable -> obsLogger.debug("Loaded new table schema: {}", tapTable));
 				}
 			}
-			obsLogger.info("Load new table(s) schema finished, loaded schema count: {}", addTapTables.size());
+			if (loggedTableMap.get(addList) == null || addTapTables.size() != loggedTableMap.get(addList)) {
+				obsLogger.info("Load new table(s) schema finished, loaded schema count: {}", addTapTables.size());
+				loggedTableMap.put(addList, addTapTables.size());
+			}
 			loadedTableNames = addTapTables.stream().map(TapTable::getId).collect(Collectors.toList());
 			List<String> missingTableNames = new ArrayList<>();
 			addList.forEach(tableName -> {
