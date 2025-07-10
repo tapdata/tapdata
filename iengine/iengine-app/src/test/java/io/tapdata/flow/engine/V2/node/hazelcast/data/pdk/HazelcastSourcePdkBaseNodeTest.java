70f327b30e9bf23d32b8540936f23558d81fecc7
ljvv7 <fes504200361@qq.com>
2025-07-10 12:27:13 +0800

    fix(iengie):fix unit case

--- iengine/iengine-app/src/test/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastSourcePdkBaseNodeTest.java
+++ iengine/iengine-app/src/test/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastSourcePdkBaseNodeTest.java
@@ -833,7 +833,7 @@ class HazelcastSourcePdkBaseNodeTest extends BaseHazelcastNodeTest {
 			assertDoesNotThrow(() -> instance.readBatchOffset(syncProgress));
 
 			assertNotNull(syncProgress.getBatchOffsetObj());
-			assertInstanceOf(HashMap.class, syncProgress.getBatchOffsetObj());
+			assertInstanceOf(ConcurrentHashMap.class, syncProgress.getBatchOffsetObj());
 			assertTrue(((Map) syncProgress.getBatchOffsetObj()).isEmpty());
 		}
 
@@ -845,7 +845,7 @@ class HazelcastSourcePdkBaseNodeTest extends BaseHazelcastNodeTest {
 			fakeBatchOffset.put("test", 1);
 			syncProgress.setBatchOffset(PdkUtil.encodeOffset(fakeBatchOffset));
 			instance.readBatchOffset(syncProgress);
-			assertInstanceOf(HashMap.class, syncProgress.getBatchOffsetObj());
+			assertInstanceOf(ConcurrentHashMap.class, syncProgress.getBatchOffsetObj());
 			assertTrue(((Map<?, ?>) syncProgress.getBatchOffsetObj()).isEmpty());
 		}
 	}
@@ -887,7 +887,7 @@ class HazelcastSourcePdkBaseNodeTest extends BaseHazelcastNodeTest {
 			verify(instance, never()).initStreamOffsetInitial();
 			verify(instance, never()).initStreamOffsetCDC(any(TaskDto.class), anyLong());
 			assertNotNull(actualSyncProgress.getBatchOffsetObj());
-			assertInstanceOf(HashMap.class, actualSyncProgress.getBatchOffsetObj());
+			assertInstanceOf(ConcurrentHashMap.class, actualSyncProgress.getBatchOffsetObj());
 			assertTrue(((Map) actualSyncProgress.getBatchOffsetObj()).isEmpty());
 			assertNotNull(actualSyncProgress.getStreamOffsetObj());
 			assertInstanceOf(HashMap.class, actualSyncProgress.getStreamOffsetObj());
@@ -907,7 +907,7 @@ class HazelcastSourcePdkBaseNodeTest extends BaseHazelcastNodeTest {
 			verify(instance, times(1)).initStreamOffsetInitial();
 			verify(instance, never()).initStreamOffsetCDC(any(TaskDto.class), anyLong());
 			assertNotNull(actualSyncProgress.getBatchOffsetObj());
-			assertInstanceOf(HashMap.class, actualSyncProgress.getBatchOffsetObj());
+			assertInstanceOf(ConcurrentHashMap.class, actualSyncProgress.getBatchOffsetObj());
 			assertTrue(((Map) actualSyncProgress.getBatchOffsetObj()).isEmpty());
 			assertNull(actualSyncProgress.getStreamOffsetObj());
 			assertEquals(SyncStage.INITIAL_SYNC.name(), actualSyncProgress.getSyncStage());
