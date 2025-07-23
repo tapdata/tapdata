f1a7960f69bcceee64c0bdf44766af5d389ba611
jarad0628 <coolgj@163.com>
2025-07-23 18:39:18 +0800

    fix: missing tables contain master table warning problem fix unit test bug

--- iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastSourcePdkBaseNode.java
+++ iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastSourcePdkBaseNode.java
@@ -981,7 +981,7 @@ public abstract class HazelcastSourcePdkBaseNode extends HazelcastPdkBaseNode {
             }).orElse(tapTable -> false);
             final Map<TapTable, TapTable> masterAndNewMasterTable = new HashMap<>();
             Set<String> table = partitionTableSubMasterMap.values().stream().map(TapTable::getId).collect(Collectors.toSet());
-            List<List<String>> partition = Lists.partition(addList, BATCH_SIZE);
+            List<List<String>> partition = Lists.partition(new ArrayList<>(addList), BATCH_SIZE);
             partition.forEach(part -> {
                 List<String> batchList = new ArrayList<>(part);
                 LoadSchemaRunner.pdkDiscoverSchema(getConnectorNode(), batchList, tapTable -> {
