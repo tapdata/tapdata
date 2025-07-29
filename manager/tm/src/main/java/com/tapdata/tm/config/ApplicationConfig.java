09ffaf83d1e95bd3a7193b9ef582aeac2e3cb9cf
GavinXiao <2749984520@qq.com>
2025-07-29 17:58:30 +0800

    feat(TAP-7216): TM supports normal return of API ID and client name; Filter invalid API names; The link for displaying API details normally; Support fuzzy search through client name

---
+++ manager/tm/src/main/java/com/tapdata/tm/config/ApplicationConfig.java
@@ -0,0 +1,34 @@
+package com.tapdata.tm.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author <a href="2749984520@qq.com">Gavin'Xiao</a>
+ * @author <a href="https://github.com/11000100111010101100111">Gavin'Xiao</a>
+ * @version v1.0 2025/7/29 11:26 Create
+ * @description
+ */
+
+@Service
+@Data
+public class ApplicationConfig {
+    @Value("{application.title:TM}")
+    String title;
+
+    @Value("{application.version}")
+    String version;
+
+    @Value("{application.commit_version}")
+    String commitVersion;
+
+    @Value("{application.description:Tapdata Manager}")
+    String description;
+
+    @Value("{application.build}")
+    String build;
+
+    @Value("{application.admin-account:admin@admin.com}")
+    String adminAccount;
+}
