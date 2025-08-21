b230ef50864a9027529221303150c44a76ecae8d
hardy <2545433047@qq.com>
2025-08-21 12:24:45 +0800

    feat:TAP-7182 Support target model validation

---
+++ manager/tm-api/src/main/java/com/tapdata/tm/metadatainstance/vo/MetadataInstancesCompareResult.java
@@ -0,0 +1,15 @@
+package com.tapdata.tm.metadatainstance.vo;
+
+import com.tapdata.tm.base.dto.Page;
+import com.tapdata.tm.commons.schema.MetadataInstancesCompareDto;
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+public class MetadataInstancesCompareResult {
+    private Page<MetadataInstancesCompareDto> compareDtos;
+    private List<MetadataInstancesCompareDto> invalidApplyDtos;
+    private String status;
+}
