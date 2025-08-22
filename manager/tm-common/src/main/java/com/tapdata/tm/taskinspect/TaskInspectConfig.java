4823ac0ed24cec841ae028f6556be21b2540356c
harsen <harsen@tapdata.io>
2025-08-22 16:43:29 +0800

    feat(taskinspect): TAP-7986 add update 'queueCapacity' feature, and set default is 1000 or 3 * cdcSample.limit

--- manager/tm-common/src/main/java/com/tapdata/tm/taskinspect/TaskInspectConfig.java
+++ manager/tm-common/src/main/java/com/tapdata/tm/taskinspect/TaskInspectConfig.java
@@ -3,12 +3,13 @@ package com.tapdata.tm.taskinspect;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.tapdata.tm.taskinspect.config.Custom;
-import com.tapdata.tm.taskinspect.config.IConfig;
-import com.tapdata.tm.taskinspect.config.Intelligent;
+import com.tapdata.tm.taskinspect.config.*;
 import com.tapdata.tm.taskinspect.cons.TimeCheckModeEnum;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.query.Update;
+
+import java.util.Optional;
 
 /**
  * @author <a href="mailto:harsen_lin@163.com">Harsen</a>
@@ -17,6 +18,15 @@ import lombok.Setter;
 @Getter
 @Setter
 public class TaskInspectConfig implements IConfig<TaskInspectConfig> {
+    public static final String FIELD_ENABLE = "enable";
+    public static final String FIELD_MODE = "mode";
+    public static final String FIELD_INTELLIGENT = "intelligent";
+    public static final String FIELD_CUSTOM = "custom";
+    public static final String FIELD_QUEUE_CAPACITY = "queueCapacity";
+    public static final String FIELD_CDC_TIMEOUT = "cdcTimeout";
+    public static final String FIELD_CHECK_NO_PK_TABLE = "checkNoPkTable";
+    public static final String FIELD_TIME_CHECK_MODE = "timeCheckMode";
+
     private Boolean enable;                  // 是否开启校验
     private TaskInspectMode mode;            // 校验模式
     private Intelligent intelligent;         // 智能校验配置
@@ -32,11 +42,44 @@ public class TaskInspectConfig implements IConfig<TaskInspectConfig> {
         setMode(init(getMode(), TaskInspectMode.CLOSE));
         setCustom(init(getCustom(), depth, Custom.class));
         setIntelligent(init(getIntelligent(), depth, Intelligent.class));
-        setQueueCapacity(init(getQueueCapacity(), 1000));
         setCdcTimeout(init(getCdcTimeout(), 60 * 1000L));
         setCheckNoPkTable(init(getCheckNoPkTable(), false));
         setTimeCheckMode(init(getTimeCheckMode(), TimeCheckModeEnum.NORMAL));
+
+        // 设置队列容量，默认为：3 倍 CDC 采样数量
+        Integer queueCapacity = getQueueCapacity();
+        if (null == queueCapacity) {
+            if (TaskInspectMode.CUSTOM == getMode()) {
+                queueCapacity = Optional.ofNullable(getCustom())
+                    .map(Custom::getCdc)
+                    .map(CustomCdc::getSample)
+                    .map(CustomCdcSample::getLimit)
+                    .map(capacity -> capacity * 3)
+                    .orElse(null);
+            }
+        }
+        setQueueCapacity(init(queueCapacity, 1000));
         return this;
+    }
+
+    public Update toUpdateUnsetNullValue() {
+        Update update = new Update();
+        update = set(update, FIELD_ENABLE, getEnable());
+        update = set(update, FIELD_MODE, getMode());
+        update = set(update, FIELD_INTELLIGENT, getIntelligent());
+        update = set(update, FIELD_CUSTOM, getCustom());
+        update = set(update, FIELD_QUEUE_CAPACITY, getQueueCapacity());
+        update = set(update, FIELD_CDC_TIMEOUT, getCdcTimeout());
+        update = set(update, FIELD_CHECK_NO_PK_TABLE, getCheckNoPkTable());
+        update = set(update, FIELD_TIME_CHECK_MODE, getTimeCheckMode());
+        return update;
+    }
+
+    private Update set(Update update, String field, Object value) {
+        if (null == value) {
+            return update.unset(field);
+        }
+        return update.set(field, value);
     }
 
     public static TaskInspectConfig createClose() {
