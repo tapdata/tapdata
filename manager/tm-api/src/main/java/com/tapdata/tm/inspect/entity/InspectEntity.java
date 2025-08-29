e7adaea7877704fae6490bd425730c35baacfce1
sam.p <102567930@qq.com>
2025-08-29 14:37:22 +0800

    fix: Handle merge conflict issues

--- manager/tm-api/src/main/java/com/tapdata/tm/inspect/entity/InspectEntity.java
+++ manager/tm-api/src/main/java/com/tapdata/tm/inspect/entity/InspectEntity.java
@@ -2,10 +2,10 @@ package com.tapdata.tm.inspect.entity;
 
 import com.tapdata.tm.base.entity.SchedulableEntity;
 import com.tapdata.tm.commons.base.IDataPermissionEntity;
+import com.tapdata.tm.commons.schema.Tag;
 import com.tapdata.tm.commons.schema.bean.PlatformInfo;
 import com.tapdata.tm.commons.task.dto.alarm.AlarmRuleVO;
 import com.tapdata.tm.commons.task.dto.alarm.AlarmSettingVO;
-import com.tapdata.tm.commons.schema.Tag;
 import com.tapdata.tm.inspect.bean.Limit;
 import com.tapdata.tm.inspect.bean.Task;
 import com.tapdata.tm.inspect.bean.Timing;
@@ -23,38 +23,35 @@ import java.util.List;
 @Data
 @Document("Inspect")
 public class InspectEntity extends SchedulableEntity implements IDataPermissionEntity {
-    private String name;        // 校验任务名称
-    private String flowId;      // 任务ID
-    private String mode;        // 运行方式: 手工执行, 定时调度执行, manual/cron
-    private String inspectMethod;       // 校验方法，row_count: 行数校验；field：字段校验
-    private String inspectDifferenceMode;// 差异结果模式：All(输出所有差异),OnSourceExists(只输出源存在的异常数据)
-    private Timing timing;   // 定时调度表达式，mode = cron 时，需要配置
-    private Limit limit;             //
-    private List<Task> tasks;        // 校验任务明细
+	private String name;        // 校验任务名称
+	private String flowId;      // 任务ID
+	private String mode;        // 运行方式: 手工执行, 定时调度执行, manual/cron
+	private String inspectMethod;       // 校验方法，row_count: 行数校验；field：字段校验
+	private String inspectDifferenceMode;// 差异结果模式：All(输出所有差异),OnSourceExists(只输出源存在的异常数据)
+	private Timing timing;   // 定时调度表达式，mode = cron 时，需要配置
+	private Limit limit;             //
+	private List<Task> tasks;        // 校验任务明细
 
-    private Boolean enabled=true;         // 启用, 禁用校验；禁用时，重复任务都不在执行
+	private Boolean enabled = true;         // 启用, 禁用校验；禁用时，重复任务都不在执行
 
-    private String byFirstCheckId; // 如果是差异校验，需要指定对应批次第一次校验结果编号
+	private String byFirstCheckId; // 如果是差异校验，需要指定对应批次第一次校验结果编号
 
-    private String status;          // "pause/scheduling/running/error/done",    // 任务状态
-    private String errorMsg;        // 状态为 error 时有效
+	private String status;          // "pause/scheduling/running/error/done",    // 任务状态
+	private String errorMsg;        // 状态为 error 时有效
 
-    private Long ping_time;
-    /**  */
-    private Long lastStartTime;
-    private String result;
-    private PlatformInfo platformInfo;
-    private  Boolean is_deleted;
-    private String taskId;
-    private Boolean ignoreTimePrecision;
-    private String roundingMode;
-    private Integer checkTableThreadNum;
-
-    private Integer checkTableThreadNum;
-
-
-
-		private List<AlarmSettingVO> alarmSettings;
-		private List<AlarmRuleVO> alarmRules;
-    private List<Tag> listtags;
+	private Long ping_time;
+	/**
+	 *
+	 */
+	private Long lastStartTime;
+	private String result;
+	private PlatformInfo platformInfo;
+	private Boolean is_deleted;
+	private String taskId;
+	private Boolean ignoreTimePrecision;
+	private String roundingMode;
+	private Integer checkTableThreadNum;
+	private List<AlarmSettingVO> alarmSettings;
+	private List<AlarmRuleVO> alarmRules;
+	private List<Tag> listtags;
 }
