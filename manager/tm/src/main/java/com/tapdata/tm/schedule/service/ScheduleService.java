a07728bdc8e346f1f39e5c20c2e772a511cce892
mnianqi <mnq0626@163.com>
2025-09-03 10:39:36 +0800

    fix: TAP-8138 no need to schedule for cron task when status is wait_run

--- manager/tm/src/main/java/com/tapdata/tm/schedule/service/ScheduleService.java
+++ manager/tm/src/main/java/com/tapdata/tm/schedule/service/ScheduleService.java
@@ -91,7 +91,7 @@ public class ScheduleService {
 
             return;
         }
-        if (TaskDto.STATUS_SCHEDULING.equals(status) || TaskDto.STATUS_WAIT_RUN.equals(status) || (TaskDto.STATUS_RUNNING.equals(status) && TaskDto.TYPE_INITIAL_SYNC.equals(taskDto.getType()))) {
+        if (TaskDto.STATUS_SCHEDULING.equals(status) || TaskDto.STATUS_WAIT_RUN.equals(status) || (TaskDto.STATUS_RUNNING.equals(status) && ParentTaskDto.TYPE_INITIAL_SYNC.equals(taskDto.getType()))) {
             log.info("taskId {},status:{}  不用在进行全量任务", taskId, status);
             return;
         }
