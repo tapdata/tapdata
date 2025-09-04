9e28a5cd6543e1114863fa8432af248e19a670d8
GavinXiao <2749984520@qq.com>
2025-09-04 16:52:02 +0800

    feat(TAP-8111): support get all api-server's info (cup & memory usage)

--- manager/tm/src/main/java/com/tapdata/tm/worker/service/ApiWorkerServer.java
+++ manager/tm/src/main/java/com/tapdata/tm/worker/service/ApiWorkerServer.java
@@ -2,7 +2,9 @@ package com.tapdata.tm.worker.service;
 
 import com.tapdata.tm.apiCalls.service.WorkerCallService;
 import com.tapdata.tm.base.exception.BizException;
+import com.tapdata.tm.worker.dto.ApiServerInfo;
 import com.tapdata.tm.worker.dto.ApiServerWorkerInfo;
+import com.tapdata.tm.worker.dto.MetricInfo;
 import com.tapdata.tm.worker.entity.Worker;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -31,15 +34,46 @@ import java.util.stream.Collectors;
 public class ApiWorkerServer {
     MongoTemplate mongoOperations;
 
-    public List<ApiServerWorkerInfo> getWorkers(String processId) {
-        Worker serverInfo;
+    public ApiServerInfo getWorkers(String processId) {
+        ApiServerInfo server = new ApiServerInfo();
+        Worker serverInfo = null;
+        List<ApiServerWorkerInfo> workers;
         try {
             serverInfo = getServerInfo(processId);
+            workers = pullWorkerInfo(serverInfo);
         } catch (Exception e) {
             log.warn(e.getMessage());
-            return new ArrayList<>();
+            workers = new ArrayList<>();
         }
-        return pullWorkerInfo(serverInfo);
+        server.setWorkers(workers);
+        withInServer(serverInfo, server);
+        return server;
+    }
+
+    public void withInServer(Worker serverInfo, ApiServerInfo server) {
+        Optional.ofNullable(serverInfo).map(Worker::getHostname).ifPresent(server::setName);
+        Optional.ofNullable(serverInfo).map(Worker::getProcessId).ifPresent(server::setProcessId);
+        if (null != serverInfo && serverInfo.getWorker_status() instanceof Map<?,?> status) {
+            server.setMetricValues(toMetricInfo(status.get("metricValues")));
+            Optional.ofNullable(status.get("worker_process_id"))
+                    .filter(Number.class::isInstance)
+                    .map(e -> ((Number) e).intValue())
+                    .ifPresent(server::setWorkerPid);
+            Optional.ofNullable(status.get("status"))
+                    .filter(String.class::isInstance)
+                    .map(e -> (String) e)
+                    .ifPresent(server::setStatus);
+            Optional.ofNullable(status.get("worker_process_start_time"))
+                    .filter(Number.class::isInstance)
+                    .map(e -> ((Number) e).longValue())
+                    .ifPresent(server::setWorkerProcessStartTime);
+            Optional.ofNullable(status.get("worker_process_end_time"))
+                    .filter(Number.class::isInstance)
+                    .map(e -> ((Number) e).longValue())
+                    .ifPresent(server::setWorkerProcessEndTime);
+            Optional.ofNullable(status.get("exit_code"))
+                    .ifPresent(server::setExitCode);
+        }
     }
 
 
@@ -96,6 +130,7 @@ public class ApiWorkerServer {
                     Optional.ofNullable(infoMap.get("metricValues"))
                             .filter(Map.class::isInstance)
                             .map(e -> (Map<String, Object>) e)
+                            .map(this::toMetricInfo)
                             .ifPresent(info::setMetricValues);
                     workerList.add(info);
                 }
@@ -105,7 +140,46 @@ public class ApiWorkerServer {
         return workerList;
     }
 
+    public MetricInfo toMetricInfo(Object value) {
+        MetricInfo info = new MetricInfo();
+        if (value instanceof Map<?, ?> infoMap) {
+            Optional.ofNullable(infoMap.get("HeapMemoryUsage"))
+                    .filter(Number.class::isInstance)
+                    .map(e -> ((Number) e).longValue())
+                    .ifPresent(info::setHeapMemoryUsage);
+            Optional.ofNullable(infoMap.get("CpuUsage"))
+                    .filter(Number.class::isInstance)
+                    .map(e -> ((Number) e).longValue())
+                    .ifPresent(info::setCpuUsage);
+            Optional.ofNullable(infoMap.get("lastUpdateTime"))
+                    .filter(Number.class::isInstance)
+                    .map(e -> ((Number) e).longValue())
+                    .ifPresent(info::setLastUpdateTime);
+        }
+
+        return info;
+    }
+
     public Map<String, String> workerMap(Worker server) {
         return pullWorkerInfo(server).stream().collect(Collectors.toMap(ApiServerWorkerInfo::getOid, ApiServerWorkerInfo::getName, (s1, s2) -> s2));
+    }
+
+    public List<ApiServerInfo> getApiServerWorkerInfo() {
+        Criteria cWorker = Criteria.where("worker_type").is("api-server")
+                .and(WorkerCallService.Tag.DELETE).ne(true);
+        Query qWorker = Query.query(cWorker);
+        List<Worker> server = mongoOperations.find(qWorker, Worker.class, "Workers");
+        if (server.isEmpty()) {
+            return new ArrayList<>();
+        }
+        return server.stream()
+                .filter(Objects::nonNull)
+                .map(e -> {
+                    ApiServerInfo info = new ApiServerInfo();
+                    info.setName(e.getHostname());
+                    withInServer(e, info);
+                    return info;
+                })
+                .toList();
     }
 }
