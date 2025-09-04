5c5993f1754d7e574b8a244c7d9c300399c01417
GavinXiao <2749984520@qq.com>
2025-09-04 12:22:12 +0800

    test(api-server): import assertFalse method

--- manager/tm/src/test/java/com/tapdata/tm/modules/service/ModulesServiceTest.java
+++ manager/tm/src/test/java/com/tapdata/tm/modules/service/ModulesServiceTest.java
@@ -53,6 +53,7 @@ import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
