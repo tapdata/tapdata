d27d3ced89599cfa5b261a8bfe597337ab653bee
GavinXiao <2749984520@qq.com>
2025-09-09 11:15:48 +0800

    feat(TAP-8372): Support setting parameter types as arrays

--- manager/tm/src/main/java/com/tapdata/tm/modules/dto/Param.java
+++ manager/tm/src/main/java/com/tapdata/tm/modules/dto/Param.java
@@ -1,13 +1,19 @@
 package com.tapdata.tm.modules.dto;
 
+import com.tapdata.tm.modules.emuns.ParamType;
 import lombok.Data;
 
 @Data
 public class Param {
-
     private String name;
+
+    /**
+     * @see ParamType
+     * */
     private String type;
+
     private String defaultvalue;
+
     private String description;
 
     private boolean required;
