a8234a0c33757c68ac5dfc5516f1116768425c4f
jarad0628 <coolgj@163.com>
2025-09-10 10:17:02 +0800

    fix: convert bigint taptype bytes to string for front end

---
+++ manager/tm/src/test/java/com/tapdata/tm/metadatainstance/MetadataInstancesControllerTest.java
@@ -0,0 +1,13 @@
+package com.tapdata.tm.metadatainstance;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Class MetadataInstancesController Test")
+public class MetadataInstancesControllerTest {
+
+    @Test
+    void testConvertTapTypeToMap() {
+
+    }
+}
