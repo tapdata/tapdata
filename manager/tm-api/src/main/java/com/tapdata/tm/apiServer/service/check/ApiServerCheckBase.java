834fd4985e6cd682a5b92600519f2c6c693fc068
GavinXiao <2749984520@qq.com>
2025-09-20 21:19:40 +0800

    test(TAP-8449): remove use less service

--- manager/tm-api/src/main/java/com/tapdata/tm/apiServer/service/check/ApiServerCheckBase.java
+++ manager/tm-api/src/main/java/com/tapdata/tm/apiServer/service/check/ApiServerCheckBase.java
@@ -3,9 +3,6 @@ package com.tapdata.tm.apiServer.service.check;
 import com.tapdata.tm.commons.task.constant.AlarmKeyEnum;
 
 public interface ApiServerCheckBase {
-    default boolean enable() {
-        return true;
-    }
 
     AlarmKeyEnum type();
 
