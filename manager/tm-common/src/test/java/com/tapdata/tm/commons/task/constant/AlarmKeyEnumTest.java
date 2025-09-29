f80f8bfc44a9d3dd8e274d26f75df66bc9a2e9df
GavinXiao <2749984520@qq.com>
2025-09-29 14:50:11 +0800

    test(alarm): add unit test case

---
+++ manager/tm-common/src/test/java/com/tapdata/tm/commons/task/constant/AlarmKeyEnumTest.java
@@ -0,0 +1,14 @@
+package com.tapdata.tm.commons.task.constant;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlarmKeyEnumTest {
+
+    @Test
+    void getTaskAlarmKeys() {
+        Assertions.assertEquals(37, AlarmKeyEnum.getTaskAlarmKeys().size());
+    }
+}
