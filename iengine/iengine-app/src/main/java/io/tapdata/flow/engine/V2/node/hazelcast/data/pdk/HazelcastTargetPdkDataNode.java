22ca9f49cad7e7d37b40c35830c7eb523df4eb92
sam.p <102567930@qq.com>
2025-12-05 22:46:44 +0800

    fix: Fixed the issue of precise write and transaction operations
    (cherry picked from commit 85d0c6a9446f70fa65c92ca2c005ee2fb4e8c5d4)

--- iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastTargetPdkDataNode.java
+++ iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastTargetPdkDataNode.java
@@ -1189,13 +1189,8 @@ public class HazelcastTargetPdkDataNode extends HazelcastTargetPdkBaseNode {
 		}
 		ConnectorFunctions connectorFunctions = connectorNode.getConnectorFunctions();
 		TransactionBeginFunction transactionBeginFunction = connectorFunctions.getTransactionBeginFunction();
-		PDKMethodInvoker pdkMethodInvoker = createPdkMethodInvoker();
-		try {
-			PDKInvocationMonitor.invoke(connectorNode, PDKMethod.TRANSACTION_BEGIN,
-					() -> pdkMethodInvoker.runnable(() -> transactionBeginFunction.begin(connectorNode.getConnectorContext())), TAG);
-		} finally {
-			removePdkMethodInvoker(pdkMethodInvoker);
-		}
+		PDKInvocationMonitor.invoke(connectorNode, PDKMethod.TRANSACTION_BEGIN,
+				() -> transactionBeginFunction.begin(connectorNode.getConnectorContext()), TAG);
 	}
 
 	@Override
@@ -1206,13 +1201,8 @@ public class HazelcastTargetPdkDataNode extends HazelcastTargetPdkBaseNode {
 		}
 		ConnectorFunctions connectorFunctions = connectorNode.getConnectorFunctions();
 		TransactionCommitFunction transactionCommitFunction = connectorFunctions.getTransactionCommitFunction();
-		PDKMethodInvoker pdkMethodInvoker = createPdkMethodInvoker();
-		try {
-			PDKInvocationMonitor.invoke(connectorNode, PDKMethod.TRANSACTION_BEGIN,
-					() -> pdkMethodInvoker.runnable(() -> transactionCommitFunction.commit(connectorNode.getConnectorContext())), TAG);
-		} finally {
-			removePdkMethodInvoker(pdkMethodInvoker);
-		}
+		PDKInvocationMonitor.invoke(connectorNode, PDKMethod.TRANSACTION_BEGIN,
+				() -> transactionCommitFunction.commit(connectorNode.getConnectorContext()), TAG);
 	}
 
 	@Override
@@ -1223,13 +1213,8 @@ public class HazelcastTargetPdkDataNode extends HazelcastTargetPdkBaseNode {
 		}
 		ConnectorFunctions connectorFunctions = connectorNode.getConnectorFunctions();
 		TransactionRollbackFunction transactionRollbackFunction = connectorFunctions.getTransactionRollbackFunction();
-		PDKMethodInvoker pdkMethodInvoker = createPdkMethodInvoker();
-		try {
-			PDKInvocationMonitor.invoke(connectorNode, PDKMethod.TRANSACTION_BEGIN,
-					() -> pdkMethodInvoker.runnable(() -> transactionRollbackFunction.rollback(connectorNode.getConnectorContext())), TAG);
-		} finally {
-			removePdkMethodInvoker(pdkMethodInvoker);
-		}
+		PDKInvocationMonitor.invoke(connectorNode, PDKMethod.TRANSACTION_BEGIN,
+				() -> transactionRollbackFunction.rollback(connectorNode.getConnectorContext()), TAG);
 	}
 
 	@Override
