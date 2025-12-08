0147aef3d44213a26ff3eb16927c6c0dd47d9289
GavinXiao <2749984520@qq.com>
2025-12-08 14:30:01 +0800

    fix(TAP-8714): Accumulate batches and add timeout checks. The receiver supports batch reception

--- iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/batch/BatchAcceptor.java
+++ iengine/iengine-app/src/main/java/io/tapdata/flow/engine/V2/node/hazelcast/data/batch/BatchAcceptor.java
@@ -1,13 +1,18 @@
 package io.tapdata.flow.engine.V2.node.hazelcast.data.batch;
 
 import io.tapdata.entity.event.TapEvent;
+import io.tapdata.observable.logging.ObsLogger;
 import io.tapdata.pdk.apis.consumer.TapStreamReadConsumer;
-import org.springframework.util.CollectionUtils;
+import io.tapdata.pdk.core.async.ThreadPoolExecutorEx;
+import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
 
 
 /**
@@ -17,17 +22,122 @@ import java.util.Objects;
  * @description
  */
 public final class BatchAcceptor {
+
+    static class EventInfo {
+        private final Object e;
+        @Getter
+        private final Object offset;
+        @Getter
+        private final int size;
+
+        public EventInfo(List<TapEvent> events, Object offset) {
+            this.e = events;
+            this.offset = offset;
+            this.size = events.size();
+        }
+
+        public EventInfo(TapEvent events, Object offset) {
+            this.e = events;
+            this.offset = offset;
+            this.size = 1;
+        }
+
+        public Object getEvents() {
+            return e;
+        }
+
+    }
+
     private final ValueGetter<Integer> batchSizeGetter;
     private final ValueGetter<Integer> delayMsGetter;
     private final TapStreamReadConsumer<List<TapEvent>, Object> consumer;
-    private List<TapEvent> values;
-    private long lastAcceptTime;
-    public BatchAcceptor(ValueGetter<Integer> batchSizeGetter, ValueGetter<Integer> delayMsGetter, TapStreamReadConsumer<List<TapEvent>, Object> consumer) {
+    private final LinkedBlockingQueue<EventInfo> pipeline;
+    private final Predicate<Boolean> alive;
+    private final ObsLogger obsLogger;
+    boolean active = false;
+    Future<?> monitor;
+
+
+    public BatchAcceptor(ValueGetter<Integer> batchSizeGetter, ValueGetter<Integer> delayMsGetter, Predicate<Boolean> isAlive, TapStreamReadConsumer<List<TapEvent>, Object> consumer, ObsLogger obsLogger) {
         this.batchSizeGetter = batchSizeGetter;
         this.delayMsGetter = delayMsGetter;
         this.consumer = consumer;
-        this.values = new ArrayList<>();
-        this.lastAcceptTime = 0L;
+        this.pipeline = new LinkedBlockingQueue<>(2000);
+        this.alive = isAlive;
+        this.obsLogger = obsLogger;
+    }
+
+    public void startMonitor(ThreadPoolExecutorEx sourceRunner) {
+        this.active = true;
+        this.monitor = sourceRunner.submit(this::consumer);
+    }
+
+    void consumer() {
+        long lastTime = System.currentTimeMillis();
+        List<TapEvent> events = new ArrayList<>();
+        Object lastOffset = null;
+        int count = 0;
+        while (alive.test(null)) {
+            EventInfo eventInfo = null;
+            while (alive.test(null) && !pipeline.isEmpty()) {
+                Integer delay = delayMsGetter.get();
+                long timeout = fixDelay(delay);
+                eventInfo = pollSingle(timeout);
+                if (eventInfo != null) {
+                    classification(eventInfo, events);
+                    lastOffset = eventInfo.getOffset();
+                    count += eventInfo.getSize();
+                }
+                if (count >= batchSizeGetter.get()) {
+                    this.consumer.accept(events, lastOffset);
+                    lastTime = System.currentTimeMillis();
+                    events = new ArrayList<>();
+                    count = 0;
+                }
+            }
+            if (!events.isEmpty() && System.currentTimeMillis() - lastTime > delayMsGetter.get()) {
+                this.consumer.accept(events, lastOffset);
+                lastTime = System.currentTimeMillis();
+                count = 0;
+            }
+        }
+        this.active = false;
+    }
+
+    long fixDelay(Integer delay) {
+        return delay <= 0 ? 1000L : delay.longValue();
+    }
+
+    void classification(EventInfo eventInfo, List<TapEvent> events) {
+        if (eventInfo.getEvents() instanceof TapEvent e) {
+            events.add(e);
+        } else if (eventInfo.getEvents() instanceof Collection<?> list) {
+            events.addAll((Collection<TapEvent>) list);
+        }
+    }
+
+    void offerSingle(EventInfo eventInfo) {
+        try {
+            boolean offered = this.pipeline.offer(eventInfo, 3, TimeUnit.SECONDS);
+            if (!offered) {
+                // Handle the case where the offer was not successful
+                // For example, log an error or take other appropriate action
+                obsLogger.debug("Failed to offer eventInfo to the pipeline within 3 seconds.");
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    EventInfo pollSingle(long timeout) {
+        EventInfo poll = null;
+        try {
+            poll = pipeline.poll(timeout, TimeUnit.SECONDS);
+            return poll;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return null;
+        }
     }
 
     public long getDelayMs() {
@@ -35,31 +145,22 @@ public final class BatchAcceptor {
     }
 
     public void accept(TapEvent event, Object offset) {
-        if (null == event) {
+        if (!this.active || null == event) {
             return;
         }
-        Collection<TapEvent> events = new ArrayList<>();
-        events.add(event);
-        accept(events, offset);
+        offerSingle(new EventInfo(event, offset));
     }
 
-    public void accept(Collection<TapEvent> events, Object offset) {
-        if (CollectionUtils.isEmpty(events)) {
+    public void accept(List<TapEvent> event, Object offset) {
+        if (!this.active || null == event || event.isEmpty()) {
             return;
         }
-        List<TapEvent> es = events.stream()
-                .filter(Objects::nonNull)
-                .toList();
-        if (CollectionUtils.isEmpty(es)) {
-            return;
-        }
-        this.values.addAll(es);
-        if (this.values.size() >= batchSizeGetter.get() ||
-                (this.lastAcceptTime > 0L && System.currentTimeMillis() - this.lastAcceptTime > this.getDelayMs())) {
-            consumer.accept(this.values, offset);
-            this.values = new ArrayList<>();
-            this.lastAcceptTime = System.currentTimeMillis();
-        }
+        offerSingle(new EventInfo(event, offset));
+    }
+
+    public void close() {
+        this.active = false;
+        this.monitor.cancel(true);
     }
 
 
