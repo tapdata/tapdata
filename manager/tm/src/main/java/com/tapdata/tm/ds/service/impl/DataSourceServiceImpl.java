27167ce0de7716645df5aaa3c1f44007be9ea4ee
GavinXiao <2749984520@qq.com>
2025-12-19 15:10:42 +0800

    fix(TAP-9016): Use the correct key

--- manager/tm/src/main/java/com/tapdata/tm/ds/service/impl/DataSourceServiceImpl.java
+++ manager/tm/src/main/java/com/tapdata/tm/ds/service/impl/DataSourceServiceImpl.java
@@ -2365,7 +2365,7 @@ public class DataSourceServiceImpl extends DataSourceService{
         final Map<String, String> mapPdkHash = all.stream()
                 .filter(e -> StringUtils.isNotBlank(e.getRealName()))
                 .collect(Collectors.toMap(DataSourceDefinitionDto::getPdkHash, DataSourceDefinitionDto::getRealName, (e1, e2) -> e2));
-        databaseTypes.forEach(item -> Optional.ofNullable(mapPdkHash.get(item.get(PDK_HASH))).ifPresent(n -> item.put(DATABASE_TYPE, n)));
+        databaseTypes.forEach(item -> Optional.ofNullable(mapPdkHash.get(item.get(PDK_HASH))).ifPresent(n -> item.put(DATABASE_TYPE_HUMP, n)));
         return databaseTypes;
     }
 
