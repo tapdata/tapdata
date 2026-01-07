8ffd2eea750ddbce95d999f5eedc90b6368db308
GavinXiao <2749984520@qq.com>
2026-01-07 18:04:51 +0800

    feat(TAP-9227):API monitoring refactoring

---
+++ manager/tm/src/main/java/com/tapdata/tm/v2/api/monitor/main/dto/ApiItem.java
@@ -0,0 +1,41 @@
+package com.tapdata.tm.v2.api.monitor.main.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @author <a href="2749984520@qq.com">Gavin'Xiao</a>
+ * @author <a href="https://github.com/11000100111010101100111">Gavin'Xiao</a>
+ * @version v1.0 2025/12/31 09:03 Create
+ * @description
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class ApiItem extends ValueBase {
+    String apiId;
+    String apiPath;
+    String apiName;
+    /**
+     * 总调用数
+     */
+    long requestCount;
+
+    /**
+     * 平均耗时
+     */
+    double requestCostAvg;
+    Long p95;
+    Long p99;
+    Long maxDelay;
+    Long minDelay;
+
+    /**
+     * 错误率
+     */
+    double errorRate;
+
+    /**
+     * 吞吐量
+     */
+    double totalRps;
+}
---
+++ manager/tm/src/main/java/com/tapdata/tm/v2/api/monitor/service/ApiMetricsRawScheduleExecutor.java
@@ -0,0 +1,141 @@
+package com.tapdata.tm.v2.api.monitor.service;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Sorts;
+import com.tapdata.tm.apiCalls.entity.ApiCallEntity;
+import com.tapdata.tm.apiCalls.service.WorkerCallServiceImpl;
+import com.tapdata.tm.utils.MongoUtils;
+import com.tapdata.tm.v2.api.monitor.main.entity.ApiMetricsRaw;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.BulkOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+
+/**
+ * @author <a href="2749984520@qq.com">Gavin'Xiao</a>
+ * @author <a href="https://github.com/11000100111010101100111">Gavin'Xiao</a>
+ * @version v1.0 2025/12/26 11:44 Create
+ * @description
+ */
+@Service
+@Slf4j
+@Setter(onMethod_ = {@Autowired})
+public class ApiMetricsRawScheduleExecutor {
+    public static final String OBJECT_ID = "_id";
+    MongoTemplate mongoTemplate;
+    MongoTemplate mongoOperations;
+
+    public void aggregateApiCall() {
+        final String collectionName = MongoUtils.getCollectionNameIgnore(ApiCallEntity.class);
+        if (StringUtils.isBlank(collectionName)) {
+            return;
+        }
+        ObjectId lastCallId = lastOne();
+        try (MetricInstanceFactory acceptor = new MetricInstanceFactory(this::saveApiMetricsRaw, this::findMetricStart)) {
+            final MongoCollection<Document> collection = mongoTemplate.getCollection(collectionName);
+            final Criteria criteria = Criteria.where("deleted").ne(true)
+                    .and("supplement").ne(true);
+            if (Objects.nonNull(lastCallId)) {
+                criteria.and(OBJECT_ID).gt(lastCallId);
+            }
+            final Query query = Query.query(criteria);
+            final Document queryObject = query.getQueryObject();
+            final FindIterable<Document> iterable =
+                    collection.find(queryObject, Document.class)
+                            .sort(Sorts.ascending(OBJECT_ID))
+                            .batchSize(1000);
+            try (final MongoCursor<Document> cursor = iterable.iterator()) {
+                while (cursor.hasNext()) {
+                    final Document entity = cursor.next();
+                    acceptor.accept(entity);
+                }
+            }
+        }
+    }
+
+    ObjectId lastOne() {
+        Query query = Query.query(Criteria.where("timeGranularity").is(2));
+        query.with(Sort.by(Sort.Order.desc("callId"))).limit(1);
+        ApiMetricsRaw lastOne = mongoTemplate.findOne(query, ApiMetricsRaw.class);
+        if (null != lastOne) {
+            return lastOne.getCallId();
+        }
+        return null;
+    }
+
+
+    public void saveApiMetricsRaw(List<ApiMetricsRaw> apiMetricsRawList) {
+        if (CollectionUtils.isEmpty(apiMetricsRawList)) {
+            return;
+        }
+        bulkUpsert(apiMetricsRawList);
+    }
+
+    public void bulkUpsert(List<ApiMetricsRaw> entities) {
+        bulkUpsert(entities, this::buildDefaultQuery, this::buildDefaultUpdate);
+    }
+
+
+    public void bulkUpsert(List<ApiMetricsRaw> entities,
+                           Function<ApiMetricsRaw, Query> queryBuilder,
+                           Function<ApiMetricsRaw, Update> updateBuilder) {
+        try {
+            if (entities == null || entities.isEmpty()) {
+                return;
+            }
+            BulkOperations bulkOps = mongoOperations.bulkOps(BulkOperations.BulkMode.ORDERED, ApiMetricsRaw.class);
+            for (ApiMetricsRaw entity : entities) {
+                Query query = queryBuilder.apply(entity);
+                Update update = updateBuilder.apply(entity);
+                bulkOps.upsert(query, update);
+            }
+            bulkOps.execute();
+        } catch (Exception e) {
+            log.error("bulkUpsert ApiMetricsRaw error", e);
+        }
+    }
+
+    private Query buildDefaultQuery(ApiMetricsRaw entity) {
+        Criteria criteria = Criteria.where(WorkerCallServiceImpl.Tag.TIME_START).is(entity.getTimeStart())
+                .and(WorkerCallServiceImpl.Tag.TIME_GRANULARITY).is(entity.getTimeGranularity())
+                .and(WorkerCallServiceImpl.Tag.PROCESS_ID).is(entity.getProcessId())
+                .and(WorkerCallServiceImpl.Tag.API_ID).is(entity.getApiId());
+        return Query.query(criteria);
+    }
+
+    private Update buildDefaultUpdate(ApiMetricsRaw entity) {
+        Update update = new Update();
+        update.set("reqCount", entity.getReqCount());
+        update.set("errorCount", entity.getErrorCount());
+        update.set("rps", entity.getRps());
+        update.set("bytes", entity.getBytes());
+        update.set("delay", entity.getDelay());
+        update.set("subMetrics", entity.getSubMetrics());
+        update.set("p50", entity.getP50());
+        update.set("p95", entity.getP95());
+        update.set("p99", entity.getP99());
+        update.set("callId", entity.getCallId());
+        update.currentDate("updatedAt");
+        return update;
+    }
+
+    public ApiMetricsRaw findMetricStart(Query query) {
+        return mongoTemplate.findOne(query, ApiMetricsRaw.class);
+    }
+}
