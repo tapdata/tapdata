3425e7788dba2a99d8da4757d027e1ae9ceb2a28
GavinXiao <2749984520@qq.com>
2026-03-12 18:50:28 +0800

    fix(lombok): Cannot duplicate methods with lombok

--- manager/tm-api/src/main/java/com/tapdata/tm/worker/dto/ApiServerStatus.java
+++ manager/tm-api/src/main/java/com/tapdata/tm/worker/dto/ApiServerStatus.java
@@ -1,7 +1,6 @@
 package com.tapdata.tm.worker.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.HashMap;
@@ -14,7 +13,6 @@ import java.util.Map;
  * @version v1.0 2025/9/17 14:42 Create
  * @description
  */
-@Data
 public class ApiServerStatus {
 
     @JsonProperty("worker_process_id")
@@ -49,7 +47,17 @@ public class ApiServerStatus {
 
     Long auditLogPushMaxDelay;
 
-    public void setWorkers(List<ApiServerWorkerInfo> workers) {
+    public void setWorkers(Object obj) {
+        if (obj instanceof Map map) {
+            workers = (Map<String, ApiServerWorkerInfo>) map;
+        } else if (obj instanceof List list) {
+            setWorkersList((List<ApiServerWorkerInfo>)list);
+        } else {
+            workers = new HashMap<>();
+        }
+    }
+
+    public void setWorkersList(List<ApiServerWorkerInfo> workers) {
         if (workers == null) {
             this.workers = new HashMap<>();
             return;
@@ -61,7 +69,87 @@ public class ApiServerStatus {
         setWorkers(newWorkers);
     }
 
-    public void setWorkers(Map<String, ApiServerWorkerInfo> workers) {
-        this.workers = workers;
+    public Integer getWorkerProcessId() {
+        return workerProcessId;
+    }
+
+    public void setWorkerProcessId(Integer workerProcessId) {
+        this.workerProcessId = workerProcessId;
+    }
+
+    public Long getWorkerProcessStartTime() {
+        return workerProcessStartTime;
+    }
+
+    public void setWorkerProcessStartTime(Long workerProcessStartTime) {
+        this.workerProcessStartTime = workerProcessStartTime;
+    }
+
+    public Long getWorkerProcessEndTime() {
+        return workerProcessEndTime;
+    }
+
+    public void setWorkerProcessEndTime(Long workerProcessEndTime) {
+        this.workerProcessEndTime = workerProcessEndTime;
+    }
+
+    public Integer getExitCode() {
+        return exitCode;
+    }
+
+    public void setExitCode(Integer exitCode) {
+        this.exitCode = exitCode;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public Long getActiveTime() {
+        return activeTime;
+    }
+
+    public void setActiveTime(Long activeTime) {
+        this.activeTime = activeTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Map<String, ApiServerWorkerInfo> getWorkers() {
+        return workers;
+    }
+
+    public MetricInfo getMetricValues() {
+        return metricValues;
+    }
+
+    public void setMetricValues(MetricInfo metricValues) {
+        this.metricValues = metricValues;
+    }
+
+    public Boolean getUpdateCpuMem() {
+        return updateCpuMem;
+    }
+
+    public void setUpdateCpuMem(Boolean updateCpuMem) {
+        this.updateCpuMem = updateCpuMem;
+    }
+
+    public Long getAuditLogPushMaxDelay() {
+        return auditLogPushMaxDelay;
+    }
+
+    public void setAuditLogPushMaxDelay(Long auditLogPushMaxDelay) {
+        this.auditLogPushMaxDelay = auditLogPushMaxDelay;
     }
 }
