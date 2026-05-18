6c83ee4e06ffcc02d08b69514af84b72ce19b138
harsen <harsen@tapdata.io>
2026-05-18 11:38:39 +0800

    test(HazelcastTargetPdkDataNode): Initializes the test context with a task ID.
    - Sets the `ObjectId` as the ID for `taskDto` in the `beforeEach` method.
    - Ensures that task objects in the test environment have unique identifiers.
    - Fixes an issue that could cause test failures due to missing task IDs.
    
    (cherry picked from commit 3a1caf118f8dd9907452de84d57d1f5aa305edc9)

--- iengine/iengine-app/src/test/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastTargetPdkDataNodeTest.java
+++ iengine/iengine-app/src/test/java/io/tapdata/flow/engine/V2/node/hazelcast/data/pdk/HazelcastTargetPdkDataNodeTest.java
@@ -1761,6 +1761,7 @@ class HazelcastTargetPdkDataNodeTest extends BaseTaskTest {
 		void beforeEach() {
 			context = mock(DataProcessorContext.class);
 			TaskDto taskDto = new TaskDto();
+            taskDto.setId(ObjectId.get());
 			taskDto.setSyncType(SyncTypeEnum.INITIAL_SYNC.getSyncType());
 			taskDto.setType(SyncTypeEnum.INITIAL_SYNC.getSyncType());
 			when(context.getTaskDto()).thenReturn(taskDto);
