df24deae5721e7b04187894a57c8210ff8da276c
harsen <harsen@tapdata.io>
2026-05-18 11:24:57 +0800

    test(AgentLogConfigurationWatcher): Removes redundant additive property assertions
    - Removed the assertion test for logger.isAdditive()
    - Retained core log level update functionality verification
    - Cleaned up redundant assertion statements in the test code

--- iengine/iengine-app/src/test/java/io/tapdata/Schedule/Watcher/AgentLogConfigurationWatcherTest.java
+++ iengine/iengine-app/src/test/java/io/tapdata/Schedule/Watcher/AgentLogConfigurationWatcherTest.java
@@ -122,7 +122,6 @@ public class AgentLogConfigurationWatcherTest {
             doCallRealMethod().when(agentLogConfigurationWatcher).updateLogLevel(logConfiguration);
             agentLogConfigurationWatcher.updateLogLevel(logConfiguration);
             assertEquals("DEBUG",logger.getLevel().name());
-            assertEquals(true,logger.isAdditive());
         }
         @DisplayName("test Update logLevel with info, but logger use parent additive")
         @Test
