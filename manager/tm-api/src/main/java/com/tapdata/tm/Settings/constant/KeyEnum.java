286b1a7d5faff0ecae040aa0b6ee84372aabfafc
mnianqi <mnq0626@163.com>
2026-08-13 15:51:34 +0800

    feat: TAP-12033 Add data models and dependency

--- manager/tm-api/src/main/java/com/tapdata/tm/Settings/constant/KeyEnum.java
+++ manager/tm-api/src/main/java/com/tapdata/tm/Settings/constant/KeyEnum.java
@@ -18,7 +18,27 @@ public enum KeyEnum {
     LOGIN_SINGLE_SESSION("login.single.session"),
     LOGIN_BRIEF_TIPS("login.brief.tips"),
     TASK_START_TRANSFORM_WAIT_SECONDS("task.start.transformWaitSeconds"),
-    TOKEN_IDLE_TIMEOUT_MINUTES("access.token.idle.timeout.minutes")
+    TOKEN_IDLE_TIMEOUT_MINUTES("access.token.idle.timeout.minutes"),
+
+    // ---- Generic, IdP-agnostic SAML 2.0 SSO configuration (single active config) ----
+    SAML_LOGIN_ENABLE("saml.login.enable"),
+    SAML_SP_ENTITY_ID("saml.sp.entityId"),
+    SAML_SP_ACS_URL("saml.sp.acsUrl"),
+    SAML_SP_PRIVATE_KEY("saml.sp.privateKey"),
+    SAML_SP_CERTIFICATE("saml.sp.certificate"),
+    SAML_IDP_ENTITY_ID("saml.idp.entityId"),
+    SAML_IDP_SSO_URL("saml.idp.ssoUrl"),
+    SAML_IDP_SLO_URL("saml.idp.sloUrl"),
+    SAML_IDP_SIGNING_CERTIFICATE("saml.idp.signingCertificate"),
+    SAML_NAME_ID_FORMAT("saml.nameIdFormat"),
+    SAML_WANT_ASSERTIONS_SIGNED("saml.wantAssertionsSigned"),
+    SAML_SIGN_AUTHN_REQUEST("saml.signAuthnRequest"),
+    SAML_SIGNATURE_ALGORITHM("saml.signatureAlgorithm"),
+    SAML_CLOCK_SKEW_SECONDS("saml.clockSkewSeconds"),
+    SAML_CLAIM_USERNAME("saml.claim.username"),
+    SAML_CLAIM_EMAIL("saml.claim.email"),
+    SAML_CLAIM_DISPLAY_NAME("saml.claim.displayName"),
+    SAML_CLAIM_GROUPS("saml.claim.groups")
     ;
 
     private String value;
